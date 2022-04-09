abc3b9304c86a2b9fcc029e2597d0a75e270c168
David Scicluna <dave_nostra@hotmail.com>
2022-04-09 17:15:09 +0200

    Feature: Button

---
+++ src/components/Clickable/Button/index.tsx
@@ -0,0 +1,85 @@
+import React, { ReactElement, forwardRef, useCallback } from 'react';
+
+import { ColorMode, useTheme, useColorMode, Button as CUIButton, Center, HStack } from '@chakra-ui/react';
+
+import { merge } from 'lodash';
+import { useElementSize } from 'usehooks-ts';
+
+import useStyles from './common/styles';
+import { handleSize } from './common/utils';
+import Spinner from './components/Spinner';
+import { ButtonRef, ButtonProps } from './types';
+
+import { Theme } from '../../../theme/types';
+
+const Button = forwardRef<ButtonRef, ButtonProps>(function Button(props, ref): ReactElement {
+	const theme = useTheme<Theme>();
+	const { colorMode: colorModeHook } = useColorMode();
+
+	const [childrenRef, { width, height }] = useElementSize();
+
+	const {
+		children,
+		color = 'gray',
+		colorMode: colorModeProp,
+		renderLeft,
+		renderRight,
+		isDisabled = false,
+		isFullWidth = false,
+		isLoading = false,
+		size = 'md',
+		variant = 'contained',
+		sx,
+		...rest
+	} = props;
+
+	const colorMode: ColorMode = colorModeProp || colorModeHook;
+
+	const style = useStyles(theme, { color, colorMode, isFullWidth, isLoading, size, variant });
+
+	const handleReturnSpacing = useCallback((): number => handleSize(size).spacing, [size, handleSize]);
+
+	return (
+		<CUIButton
+			{...rest}
+			ref={ref}
+			tabIndex={0}
+			isDisabled={isLoading || isDisabled}
+			variant='unstyled'
+			sx={merge(style.button, sx)}
+			_disabled={style.disabled}
+		>
+			<HStack width='inherit' align='center' justify='center' flex={1} spacing={handleReturnSpacing()}>
+				{isLoading && !renderLeft ? (
+					<Spinner
+						color={color}
+						colorMode={colorMode}
+						hasChildren={typeof children === 'string'}
+						size={size}
+						variant={variant}
+					/>
+				) : renderLeft ? (
+					renderLeft({ color, colorMode, width, height, size })
+				) : null}
+				{children ? (
+					<Center ref={childrenRef} as='span'>
+						{children}
+					</Center>
+				) : null}
+				{isLoading && renderLeft ? (
+					<Spinner
+						color={color}
+						colorMode={colorMode}
+						hasChildren={typeof children === 'string'}
+						size={size}
+						variant={variant}
+					/>
+				) : renderRight ? (
+					renderRight({ color, colorMode, width, height, size })
+				) : null}
+			</HStack>
+		</CUIButton>
+	);
+});
+
+export default Button;
---
+++ src/components/Clickable/Button/types.ts
@@ -0,0 +1,75 @@
+import { ReactNode } from 'react';
+
+import { ColorMode, ButtonProps as CUIButtonProps } from '@chakra-ui/react';
+
+import {
+	BoxPadding,
+	BoxColor,
+	BoxGradient,
+	BoxTypography,
+	BoxLayout,
+	BoxFlexbox,
+	BoxGrid,
+	BoxBackground,
+	BoxBorders,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../../common/types/box';
+import { Color } from '../../../theme/types';
+
+export type ButtonRef = HTMLButtonElement | null;
+
+export type ButtonColor = Exclude<Color, 'transparent'>;
+
+export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type Variant = 'contained' | 'outlined' | 'text';
+
+export type RenderProps = {
+	color?: Color;
+	colorMode?: ColorMode;
+	width?: number; // In Pixels
+	height?: number; // In Pixels
+	size?: Size;
+};
+
+type Omitted =
+	// MUI Box Props
+	| BoxPadding
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxLayout
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// MUI Button Props
+	| 'as'
+	| 'children'
+	| 'color'
+	| 'colorScheme'
+	| 'iconSpacing'
+	| 'leftIcon'
+	| 'loadingText'
+	| 'rightIcon'
+	| 'size'
+	| 'spinner'
+	| 'spinnerPlacement'
+	| 'variant';
+
+export type ButtonProps = {
+	children?: ReactNode;
+	color?: ButtonColor;
+	colorMode?: ColorMode;
+	renderLeft?: (props: RenderProps) => ReactNode;
+	renderRight?: (props: RenderProps) => ReactNode;
+	size?: Size;
+	variant?: Variant;
+} & Omit<CUIButtonProps, Omitted>;
