9fb53b7af289a0a00a563dce1464784f76631148
David Scicluna <dave_nostra@hotmail.com>
2022-04-09 14:53:45 +0200

    Feature: Common types & utils

---
+++ src/common/types/index.ts
@@ -0,0 +1,7 @@
+import { CSSObject } from '@chakra-ui/react';
+
+// Utility Types
+export type NonNullable<T> = Exclude<T, null | undefined>; // Remove null and undefined from Type
+
+// Component Types
+export type Style = CSSObject;
---
+++ src/common/utils/index.ts
@@ -0,0 +1,30 @@
+/**
+ * This method will check whether the user's device is a touch device or not
+ *
+ * @returns boolean: Either its a touch device or not
+ */
+export const handleIsTouchDevice = (): boolean => {
+	return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+	// || navigator.msMaxTouchPoints > 0;
+};
+
+/**
+ * This method will convert a REM size to PX size
+ *
+ * @param rem - number: REM size
+ * @returns - number: Converted PX size from REM size
+ */
+export const handleConvertREMToPixels = (rem: number): number => {
+	return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+};
+
+/**
+ * This method will remove a portion of a string from the string passed and convert the string to a number
+ *
+ * @param string - string: String value to be cut & converted
+ * @param cut - string: The string to cut from the string
+ * @returns number: A number from the string passed
+ */
+export const handleConvertStringToNumber = (string: string, cut: string): number => {
+	return Number(string.replace(cut, ''));
+};
