26bdb25efefc1babc263434396ca11ca05474041
David Scicluna <dave_nostra@hotmail.com>
2022-04-10 17:42:58 +0200

    Feature: IconButton Spinner

---
+++ src/components/Clickable/IconButton/components/Spinner/index.tsx
@@ -0,0 +1,62 @@
+import { ReactElement, useCallback } from 'react';
+
+import { useTheme } from '@chakra-ui/react';
+
+import { toRgba } from 'color2k';
+import { SpinnerCircularFixed } from 'spinners-react';
+
+import { SpinnerProps } from './types';
+
+import { handleConvertREMToPixels, handleConvertStringToNumber } from '../../../../../common/utils';
+import { Theme } from '../../../../../theme/types';
+
+const Spinner = (props: SpinnerProps): ReactElement => {
+	const theme = useTheme<Theme>();
+
+	const { color, colorMode, size, variant } = props;
+
+	/**
+	 * This method will return the appropriate color in RGBA depending on color & colorMode props
+	 *
+	 * @returns - string: RGBA color
+	 */
+	const handleReturnColorRGBA = useCallback((): string => {
+		switch (variant) {
+			case 'outlined':
+			case 'icon': {
+				return toRgba(theme.colors[color][colorMode === 'light' ? 500 : 400]);
+			}
+			default: {
+				return toRgba(
+					theme.colors.gray[
+						colorMode === 'light' ? (color === 'white' ? 400 : 50) : color === 'black' ? 500 : 900
+					]
+				);
+			}
+		}
+	}, [variant, theme, color, colorMode]);
+
+	/**
+	 * This method will return the appropriate font-size in PX depending on size prop
+	 *
+	 * @returns - number: Font-size in PX
+	 */
+	const handleReturnSize = useCallback((): number => {
+		return handleConvertREMToPixels(handleConvertStringToNumber(theme.fontSizes[size], 'rem'));
+	}, [size, theme]);
+
+	const rgba = handleReturnColorRGBA();
+	const pixels = handleReturnSize();
+
+	return (
+		<SpinnerCircularFixed
+			color={rgba}
+			secondaryColor={toRgba(theme.colors.gray[colorMode === 'light' ? 200 : 700])}
+			size={pixels}
+			thickness={160}
+			speed={140}
+		/>
+	);
+};
+
+export default Spinner;
---
+++ src/components/Clickable/IconButton/components/Spinner/types.ts
@@ -0,0 +1,10 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { IconButtonColor, Size, Variant } from '../../types';
+
+export type SpinnerProps = {
+	color: IconButtonColor;
+	colorMode: ColorMode;
+	size: Size;
+	variant: Variant;
+};
