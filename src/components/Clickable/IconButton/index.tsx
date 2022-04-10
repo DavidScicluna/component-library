72fad5c86c7b6d83fdec82686cfa016b4573bb2a
David Scicluna <dave_nostra@hotmail.com>
2022-04-10 17:43:10 +0200

    Feature: IconButton

---
+++ src/components/Clickable/IconButton/index.tsx
@@ -0,0 +1,49 @@
+import { ReactElement, forwardRef } from 'react';
+
+import { ColorMode, useTheme, useColorMode, IconButton as CUIIconButton } from '@chakra-ui/react';
+
+import merge from 'lodash/merge';
+
+import useStyles from './common/styles';
+import Spinner from './components/Spinner';
+import { IconButtonRef, IconButtonProps } from './types';
+
+import { Theme } from '../../../theme/types';
+
+const IconButton = forwardRef<IconButtonRef, IconButtonProps>(function IconButton(props, ref): ReactElement {
+	const theme = useTheme<Theme>();
+	const { colorMode: colorModeHook } = useColorMode();
+
+	const {
+		children,
+		color = 'gray',
+		colorMode: colorModeProp,
+		isDisabled = false,
+		isLoading = false,
+		size = 'md',
+		variant = 'contained',
+		sx,
+		...rest
+	} = props;
+
+	const colorMode: ColorMode = colorModeProp || colorModeHook;
+
+	const style = useStyles(theme, { color, colorMode, isLoading, size, variant });
+
+	return (
+		<CUIIconButton
+			{...rest}
+			ref={ref}
+			tabIndex={0}
+			isDisabled={isLoading || isDisabled}
+			variant='unstyled'
+			sx={merge(style.iconbutton, sx)}
+			_disabled={style.disabled}
+			_active={style.active}
+		>
+			{isLoading ? <Spinner color={color} colorMode={colorMode} size={size} variant={variant} /> : children}
+		</CUIIconButton>
+	);
+});
+
+export default IconButton;
---
+++ src/components/Clickable/IconButton/types.ts
@@ -0,0 +1,65 @@
+import { ReactNode } from 'react';
+
+import { ColorMode, IconButtonProps as CUIIconButtonProps } from '@chakra-ui/react';
+
+import {
+	BoxPadding,
+	BoxColor,
+	BoxGradient,
+	BoxTypography,
+	BoxLayout,
+	BoxFlexbox,
+	BoxGrid,
+	BoxBackground,
+	BoxBorders,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../../common/types/box';
+import { Color } from '../../../theme/types';
+
+export type IconButtonRef = HTMLButtonElement | null;
+
+export type IconButtonColor = Exclude<Color, 'transparent'>;
+
+export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type Variant = 'contained' | 'outlined' | 'icon';
+
+type Omitted =
+	// CUI Box Props
+	| BoxPadding
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxLayout
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// CUI Icon Button Props
+	| 'as'
+	| 'children'
+	| 'color'
+	| 'colorScheme'
+	| 'iconSpacing'
+	| 'leftIcon'
+	| 'loadingText'
+	| 'rightIcon'
+	| 'size'
+	| 'spinner'
+	| 'spinnerPlacement'
+	| 'variant';
+
+export type IconButtonProps = {
+	children: ReactNode;
+	color?: IconButtonColor;
+	colorMode?: ColorMode;
+	size?: Size;
+	variant?: Variant;
+} & Omit<CUIIconButtonProps, Omitted>;
