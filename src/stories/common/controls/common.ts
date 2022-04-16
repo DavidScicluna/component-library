ac2f28b1e2b3117cbc9f7d0e3fa9a55b5c1a5054
David Scicluna <dave_nostra@hotmail.com>
2022-04-16 21:51:33 +0200

    Feature: Common Story Controls

---
+++ src/stories/common/controls/common.ts
@@ -0,0 +1,15 @@
+const color = {
+	name: 'Color',
+	type: 'string',
+	// description: '',
+	control: 'color'
+};
+
+const backgroundColor = {
+	name: 'Background-Color',
+	type: 'string',
+	// description: '',
+	control: 'color'
+};
+
+export default { color, backgroundColor };
---
+++ src/stories/common/controls/index.ts
@@ -0,0 +1,4 @@
+import common from './common';
+import theme from './theme';
+
+export default { common, theme };
