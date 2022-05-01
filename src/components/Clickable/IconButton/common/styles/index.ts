9754c4102e66bdf63c9b9590ee229321386428e7
David Scicluna <dave_nostra@hotmail.com>
2022-05-01 20:52:14 +0200

    Fix: IconButton - Styling

--- src/components/Clickable/IconButton/common/styles/index.ts
+++ src/components/Clickable/IconButton/common/styles/index.ts
@@ -1,29 +1,28 @@
-import { merge } from 'lodash';
+import { memoize, merge } from 'lodash';
 
 import active from './active';
 import dark from './dark';
 import disabled from './disabled';
 import iconbutton from './iconbutton';
 import light from './light';
-import { IconButtonStyleProps } from './types';
+import { ButtonStyleProps, ButtonStyleReturn } from './types';
 
-import { Style } from '../../../../../common/types';
-import { Theme } from '../../../../../theme/types';
-
-type IconButtonStyle = {
-	iconbutton: Style;
-	active: Style;
-	disabled: Style;
-};
-
-export default (theme: Theme, props: IconButtonStyleProps): IconButtonStyle => {
-	const { color, colorMode, isLoading, isRound, size, variant } = props;
+export default memoize((props: ButtonStyleProps): ButtonStyleReturn => {
+	const { theme, color = 'gray', colorMode, isLoading = false, size = 'md', variant = 'contained' } = props;
 
 	const scheme = colorMode === 'light' ? light : dark;
 
 	return {
-		iconbutton: merge(iconbutton(theme, isRound, size, variant), scheme.iconbutton[variant](theme, color, size)),
-		active: merge(active(size, variant), scheme.active[variant](theme, color, size)),
-		disabled: merge(disabled(isLoading, size, variant), scheme.disabled[variant](theme, color, isLoading, size))
+		iconbutton: merge(
+			iconbutton.general({ theme, size }),
+			iconbutton[variant]({ theme, size }),
+			scheme.iconbutton[variant]({ theme, color, size })
+		),
+		active: merge(active[variant]({ size }), scheme.active[variant]({ theme, color, size })),
+		disabled: merge(
+			disabled.general({ isLoading, size }),
+			disabled[variant]({ isLoading, size }),
+			scheme.disabled[variant]({ theme, color, isLoading })
+		)
 	};
-};
+});
