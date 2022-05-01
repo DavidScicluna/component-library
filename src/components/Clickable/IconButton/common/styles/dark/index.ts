de976e0903783fdef0b9494928b2cc52cb4906b8
David Scicluna <dave_nostra@hotmail.com>
2022-05-01 20:51:09 +0200

    Fix: IconButton - Styling - Fix Light/Dark imports

--- src/components/Clickable/IconButton/common/styles/dark/index.ts
+++ src/components/Clickable/IconButton/common/styles/dark/index.ts
@@ -1,5 +1,5 @@
-import activeStyle from './active';
-import disabledStyle from './disabled';
-import iconbuttonStyle from './iconbutton';
+import active from './active';
+import disabled from './disabled';
+import iconbutton from './iconbutton';
 
-export default { iconbutton: iconbuttonStyle, active: activeStyle, disabled: disabledStyle };
+export default { active, disabled, iconbutton };
