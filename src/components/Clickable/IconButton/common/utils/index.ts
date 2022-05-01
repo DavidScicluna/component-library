dd9be2ac044eb1b6c464b8876b66c7ca499b6fee
David Scicluna <dave_nostra@hotmail.com>
2022-05-01 20:51:28 +0200

    Fix: IconButton - Utils

--- src/components/Clickable/IconButton/common/utils/index.ts
+++ src/components/Clickable/IconButton/common/utils/index.ts
@@ -1,13 +1,5 @@
-import { ColorMode } from '@chakra-ui/react';
-
-import { Color, ColorHues, Space } from '../../../../../theme/types';
+import { Space } from '../../../../../theme/types';
 import { Size, Variant } from '../../types';
-
-type Amounts = {
-	back: number;
-	hover: number;
-	active: number;
-};
 
 type Padding = {
 	x: Space; // In Space (Theme) Values
@@ -20,7 +12,7 @@ type Variants = {
 	icon: number; // In Pixels
 };
 
-type SizeStyle = {
+type GetSizeConfigReturn = {
 	padding: Padding;
 	border: number; // In Pixels
 	spacing: Space; // In Space (Theme) Values
@@ -28,78 +20,82 @@ type SizeStyle = {
 	offset: Variants;
 };
 
-export const handleSize = (size: Size): SizeStyle => {
+type GetSizeConfigProps = { size: Size };
+
+export const getSizeConfig = ({ size = 'md' }: GetSizeConfigProps): GetSizeConfigReturn => {
 	switch (size) {
 		case 'xs':
 			return {
-				padding: { x: 0.25, y: 0.25 },
+				padding: { x: 0.5, y: 0.25 },
 				border: 1,
 				spacing: 0.5,
-				transform: { contained: 2, outlined: 2, icon: 0 },
+				transform: { contained: 3, outlined: 3, icon: 0 },
 				offset: { contained: 2, outlined: 2, icon: 0 }
 			};
 		case 'sm':
 			return {
-				padding: { x: 0.5, y: 0.5 },
+				padding: { x: 1, y: 0.5 },
 				border: 1,
 				spacing: 1,
-				transform: { contained: 2, outlined: 2, icon: 0 },
+				transform: { contained: 3, outlined: 3, icon: 0 },
 				offset: { contained: 2, outlined: 2, icon: 0 }
 			};
 		case 'lg':
 			return {
-				padding: { x: 1.75, y: 1.5 },
-				border: 2,
-				spacing: 3,
-				transform: { contained: 4, outlined: 4, icon: 0 },
-				offset: { contained: 4, outlined: 4, icon: 0 }
-			};
-		case 'xl':
-			return {
-				padding: { x: 2.25, y: 2 },
-				border: 2,
-				spacing: 4,
-				transform: { contained: 4, outlined: 4, icon: 0 },
-				offset: { contained: 4, outlined: 4, icon: 0 }
-			};
-		default:
-			return {
-				padding: { x: 1.25, y: 1 },
+				padding: { x: 4, y: 2 },
 				border: 2,
 				spacing: 2,
 				transform: { contained: 4, outlined: 4, icon: 0 },
 				offset: { contained: 4, outlined: 4, icon: 0 }
 			};
+		case 'xl':
+			return {
+				padding: { x: 5, y: 3 },
+				border: 2,
+				spacing: 3,
+				transform: { contained: 4, outlined: 4, icon: 0 },
+				offset: { contained: 4, outlined: 4, icon: 0 }
+			};
+		default:
+			return {
+				padding: { x: 2, y: 1 },
+				border: 2,
+				spacing: 1,
+				transform: { contained: 4, outlined: 4, icon: 0 },
+				offset: { contained: 4, outlined: 4, icon: 0 }
+			};
 	}
 };
 
-export const handleHue = (colorMode: ColorMode, color: Color): ColorHues => {
-	if (colorMode === 'light') {
-		return color === 'gray' ? 400 : color === 'white' ? 100 : color === 'black' ? 800 : 500;
-	} else {
-		return color === 'gray' ? 500 : color === 'white' ? 100 : color === 'black' ? 800 : 400;
-	}
+type GetAmountReturn = {
+	back: number;
+	hover: number;
+	active: number;
 };
 
-export const handleAmount = (variant: Variant): Amounts => {
+type GetAmountProps = { variant: Variant };
+
+export const getAmount = ({ variant = 'contained' }: GetAmountProps): GetAmountReturn => {
 	return {
-		back: variant === 'contained' ? 0.1 : 0,
-		hover: 0.05,
+		back: variant === 'contained' ? 0.15 : 0,
+		hover: 0.1,
 		active: 0.15
 	};
 };
 
-export const handleFontSize = (size: Size): string => {
+type GetIconFontSize = { size: Size };
+
+export const getIconFontSize = ({ size = 'md' }: GetIconFontSize): string => {
 	switch (size) {
 		case 'xs':
 			return '14px'; // Height of theme xs font-size
 		case 'sm':
-			return '16px'; // Height of theme sm font-size
+			return '17px'; // Height of theme sm font-size
 		case 'lg':
-			return '21px'; // Height of theme lg font-size
+			return '23px'; // Height of theme lg font-size
 		case 'xl':
-			return '24px'; // Height of theme xl font-size
+			return '26px'; // Height of theme xl font-size
 		default:
-			return '19px'; // Height of theme md font-size
+			return '20px'; // Height of theme md font-size
 	}
 };
