594815d28bec7a2e8306b135752833f88d520e8e
David Scicluna <dave_nostra@hotmail.com>
2022-05-06 21:53:23 +0200

    Feature: Button - defaultPropValues

---
+++ src/components/Clickable/Button/common/data/defaultPropValues.ts
@@ -0,0 +1,15 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { ButtonColor, Size, Variant } from '../../types';
+
+export const color: ButtonColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isFullWidth = false;
+export const isLoading = false;
+
+export const size: Size = 'md';
+
+export const variant: Variant = 'contained';
