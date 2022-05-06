0d34d401cedd56c039214329531cf6b2ba033a27
David Scicluna <dave_nostra@hotmail.com>
2022-05-06 22:36:08 +0200

    Fix: Button - Dark Styling

--- src/components/Clickable/Button/common/styles/dark/index.ts
+++ src/components/Clickable/Button/common/styles/dark/index.ts
@@ -2,4 +2,4 @@ import active from './active';
 import button from './button';
 import disabled from './disabled';
 
-export default { button, active, disabled };
+export default { active, button, disabled };
