c759bc03cc5af0546c23c2613885b1375ef265ff
David Scicluna <dave_nostra@hotmail.com>
2022-05-06 22:20:49 +0200

    Fix: Button - Styling - DefaultPropValues

--- src/components/Clickable/Button/common/styles/index.ts
+++ src/components/Clickable/Button/common/styles/index.ts
@@ -7,15 +7,24 @@ import disabled from './disabled';
 import light from './light';
 import { ButtonStyleProps, ButtonStyleReturn } from './types';
 
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	isFullWidth as defaultIsFullWidth,
+	isLoading as defaultIsLoading,
+	size as defaultSize,
+	variant as defaultVariant
+} from '../data/defaultPropValues';
+
 export default memoize((props: ButtonStyleProps): ButtonStyleReturn => {
 	const {
 		theme,
-		color = 'gray',
-		colorMode,
-		isFullWidth = false,
-		isLoading = false,
-		size = 'md',
-		variant = 'contained'
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		isFullWidth = defaultIsFullWidth,
+		isLoading = defaultIsLoading,
+		size = defaultSize,
+		variant = defaultVariant
 	} = props;
 
 	const scheme = colorMode === 'light' ? light : dark;
@@ -26,10 +35,10 @@ export default memoize((props: ButtonStyleProps): ButtonStyleReturn => {
 			button[variant]({ theme, isFullWidth, size }),
 			scheme.button[variant]({ theme, color, size })
 		),
-		active: merge(active[variant]({ size }), scheme.active[variant]({ theme, color, size })),
+		active: merge(active[variant]({ theme, size }), scheme.active[variant]({ theme, color, size })),
 		disabled: merge(
-			disabled.general({ isLoading, size }),
-			disabled[variant]({ isLoading, size }),
+			disabled.general({ theme, isLoading, size }),
+			disabled[variant]({ theme, isLoading, size }),
 			scheme.disabled[variant]({ theme, color, isLoading })
 		)
 	};
