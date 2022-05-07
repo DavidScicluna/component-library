d8250e9249b06704aafafe403302aa763f9f95f6
David Scicluna <dave_nostra@hotmail.com>
2022-05-07 19:50:56 +0200

    Feature: IconButton - defaultPropValues

---
+++ src/components/Clickable/IconButton/common/data/defaultPropValues.ts
@@ -0,0 +1,15 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { IconButtonColor, Size, Variant } from '../../types';
+
+export const color: IconButtonColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isRound = false;
+export const isLoading = false;
+
+export const size: Size = 'md';
+
+export const variant: Variant = 'contained';
