153c18dbd1b3ed6fa3c0a7fcd9182a1a24977109
David Scicluna <dave_nostra@hotmail.com>
2022-05-08 15:51:44 +0200

    Fix: TabBar - Tab - Dark Disabled Styling

--- src/components/TabBar/components/Tab/common/styles/dark/disabled/index.ts
+++ src/components/TabBar/components/Tab/common/styles/dark/disabled/index.ts
@@ -1,12 +1,13 @@
+import { TabDarkDisabledStylingProps } from './types';
+
 import { Style } from '../../../../../../../../common/types';
 import { getHue } from '../../../../../../../../common/utils/color';
-import { Theme } from '../../../../../../../../theme/types';
 
-export default (theme: Theme): Style => {
-	const shade = getHue({ type: 'text.secondary', colorMode: 'dark' });
+export default ({ theme }: TabDarkDisabledStylingProps): Style => {
+	const shade = getHue({ colorMode: 'dark', type: 'text.secondary' });
 
 	return {
-		'& svg, .ds-cl-icon, ': {
+		'& svg, .ds-cl-icon': {
 			color: `${theme.colors.gray[shade]} !important`
 		}
 	};
---
+++ src/components/TabBar/components/Tab/common/styles/dark/disabled/types.ts
@@ -0,0 +1,5 @@
+import { TabStyleProps } from '../../types';
+
+type Picked = 'theme';
+
+export type TabDarkDisabledStylingProps = Pick<TabStyleProps, Picked>;
