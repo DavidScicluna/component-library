42ee7533dedb0ea7cf608b67c71d2a1af8d55d2b
David Scicluna <dave_nostra@hotmail.com>
2022-05-08 15:42:01 +0200

    Fix: TabBar - Tab - Style Types

--- src/components/TabBar/components/Tab/common/styles/types.ts
+++ src/components/TabBar/components/Tab/common/styles/types.ts
@@ -1,8 +1,14 @@
-import { ColorMode } from '@chakra-ui/react';
+import { Style } from '../../../../../../common/types';
+import { Theme } from '../../../../../../theme/types';
+import { TabProps } from '../../types';
 
-import { TabBarColor } from '../../../../types';
+type Picked = 'color' | 'colorMode' | 'isActive';
 
 export type TabStyleProps = {
-	color: TabBarColor;
-	colorMode: ColorMode;
+	theme: Theme;
+} & Pick<TabProps, Picked>;
+
+export type TabStyleReturn = {
+	tab: Style;
+	disabled: Style;
 };
