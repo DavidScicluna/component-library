a400368fd76a23aff300f651bdd07e033dd985ed
David Scicluna <dave_nostra@hotmail.com>
2022-05-08 17:47:48 +0200

    Fix: Card - Styles

--- src/components/Card/common/styles/index.ts
+++ src/components/Card/common/styles/index.ts
@@ -1,46 +1,46 @@
-import { merge } from 'lodash';
+import { memoize, merge } from 'lodash';
 
 import active from './active';
 import card from './card';
 import dark from './dark';
 import disabled from './disabled';
 import light from './light';
-import { CardStyleProps } from './types';
+import { CardStyleProps, CardStyleReturn } from './types';
 
-import { Style } from '../../../../common/types';
-import { Theme } from '../../../../theme/types';
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	isFullWidth as defaultIsFullWidth,
+	isLight as defaultIsLight,
+	isClickable as defaultIsClickable,
+	// isFixed as defaultIsFixed,
+	variant as defaultVariant
+} from '../data/defaultPropValues';
 
-type CardStyle = {
-	card: Style;
-	active: Style;
-	disabled: Style;
-};
-
-export default (theme: Theme, props: CardStyleProps): CardStyle => {
+export default memoize((props: CardStyleProps): CardStyleReturn => {
 	const {
-		color,
-		colorMode,
-		isFullWidth,
-		isLight,
-		isClickable,
-		// isFixed,
-		variant
+		theme,
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		isFullWidth = defaultIsFullWidth,
+		isLight = defaultIsLight,
+		isClickable = defaultIsClickable,
+		variant = defaultVariant
 	} = props;
 
 	const scheme = colorMode === 'light' ? light : dark;
 
 	return {
 		card: merge(
-			card(theme, isFullWidth, isClickable, variant),
-			variant !== 'transparent' ? scheme.card[variant](theme, color, isLight, isClickable) : {}
+			card.general({ theme, isFullWidth, size }),
+			card[variant]({ theme, isFullWidth, size }),
+			scheme.card[variant]({ theme, color, size })
 		),
-		active: merge(
-			active(isClickable, variant),
-			variant !== 'transparent' ? scheme.active[variant](theme, color, isLight, isClickable) : {}
-		),
+		active: merge(active[variant]({ theme, size }), scheme.active[variant]({ theme, color, size })),
 		disabled: merge(
-			disabled(isClickable, variant),
-			variant !== 'transparent' ? scheme.disabled[variant](theme, color, isLight, isClickable) : {}
+			disabled.general({ theme, isLoading, size }),
+			disabled[variant]({ theme, isLoading, size }),
+			scheme.disabled[variant]({ theme, color, isLoading, size })
 		)
 	};
-};
+});
