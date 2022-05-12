c605c03c9eb4642eae3585fa181611d75516a23c
David Scicluna <dave_nostra@hotmail.com>
2022-05-12 22:37:29 +0200

    Feature: Tooltip - Types

---
+++ src/components/Tooltip/types.ts
@@ -0,0 +1,70 @@
+import { ColorMode, TooltipProps as CUITooltipProps } from '@chakra-ui/react';
+
+import {
+	BoxMargin,
+	BoxPadding,
+	BoxColor,
+	BoxGradient,
+	BoxTypography,
+	BoxLayout,
+	BoxFlexbox,
+	BoxGrid,
+	BoxBackground,
+	BoxBorders,
+	BoxBorderRadius,
+	BoxPosition,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../common/types/box';
+import { Color } from '../../theme/types';
+
+export type TooltipRef = HTMLDivElement | null;
+
+export type TooltipColor = Exclude<Color, 'transparent'>;
+
+type Omitted =
+	// CUI Box Props
+	| BoxMargin
+	| BoxPadding
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxLayout
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxBorderRadius
+	| BoxPosition
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// CUI Button Props
+	| 'as'
+	| 'aria-label'
+	| 'arrowPadding'
+	| 'arrowShadowColor'
+	| 'arrowSize'
+	// | 'closeDelay'
+	| 'colorScheme'
+	| 'direction'
+	| 'hasArrow'
+	| 'isOpen'
+	| 'modifiers'
+	| 'offset'
+	// | 'openDelay'
+	| 'portalProps'
+	| 'size'
+	| 'variant';
+
+export type TooltipProps = {
+	'aria-label': string;
+	'color'?: TooltipColor;
+	'colorMode'?: ColorMode;
+	'isOpen': boolean;
+	// 'closeDelay'?: number;
+	// 'openDelay'?: number;
+} & Omit<CUITooltipProps, Omitted>;
