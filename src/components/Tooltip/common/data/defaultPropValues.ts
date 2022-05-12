165d75922dd7780b5409c3331be7037d9298f425
David Scicluna <dave_nostra@hotmail.com>
2022-05-12 22:37:22 +0200

    Feature: Tooltip - defaultPropValues

---
+++ src/components/Tooltip/common/data/defaultPropValues.ts
@@ -0,0 +1,7 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { TooltipColor } from '../../types';
+
+export const color: TooltipColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
