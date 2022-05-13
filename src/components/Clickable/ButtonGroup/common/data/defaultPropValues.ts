4cc6e3209e2434c9a2513717029b37c9b32860ad
David Scicluna <dave_nostra@hotmail.com>
2022-05-13 22:04:31 +0200

    Feature: ButtonGroup - defaultPropValues

---
+++ src/components/Clickable/ButtonGroup/common/data/defaultPropValues.ts
@@ -0,0 +1,10 @@
+import { Space } from '../../../../../theme/types';
+import { Size } from '../../types';
+
+export const children = [];
+
+export const isAttached = true;
+
+export const size: Size = 'md';
+
+export const spacing: Space = 0;
