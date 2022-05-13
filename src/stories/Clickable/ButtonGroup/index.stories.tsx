f01705a64bd6092b79628d19933f911bc96fb46f
David Scicluna <dave_nostra@hotmail.com>
2022-05-13 22:05:02 +0200

    Feature: ButtonGroup - Story

---
+++ src/stories/Clickable/ButtonGroup/index.stories.tsx
@@ -0,0 +1,58 @@
+import { ReactElement } from 'react';
+
+import { Box } from '@chakra-ui/react';
+
+import { Meta, Story } from './types';
+
+import ButtonGroupComponent from '../../../components/Clickable/ButtonGroup';
+import {
+	isAttached as defaultIsAttached,
+	size as defaultSize,
+	spacing as defaultSpacing
+} from '../../../components/Clickable/ButtonGroup/common/data/defaultPropValues';
+import { Size, ButtonGroupProps } from '../../../components/Clickable/ButtonGroup/types';
+import controls from '../../common/controls';
+
+const sizeOptions: Size[] = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+export default {
+	title: 'Clickable/Button Group',
+	component: ButtonGroupComponent,
+	argTypes: {
+		isAttached: {
+			name: 'Attached',
+			type: 'boolean',
+			defaultValue: defaultIsAttached,
+			// description: '',
+			control: 'boolean'
+		},
+		size: {
+			name: 'Size',
+			type: 'string',
+			defaultValue: defaultSize,
+			// description: '',
+			options: [...sizeOptions],
+			control: 'select'
+		},
+		spacing: { ...controls.theme.spacing, defaultValue: defaultSpacing }
+	}
+	// parameters: {
+	// 	backgrounds: {
+	// 		default: 'twitter',
+	// 		values: [
+	// 			{ name: 'twitter', value: '#00aced' },
+	// 			{ name: 'facebook', value: '#3b5998' }
+	// 		]
+	// 	}
+	// }
+} as Meta;
+
+export const ButtonGroup: Story = (props: ButtonGroupProps): ReactElement => (
+	<ButtonGroupComponent {...props}>
+		<Box backgroundColor='green.200' p={3} />
+		<Box backgroundColor='green.300' p={3} />
+		<Box backgroundColor='green.400' p={3} />
+		<Box backgroundColor='green.500' p={3} />
+		<Box backgroundColor='green.600' p={3} />
+	</ButtonGroupComponent>
+);
---
+++ src/stories/Clickable/ButtonGroup/types.ts
@@ -0,0 +1,7 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import ButtonGroup from '../../../components/Clickable/ButtonGroup';
+
+export type Meta = ComponentMeta<typeof ButtonGroup>;
+
+export type Story = ComponentStory<typeof ButtonGroup>;
