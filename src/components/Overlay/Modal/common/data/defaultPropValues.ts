e0cae20f5b5c066d89198e44d18ba927ee1e1e1f
David Scicluna <dave_nostra@hotmail.com>
2022-05-14 19:02:28 +0200

    Feature: Modal - defaultPropValues

---
+++ src/components/Overlay/Modal/common/data/defaultPropValues.ts
@@ -0,0 +1,12 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Space } from '../../../../../theme/types';
+import { ModalSize } from '../../types';
+
+export const colorMode: ColorMode = 'light';
+
+export const isOpen = false;
+
+export const size: ModalSize = 'xl';
+
+export const spacing: Space = 2;
