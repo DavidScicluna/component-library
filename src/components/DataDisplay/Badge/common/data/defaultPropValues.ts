c5ceaf061b58d6235032663d11dca1ebe72c6166
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 16:10:58 +0200

    Feature: Badge - defaultPropValues

---
+++ src/components/DataDisplay/Badge/common/data/defaultPropValues.ts
@@ -0,0 +1,13 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { BadgeColor, BadgeSize, BadgeVariant } from '../../types';
+
+export const color: BadgeColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isLight = false;
+
+export const size: BadgeSize = 'xl';
+
+export const variant: BadgeVariant = 'contained';
