93a230529ba5726d49661100fb9bd9e9d3e93a16
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 12:09:44 +0200

    Feature: Tag - TagDeleteButton

---
+++ src/components/Clickable/Tag/components/TagDeleteButton/index.tsx
@@ -0,0 +1,47 @@
+import { FC, useContext } from 'react';
+
+import { TagDeleteIconButtonProps, TagEvent } from './types';
+
+import { TagContext } from '../..';
+import Icon from '../../../../Icon';
+import IconButton from '../../../IconButton';
+import {
+	colorMode as defaultColorMode,
+	size as defaultSize,
+	variant as defaultVariant
+} from '../../common/data/defaultPropValues';
+import { TagContext as TagContextType } from '../../types';
+
+const TagDeleteIconButton: FC<TagDeleteIconButtonProps> = (props) => {
+	const {
+		colorMode = defaultColorMode,
+		isDisabled: isDisabledContext,
+		size = defaultSize,
+		variant = defaultVariant
+	} = useContext<TagContextType>(TagContext);
+
+	const { isDisabled: isDisabledProp = false, onDelete, ...rest } = props;
+
+	return (
+		<IconButton
+			{...rest}
+			aria-label='Delete Tag'
+			color={variant === 'contained' ? (colorMode === 'light' ? 'white' : 'black') : 'gray'}
+			colorMode={colorMode}
+			isDisabled={isDisabledContext || isDisabledProp}
+			onClick={(event: TagEvent) => {
+				event.preventDefault();
+				event.stopPropagation();
+
+				onDelete(event);
+			}}
+			size={size}
+			variant='icon'
+			sx={{ p: 0.5 }}
+		>
+			<Icon icon='clear' />
+		</IconButton>
+	);
+};
+
+export default TagDeleteIconButton;
---
+++ src/components/Clickable/Tag/components/TagDeleteButton/types.ts
@@ -0,0 +1,11 @@
+import { MouseEvent } from 'react';
+
+import { IconButtonProps } from '../../../IconButton/types';
+
+export type TagEvent = MouseEvent<HTMLButtonElement, globalThis.MouseEvent>;
+
+type Omitted = 'aria-label' | 'children' | 'colorMode' | 'onClick' | 'size' | 'variant';
+
+export type TagDeleteIconButtonProps = {
+	onDelete: (event: TagEvent) => void;
+} & Omit<IconButtonProps, Omitted>;
