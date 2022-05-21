9072b7d2fa2cb53e6b6ccc930ad4ca425a9099b8
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 22:24:12 +0200

    Feature: FormHelperText - defaultPropValues

---
+++ src/components/Forms/FormHelperText/common/data/defaultPropValues.ts
@@ -0,0 +1,13 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { FormHelperTextSize } from '../../types';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isError = false;
+export const isWarning = false;
+export const isSuccess = false;
+export const isReadOnly = false;
+
+export const size: FormHelperTextSize = 'md';
