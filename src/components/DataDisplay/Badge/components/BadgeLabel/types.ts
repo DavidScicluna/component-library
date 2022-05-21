e3ace1400c404788396b4a3b8635ed5c3a4f21a5
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 16:23:08 +0200

    Fix: Badge - BadgeLabel - Types

--- src/components/DataDisplay/Badge/components/BadgeLabel/types.ts
+++ src/components/DataDisplay/Badge/components/BadgeLabel/types.ts
@@ -2,6 +2,44 @@ import { ReactNode } from 'react';
 
 import { CenterProps } from '@chakra-ui/react';
 
+import {
+	BoxMargin,
+	BoxPadding,
+	BoxColor,
+	BoxGradient,
+	BoxLayout,
+	BoxFlexbox,
+	BoxGrid,
+	BoxBackground,
+	BoxBorders,
+	BoxBorderRadius,
+	BoxPosition,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../../../../common/types/box';
+
+type Omitted =
+	// CUI Box Props
+	| BoxMargin
+	| BoxPadding
+	| BoxColor
+	| BoxGradient
+	| BoxLayout
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxBorderRadius
+	| BoxPosition
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// CUI Center Props
+	| 'children';
+
 export type BadgeLabelProps = {
 	children: ReactNode;
-} & Omit<CenterProps, 'children'>;
+} & Omit<CenterProps, Omitted>;
