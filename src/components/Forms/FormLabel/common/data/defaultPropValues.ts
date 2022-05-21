15c25e01152e6f92306f1f882254d57494f65fc0
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 21:26:18 +0200

    Feature: FormLabel - defaultPropValues

---
+++ src/components/Forms/FormLabel/common/data/defaultPropValues.ts
@@ -0,0 +1,10 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { FormLabelSize } from '../../types';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isReadOnly = false;
+
+export const size: FormLabelSize = 'md';
