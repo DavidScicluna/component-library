0574b78738ca790a8fcbd3934eb145bb9eb57e36
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 22:24:40 +0200

    Feature: FormHelperText - Utils

---
+++ src/components/Forms/FormHelperText/common/utils/index.ts
@@ -0,0 +1,22 @@
+import { memoize } from 'lodash';
+
+import { FontSize } from '../../../../../theme/types';
+import { FormHelperTextSize } from '../../types';
+import { size as defaultSize } from '../data/defaultPropValues';
+
+type GetFontSizeProps = { size: FormHelperTextSize };
+
+export const getFontSize = memoize(({ size = defaultSize }: GetFontSizeProps): FontSize => {
+	switch (size) {
+		case 'xs':
+			return 'xs';
+		case 'sm':
+			return 'sm';
+		case 'lg':
+			return 'lg';
+		case 'xl':
+			return 'xl';
+		default:
+			return 'md';
+	}
+});
