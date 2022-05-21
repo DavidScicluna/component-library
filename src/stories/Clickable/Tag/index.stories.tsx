26b55cecebd28cbf13f860acb7771f0e3eb522d7
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 12:10:13 +0200

    Feature: Tag - Story

---
+++ src/stories/Clickable/Tag/index.stories.tsx
@@ -0,0 +1,115 @@
+import { ReactElement } from 'react';
+
+import { Meta, Story } from './types';
+
+import { TagColor, TagProps, TagVariant, Tag as TagComponent, TagDeleteButton, TagLabel } from '../../..';
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	isActive as defaultIsActive,
+	isFullWidth as defaultIsFullWidth,
+	isClickable as defaultIsClickable,
+	// isFixed as defaultIsFixed,
+	isDisabled as defaultIsDisabled,
+	variant as defaultVariant
+} from '../../../components/Clickable/Tag/common/data/defaultPropValues';
+import controls from '../../common/controls';
+
+const colorOptions: TagColor[] = [
+	'black',
+	'white',
+	'gray',
+	'red',
+	'pink',
+	'purple',
+	'deep_purple',
+	'indigo',
+	'blue',
+	'light_blue',
+	'cyan',
+	'teal',
+	'green',
+	'light_green',
+	'lime',
+	'yellow',
+	'orange',
+	'deep_orange'
+];
+
+const variantOptions: TagVariant[] = ['contained', 'outlined', 'text'];
+
+export default {
+	title: 'Tag',
+	component: TagComponent,
+	argTypes: {
+		color: {
+			name: 'Color',
+			type: 'string',
+			defaultValue: defaultColor,
+			// description: '',
+			options: [...colorOptions],
+			control: 'select'
+		},
+		colorMode: { ...controls.theme.colorMode, defaultValue: defaultColorMode },
+		is: {
+			name: 'Divisible',
+			type: 'boolean',
+			defaultValue: defaultIsActive,
+			// description: '',
+			control: 'boolean'
+		},
+		isDisabled: {
+			name: 'Disabled',
+			type: 'boolean',
+			defaultValue: defaultIsDisabled,
+			// description: '',
+			control: 'boolean'
+		},
+		isFullWidth: {
+			name: 'FullWidth',
+			type: 'boolean',
+			defaultValue: defaultIsFullWidth,
+			// description: '',
+			control: 'boolean'
+		},
+		isClickable: {
+			name: 'Clickable',
+			type: 'boolean',
+			defaultValue: defaultIsClickable,
+			// description: '',
+			control: 'boolean'
+		},
+		isActive: {
+			name: 'Active',
+			type: 'boolean',
+			defaultValue: false,
+			// description: '',
+			control: 'boolean'
+		},
+		margin: { ...controls.theme.margin },
+		variant: {
+			name: 'Variant',
+			type: 'string',
+			defaultValue: defaultVariant,
+			// description: '',
+			options: [...variantOptions],
+			control: 'select'
+		}
+	}
+	// parameters: {
+	// 	backgrounds: {
+	// 		default: 'twitter',
+	// 		values: [
+	// 			{ name: 'twitter', value: '#00aced' },
+	// 			{ name: 'facebook', value: '#3b5998' }
+	// 		]
+	// 	}
+	// }
+} as Meta;
+
+export const Tag: Story = (props: TagProps): ReactElement => (
+	<TagComponent {...props}>
+		<TagLabel>Hello I am a Tag</TagLabel>
+		<TagDeleteButton onDelete={() => console.log('Deleted')} />
+	</TagComponent>
+);
---
+++ src/stories/Clickable/Tag/types.ts
@@ -0,0 +1,7 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Tag from '../../../components/Clickable/Tag';
+
+export type Meta = ComponentMeta<typeof Tag>;
+
+export type Story = ComponentStory<typeof Tag>;
