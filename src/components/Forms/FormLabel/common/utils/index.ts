8c02dca431ab07ba693306b05493ccfd87fae458
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 21:26:56 +0200

    Feature: FormLabel - Utils

---
+++ src/components/Forms/FormLabel/common/utils/index.ts
@@ -0,0 +1,22 @@
+import { memoize } from 'lodash';
+
+import { FontSize } from '../../../../../theme/types';
+import { FormLabelSize } from '../../types';
+import { size as defaultSize } from '../data/defaultPropValues';
+
+type GetFontSizeProps = { size: FormLabelSize };
+
+export const getFontSize = memoize(({ size = defaultSize }: GetFontSizeProps): FontSize => {
+	switch (size) {
+		case 'xs':
+			return 'sm';
+		case 'sm':
+			return 'md';
+		case 'lg':
+			return 'xl';
+		case 'xl':
+			return '2xl';
+		default:
+			return 'lg';
+	}
+});
