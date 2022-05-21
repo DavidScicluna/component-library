6be8d9359cfd2b56e061a7f12650264c54e65f78
David Scicluna <dave_nostra@hotmail.com>
2022-05-21 21:27:24 +0200

    Feature: Input - defaultPropValues

---
+++ src/components/Forms/Input/common/data/defaultPropValues.ts
@@ -0,0 +1,18 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { AutoComplete, InputSize } from '../../types';
+
+export const autoComplete: AutoComplete = 'off';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isError = false;
+export const isWarning = false;
+export const isSuccess = false;
+export const isFocused = false;
+export const isReadOnly = false;
+export const isRequired = false;
+export const isFullWidth = true;
+
+export const size: InputSize = 'md';
