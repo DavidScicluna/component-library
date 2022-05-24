46e077642269264c4c75bff8f8e73d7a6eafeb1d
David Scicluna <dave_nostra@hotmail.com>
2022-05-24 18:56:32 +0200

    Fix: Input - Utils

--- src/components/Forms/Input/common/utils/index.ts
+++ src/components/Forms/Input/common/utils/index.ts
@@ -23,40 +23,40 @@ export const getSizeConfig = memoize(({ size = defaultSize }: GetSizeConfigProps
 	switch (size) {
 		case 'xs':
 			return {
-				height: 30,
-				padding: { x: 1, y: 0 },
+				height: 34,
+				padding: { x: 1, y: 0.5 },
 				border: 1,
 				spacing: 1,
 				offset: 2
 			};
 		case 'sm':
 			return {
-				height: 36,
-				padding: { x: 1.5, y: 0 },
+				height: 42,
+				padding: { x: 1.5, y: 0.75 },
 				border: 1,
 				spacing: 1.5,
 				offset: 2
 			};
 		case 'lg':
 			return {
-				height: 50,
-				padding: { x: 2.5, y: 0 },
+				height: 60,
+				padding: { x: 2.5, y: 1.25 },
 				border: 2,
 				spacing: 2.5,
 				offset: 4
 			};
 		case 'xl':
 			return {
-				height: 60,
-				padding: { x: 3, y: 0 },
+				height: 72,
+				padding: { x: 3, y: 1.5 },
 				border: 2,
 				spacing: 3,
 				offset: 4
 			};
 		default:
 			return {
-				height: 42,
-				padding: { x: 2, y: 0 },
+				height: 50,
+				padding: { x: 2, y: 1 },
 				border: 2,
 				spacing: 2,
 				offset: 4
@@ -68,12 +68,16 @@ type GetAmountReturn = {
 	back: number;
 	hover: number;
 	active: number;
+	selection: number;
+	placeholder: number;
 };
 
 export const getAmount = (): GetAmountReturn => {
 	return {
 		back: 0.15,
 		hover: 0.05,
-		active: 0.1
+		active: 0.1,
+		selection: 0,
+		placeholder: 0.25
 	};
 };
