edf00453fce1d7b7e11d3b6d67d7411aefcdf166
David Scicluna <dave_nostra@hotmail.com>
2022-05-27 22:03:21 +0200

    Feature: Checkbox - defaultPropValues

---
+++ src/components/Forms/Checkbox/common/data/defaultPropValues.ts
@@ -0,0 +1,20 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { CheckboxSize, CheckboxVariant } from '../../types';
+
+export const colorMode: ColorMode = 'light';
+
+export const isChecked = false;
+export const isIndeterminate = false;
+
+export const isDisabled = false;
+export const isError = false;
+export const isWarning = false;
+export const isSuccess = false;
+export const isReadOnly = false;
+export const isRequired = false;
+export const isFullWidth = false;
+
+export const size: CheckboxSize = 'md';
+
+export const variant: CheckboxVariant = 'outlined';
