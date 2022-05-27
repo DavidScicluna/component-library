0ee26d5178140ec00c3807d2171b3a4dd00353ba
David Scicluna <dave_nostra@hotmail.com>
2022-05-27 23:57:57 +0200

    Feature: Radio - Styling

---
+++ src/components/Forms/Radio/common/styles/index.ts
@@ -0,0 +1,52 @@
+import { memoize, merge } from 'lodash';
+
+import dark from './dark';
+import disabled from './disabled';
+import group from './group';
+import light from './light';
+import readOnly from './readOnly';
+import { RadioStyleProps, RadioStyleReturn } from './types';
+
+import {
+	colorMode as defaultColorMode,
+	isChecked as defaultIsChecked,
+	isError as defaultIsError,
+	isSuccess as defaultIsSuccess,
+	isWarning as defaultIsWarning,
+	isFullWidth as defaultIsFullWidth,
+	size as defaultSize,
+	variant as defaultVariant
+} from '../data/defaultPropValues';
+
+export default memoize((props: RadioStyleProps): RadioStyleReturn => {
+	const {
+		theme,
+		color,
+		colorMode = defaultColorMode,
+		isChecked = defaultIsChecked,
+		isError = defaultIsError,
+		isSuccess = defaultIsSuccess,
+		isWarning = defaultIsWarning,
+		isFullWidth = defaultIsFullWidth,
+		size = defaultSize,
+		variant = defaultVariant
+	} = props;
+
+	const scheme = colorMode === 'light' ? light : dark;
+
+	return {
+		group: merge(
+			group({ theme, isFullWidth, size }),
+			scheme.group[variant]({ theme, isError, isSuccess, isWarning })
+		),
+		checked: scheme.checked[variant]({
+			theme,
+			color,
+			isError,
+			isSuccess,
+			isWarning
+		}),
+		disabled: merge(disabled(), scheme.disabled[variant]({ theme })),
+		readOnly: merge(readOnly(), scheme.readOnly[variant]({ theme, isChecked }))
+	};
+});
