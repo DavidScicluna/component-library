21acc6a317b793e71fd6d552daded0f1a733eb55
David Scicluna <dave_nostra@hotmail.com>
2022-05-28 00:09:31 +0200

    Feature: Utils - NoUndefinedField

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -3,6 +3,8 @@ import { CSSObject } from '@chakra-ui/react';
 // Utility Types
 export type NonNullable<T> = Exclude<T, null | undefined>; // Remove null and undefined from Type
 
+export type NoUndefinedField<T> = { [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>> }; // Remove all null and undefined from Type
+
 // Common Component Types
 export type Style = CSSObject;
 
