608a9fb6d714fd109e028784dd7bda8168381689
David Scicluna <dave_nostra@hotmail.com>
2022-05-28 00:09:54 +0200

    Fix: Utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -5,10 +5,10 @@ import { memoize } from 'lodash';
  *
  * @returns boolean: Either its a touch device or not
  */
-export const checkIsTouchDevice = memoize((): boolean => {
+export const checkIsTouchDevice = (): boolean => {
 	return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 	// || navigator.msMaxTouchPoints > 0;
-});
+};
 
 /**
  * This method will convert a REM size to PX size
