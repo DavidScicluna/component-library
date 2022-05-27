d5f708018208cc6c366015aac5b41022718e2f85
David Scicluna <dave_nostra@hotmail.com>
2022-05-27 22:07:07 +0200

    Feature: Checkbox

---
+++ src/components/Forms/Checkbox/index.tsx
@@ -0,0 +1,178 @@
+import { FC, createContext, useRef, useCallback } from 'react';
+
+import {
+	ColorMode,
+	useColorMode,
+	FormControl,
+	Checkbox as CUICheckbox,
+	VStack,
+	HStack,
+	Center,
+	Collapse
+} from '@chakra-ui/react';
+
+import { isEmpty, isNil, merge } from 'lodash';
+
+import {
+	colorMode as defaultColorMode,
+	isChecked as defaultIsChecked,
+	isIndeterminate as defaultIsIndeterminate,
+	isDisabled as defaultIsDisabled,
+	isError as defaultIsError,
+	isSuccess as defaultIsSuccess,
+	isWarning as defaultIsWarning,
+	isReadOnly as defaultIsReadOnly,
+	isRequired as defaultIsRequired,
+	isFullWidth as defaultIsFullWidth,
+	size as defaultSize,
+	variant as defaultVariant
+} from './common/data/defaultPropValues';
+import useStyles from './common/styles';
+import { getSizeConfig } from './common/utils';
+import { CheckboxContext as CheckboxContextType, CheckboxProps, CheckboxRef, CheckboxPanelRenderProps } from './types';
+
+import { useTheme } from '../../../common/hooks';
+import Icon from '../../Icon';
+import FormHelperText from '../FormHelperText';
+import FormLabel from '../FormLabel';
+
+export const CheckboxContext = createContext<CheckboxContextType>({ colorMode: defaultColorMode });
+
+const Checkbox: FC<CheckboxProps> = (props) => {
+	const theme = useTheme();
+	const { colorMode: colorModeHook } = useColorMode();
+
+	const checkboxRef = useRef<CheckboxRef>(null);
+
+	const {
+		color,
+		colorMode: colorModeProp,
+		id,
+		name,
+		label,
+		helper,
+		defaultChecked = defaultIsChecked,
+		isChecked: isCheckedProp = defaultIsChecked,
+		isIndeterminate = defaultIsIndeterminate,
+		isDisabled = defaultIsDisabled,
+		isError = defaultIsError,
+		isWarning = defaultIsWarning,
+		isSuccess = defaultIsSuccess,
+		isReadOnly = defaultIsReadOnly,
+		isRequired = defaultIsRequired,
+		isFullWidth = defaultIsFullWidth,
+		icon = <Icon icon={isIndeterminate ? 'remove' : 'check'} />,
+		renderLeftPanel,
+		renderRightPanel,
+		size = defaultSize,
+		variant = defaultVariant,
+		sx,
+		...rest
+	} = props;
+
+	const colorMode: ColorMode = colorModeProp || colorModeHook;
+
+	const isChecked: boolean = defaultChecked || isIndeterminate || isCheckedProp;
+
+	const style = useStyles({
+		theme,
+		color,
+		colorMode,
+		isChecked,
+		isError,
+		isWarning,
+		isSuccess,
+		isFullWidth,
+		size,
+		variant
+	});
+
+	const handleReturnSpacing = useCallback((): number => getSizeConfig({ size }).spacing, [size, getSizeConfig]);
+
+	const handleClick = useCallback((): void => {
+		if (checkboxRef && checkboxRef.current) {
+			checkboxRef.current.focus();
+		}
+	}, [checkboxRef]);
+
+	const renderPanelProps: CheckboxPanelRenderProps = {
+		// width: handleReturnPanelSize(),
+		// height: handleReturnPanelSize(),
+		// fontSize: size,
+		color,
+		colorMode
+	};
+
+	return (
+		<CheckboxContext.Provider value={{ color, colorMode }}>
+			<VStack
+				as={FormControl}
+				tabIndex={0}
+				alignItems='flex-start'
+				onClick={handleClick}
+				sx={{ width: isFullWidth ? '100%' : 'auto' }}
+			>
+				{label && (
+					<FormLabel
+						colorMode={colorMode}
+						id={id || name}
+						isDisabled={isDisabled}
+						isRequired={isRequired}
+						isReadOnly={isReadOnly}
+						size={size}
+						sx={sx?.formLabel}
+					>
+						{label}
+					</FormLabel>
+				)}
+
+				<HStack
+					aria-checked={isChecked}
+					aria-disabled={isDisabled}
+					aria-invalid={isError}
+					aria-readonly={isReadOnly}
+					spacing={handleReturnSpacing()}
+					sx={merge(style.group, sx?.group || {})}
+					_checked={style.checked}
+					_disabled={style.disabled}
+					_readOnly={style.readOnly}
+				>
+					{renderLeftPanel && <Center flex={1}>{renderLeftPanel({ ...renderPanelProps })}</Center>}
+					<CUICheckbox
+						{...rest}
+						ref={checkboxRef}
+						icon={icon}
+						isChecked={isChecked}
+						isIndeterminate={isIndeterminate}
+						isDisabled={isDisabled}
+						isRequired={isRequired}
+						isInvalid={isError}
+						isReadOnly={isReadOnly}
+						id={id || name}
+						name={name}
+						variant='unstyled'
+						sx={merge(style.checkbox, sx?.checkbox || {})}
+					/>
+					{renderRightPanel && <Center flex={1}>{renderRightPanel({ ...renderPanelProps })}</Center>}
+				</HStack>
+
+				<Collapse in={!(isNil(helper) || isEmpty(helper))} unmountOnExit style={{ width: '100%' }}>
+					<FormHelperText
+						colorMode={colorMode}
+						isDisabled={isDisabled}
+						isError={isError}
+						isWarning={isWarning}
+						isSuccess={isSuccess}
+						isReadOnly={isReadOnly}
+						size={size}
+						sx={sx?.formHelperText || {}}
+					>
+						{helper}
+					</FormHelperText>
+				</Collapse>
+			</VStack>
+		</CheckboxContext.Provider>
+	);
+};
+
+export default Checkbox;
---
+++ src/components/Forms/Checkbox/types.ts
@@ -0,0 +1,86 @@
+import { FocusEvent, ReactNode } from 'react';
+
+import { ColorMode, CheckboxProps as CUICheckboxProps } from '@chakra-ui/react';
+
+import { Style } from '../../../common/types';
+import {
+	BoxPadding,
+	BoxColor,
+	BoxGradient,
+	BoxTypography,
+	BoxBackground,
+	BoxBorders,
+	BoxBorderRadius,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../../common/types/box';
+import { Color } from '../../../theme/types';
+
+export type Event = FocusEvent<HTMLInputElement, Element>;
+
+export type CheckboxRef = HTMLInputElement | null;
+
+export type CheckboxColor = Exclude<Color, 'transparent' | 'black' | 'white' | 'gray' | 'red' | 'yellow' | 'green'>;
+
+export type CheckboxSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type CheckboxVariant = 'outlined' | 'transparent';
+
+export type CheckboxPanelRenderProps = {
+	// width: number;
+	// height: number;
+	// fontSize: CheckboxSize;
+	color: CheckboxColor;
+	colorMode?: ColorMode;
+};
+
+export type CheckboxContext = {
+	color?: CheckboxColor;
+	colorMode?: ColorMode;
+};
+
+type Omitted =
+	// CUI Box Props
+	| BoxPadding
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxBackground
+	| BoxBorders
+	| BoxBorderRadius
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// CUI Input Props
+	| 'as'
+	| 'children'
+	| 'colorScheme'
+	| 'iconColor'
+	| 'iconSize'
+	| 'isRequired'
+	| 'size'
+	| 'spacing'
+	| 'placeholder'
+	| 'tabIndex'
+	| 'variant'
+	| 'sx';
+
+export type CheckboxProps = {
+	color: CheckboxColor;
+	colorMode?: ColorMode;
+	label?: string;
+	helper?: string;
+	isError?: boolean;
+	isWarning?: boolean;
+	isSuccess?: boolean;
+	isRequired?: boolean;
+	isFullWidth?: boolean;
+	renderLeftPanel?: (props: CheckboxPanelRenderProps) => ReactNode;
+	renderRightPanel?: (props: CheckboxPanelRenderProps) => ReactNode;
+	size?: CheckboxSize;
+	variant?: CheckboxVariant;
+	sx?: { group?: Style; checkbox?: Style; formLabel?: Style; formHelperText?: Style };
+} & Omit<CUICheckboxProps, Omitted>;
