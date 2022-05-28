e28cf9a973a842d994fe8a8467f7eb0d827a4c45
David Scicluna <dave_nostra@hotmail.com>
2022-05-28 14:02:22 +0200

    Fix: Theme - Styles

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -3,14 +3,13 @@ import { Styles, mode } from '@chakra-ui/theme-tools';
 import transition from './foundations/transition';
 
 const styles: Styles = {
-	global: (props) => ({
+	global: (props: Styles['global']) => ({
 		'body': {
 			color: mode('gray.900', 'gray.50')(props),
 			bg: mode('gray.50', 'gray.900')(props)
 		},
 		'*, *::before, *::after': {
 			fontFamily: 'body',
-			lineHeight: 'normal',
 			transition: 'none',
 			transitionDuration: transition.duration.normal,
 			transitionTimingFunction: transition.easing['ease-in-out']
