32b88a0377668429bca49888a61d3c70043892de
David Scicluna <dave_nostra@hotmail.com>
2022-05-29 16:20:26 +0200

    Fix: ConfirmModal - ConfirmModalFooter

--- src/components/Overlay/ConfirmModal/components/ConfirmModalFooter/index.tsx
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalFooter/index.tsx
@@ -5,13 +5,14 @@ import { ModalFooter, HStack } from '@chakra-ui/react';
 import { ConfirmModalFooterProps } from './types';
 
 import { ConfirmModalContext } from '../..';
+import { spacing as defaultSpacing } from '../../common/data/defaultPropValues';
 import { ConfirmModalContext as ConfirmModalContextType } from '../../types';
 
 const ConfirmModalFooter: FC<ConfirmModalFooterProps> = ({ renderCancel, renderAction, ...rest }) => {
-	const { colorMode } = useContext<ConfirmModalContextType>(ConfirmModalContext);
+	const { colorMode, spacing = defaultSpacing } = useContext<ConfirmModalContextType>(ConfirmModalContext);
 
 	return (
-		<ModalFooter as={HStack} width='100%' justifyContent='space-between' p={0} m={0} {...rest}>
+		<ModalFooter as={HStack} width='100%' justifyContent='space-between' spacing={spacing} p={0} m={0} {...rest}>
 			{renderCancel &&
 				renderCancel({ color: 'gray', colorMode, isFullWidth: true, size: 'md', variant: 'outlined' })}
 
