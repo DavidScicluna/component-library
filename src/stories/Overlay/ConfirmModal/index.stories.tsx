816d844095746ac85be2d40a4d3e94a5519b7b6d
David Scicluna <dave_nostra@hotmail.com>
2022-05-29 16:21:21 +0200

    Feature: ConfirmModal - Story

---
+++ src/stories/Overlay/ConfirmModal/index.stories.tsx
@@ -0,0 +1,73 @@
+import { ReactElement } from 'react';
+
+import { Box } from '@chakra-ui/react';
+
+import { Meta, Story } from './types';
+
+import {
+	ConfirmModalProps,
+	ConfirmModal as ConfirmModalComponent,
+	ConfirmModalBody,
+	ConfirmModalFooter,
+	ConfirmModalSubtitle,
+	ConfirmModalTitle
+} from '../../..';
+import {
+	colorMode as defaultColorMode,
+	isOpen as defaultIsOpen,
+	size as defaultSize,
+	spacing as defaultSpacing
+} from '../../../components/Overlay/ConfirmModal/common/data/defaultPropValues';
+import { ConfirmModalSize } from '../../../components/Overlay/ConfirmModal/types';
+import controls from '../../common/controls';
+
+const sizeOptions: ConfirmModalSize[] = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+export default {
+	title: 'Overlay/Confirm Modal',
+	component: ConfirmModalComponent,
+	argTypes: {
+		colorMode: { ...controls.theme.colorMode, defaultValue: defaultColorMode },
+		isOpen: {
+			name: 'Open',
+			type: 'boolean',
+			defaultValue: defaultIsOpen,
+			// description: '',
+			control: 'boolean'
+		},
+		size: {
+			name: 'Size',
+			type: 'boolean',
+			defaultValue: defaultSize,
+			// description: '',
+			options: [...sizeOptions],
+			control: 'select'
+		},
+		spacing: { ...controls.theme.spacing, defaultValue: defaultSpacing }
+	}
+	// parameters: {
+	// 	backgrounds: {
+	// 		default: 'twitter',
+	// 		values: [
+	// 			{ name: 'twitter', value: '#00aced' },
+	// 			{ name: 'facebook', value: '#3b5998' }
+	// 		]
+	// 	}
+	// }
+} as Meta;
+
+export const ConfirmModal: Story = (props: ConfirmModalProps): ReactElement => (
+	<ConfirmModalComponent
+		{...props}
+		renderCancel={() => <Box width='30px' height='30px' backgroundColor='yellow.300' borderRadius='xs' />}
+	>
+		<ConfirmModalBody>
+			<ConfirmModalTitle align='center'>Confirm Modal Title</ConfirmModalTitle>
+			<ConfirmModalSubtitle align='center'>This is the Confirm Modal description!</ConfirmModalSubtitle>
+		</ConfirmModalBody>
+		<ConfirmModalFooter
+			renderCancel={() => <Box flex={1} height='42px' backgroundColor='red.500' borderRadius='base' />}
+			renderAction={() => <Box flex={1} height='42px' backgroundColor='red.600' borderRadius='base' />}
+		/>
+	</ConfirmModalComponent>
+);
---
+++ src/stories/Overlay/ConfirmModal/types.ts
@@ -0,0 +1,7 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import ConfirmModal from '../../../components/Overlay/ConfirmModal';
+
+export type Meta = ComponentMeta<typeof ConfirmModal>;
+
+export type Story = ComponentStory<typeof ConfirmModal>;
