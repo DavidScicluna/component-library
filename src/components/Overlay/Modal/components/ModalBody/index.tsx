882346e6268ed70993bda5157d94773a501a7856
David Scicluna <dave_nostra@hotmail.com>
2022-05-29 17:44:41 +0200

    Fix: Modal - ModalBody

--- src/components/Overlay/Modal/components/ModalBody/index.tsx
+++ src/components/Overlay/Modal/components/ModalBody/index.tsx
@@ -6,7 +6,7 @@ import { ModalBodyProps } from './types';
 
 const ModalBody: FC<ModalBodyProps> = ({ children, ...rest }) => {
 	return (
-		<CUIModalBody width='100%' px={2} py={0} m={0} {...rest}>
+		<CUIModalBody width='100%' p={0} m={0} {...rest}>
 			{children}
 		</CUIModalBody>
 	);
