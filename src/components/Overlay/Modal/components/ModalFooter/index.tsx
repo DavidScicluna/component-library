d0d27df5fcafb943a7eb54f605ef02cfddd6cd70
David Scicluna <dave_nostra@hotmail.com>
2022-05-29 17:44:47 +0200

    Fix: Modal - ModalFooter

--- src/components/Overlay/Modal/components/ModalFooter/index.tsx
+++ src/components/Overlay/Modal/components/ModalFooter/index.tsx
@@ -5,13 +5,14 @@ import { ModalFooter as CUIModalFooter, HStack } from '@chakra-ui/react';
 import { ModalFooterProps } from './types';
 
 import { ModalContext } from '../..';
+import { colorMode as defaultColorMode, spacing as defaultSpacing } from '../../common/data/defaultPropValues';
 import { ModalContext as ModalContextType } from '../../types';
 
 const ModalFooter: FC<ModalFooterProps> = ({ renderCancel, renderAction, ...rest }) => {
-	const { colorMode } = useContext<ModalContextType>(ModalContext);
+	const { colorMode = defaultColorMode, spacing = defaultSpacing } = useContext<ModalContextType>(ModalContext);
 
 	return (
-		<CUIModalFooter as={HStack} width='100%' justifyContent='space-between' px={2} pt={0} pb={2} {...rest}>
+		<CUIModalFooter as={HStack} width='100%' justifyContent='space-between' spacing={spacing} p={0} m={0} {...rest}>
 			{renderCancel && renderCancel({ color: 'gray', colorMode, size: 'md', variant: 'outlined' })}
 
 			{renderAction && renderAction({ color: 'gray', colorMode, size: 'md', variant: 'contained' })}
