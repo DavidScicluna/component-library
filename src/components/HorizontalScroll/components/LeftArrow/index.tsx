5a44897faf62f48be0b202311bfaa5798d3bb385
David Scicluna <dave_nostra@hotmail.com>
2022-06-07 20:49:47 +0200

    Feature: HorizontalScroll - LeftArrow

---
+++ src/components/HorizontalScroll/components/LeftArrow/index.tsx
@@ -0,0 +1,30 @@
+import { FC, useContext, useEffect } from 'react';
+
+import { useBoolean } from '@chakra-ui/react';
+
+import { VisibilityContext } from 'react-horizontal-scrolling-menu';
+
+import Arrow from '../Arrow';
+
+const LeftArrow: FC = () => {
+	const { isFirstItemVisible, scrollPrev, visibleItemsWithoutSeparators, initComplete } =
+		useContext(VisibilityContext);
+
+	const [isDisabled, setIsDisabled] = useBoolean(!initComplete || (initComplete && isFirstItemVisible));
+
+	useEffect(() => {
+		if (visibleItemsWithoutSeparators.length) {
+			if (isFirstItemVisible) {
+				setIsDisabled.on();
+			} else {
+				setIsDisabled.off();
+			}
+		}
+	}, [visibleItemsWithoutSeparators, isFirstItemVisible]);
+
+	return (
+		<Arrow aria-label='Left Arrow Button' direction='left' isDisabled={isDisabled} onClick={() => scrollPrev()} />
+	);
+};
+
+export default LeftArrow;
