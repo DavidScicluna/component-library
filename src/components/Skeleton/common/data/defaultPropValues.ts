0d4d04e99a7f494fbf9c02963d5a89c3f0606bdc
David Scicluna <dave_nostra@hotmail.com>
2022-06-11 23:23:36 +0200

    Feature: Skeleton - defaultPropValues

---
+++ src/components/Skeleton/common/data/defaultPropValues.ts
@@ -0,0 +1,12 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { SkeletonColor, SkeletonType } from '../../types';
+
+export const color: SkeletonColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isLoaded = false;
+export const isReversed = true;
+
+export const type: SkeletonType = 'rectangle';
