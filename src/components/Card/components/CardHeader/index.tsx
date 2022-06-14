38028550bd8854beb37b0487bcdd161143701dbc
David Scicluna <dave_nostra@hotmail.com>
2022-06-14 21:57:55 +0200

    Fix: Card - CardHeader

--- src/components/Card/components/CardHeader/index.tsx
+++ src/components/Card/components/CardHeader/index.tsx
@@ -9,19 +9,23 @@ import { CardHeaderProps } from './types';
 import { CardContext } from '../..';
 import { useTheme } from '../../../../common/hooks';
 import { convertREMToPixels, convertStringToNumber } from '../../../../common/utils';
-import { getHue } from '../../../../common/utils/color';
+import { getColor } from '../../../../common/utils/color';
+import { Space } from '../../../../theme/types';
+import { colorMode as defaultColorMode } from '../../common/data/defaultPropValues';
 import { CardContext as CardContextType } from '../../types';
 
 const CardHeader: FC<CardHeaderProps> = ({ renderTitle, renderSubtitle, actions, spacing = 2, ...rest }) => {
 	const theme = useTheme();
 
-	const { colorMode } = useContext<CardContextType>(CardContext);
+	const { colorMode = defaultColorMode } = useContext<CardContextType>(CardContext);
 
 	const [actionsRef, { width: actionsWidth }] = useElementSize();
 
-	const handleCalculateTextWidth = useCallback((): number => {
-		return actionsWidth + convertREMToPixels(convertStringToNumber(theme.space[spacing], 'rem'));
-	}, [theme.space, spacing, actionsWidth]);
+	const handleCalculateTextWidth = useCallback((): string => {
+		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing as Space], 'rem'));
+
+		return `calc(100% - ${actions ? actionsWidth + spacingWidth : 0}px)`;
+	}, [theme, spacing, actionsWidth]);
 
 	return (
 		<HStack
@@ -32,15 +36,11 @@ const CardHeader: FC<CardHeaderProps> = ({ renderTitle, renderSubtitle, actions,
 			{...rest}
 		>
 			{renderTitle && (
-				<VStack
-					width={`calc(100% - ${actions ? handleCalculateTextWidth() : 0}px)`}
-					alignItems='flex-start'
-					spacing={0.5}
-				>
+				<VStack width={handleCalculateTextWidth()} alignItems='flex-start' spacing={0.5}>
 					{/* Title */}
 					{renderTitle({
 						align: 'left',
-						color: `gray.${getHue({ colorMode, type: 'text.primary' })}`,
+						color: getColor({ theme, colorMode, type: 'text.primary' }),
 						fontSize: 'xl',
 						fontWeight: 'bold',
 						lineHeight: 'normal',
@@ -51,7 +51,7 @@ const CardHeader: FC<CardHeaderProps> = ({ renderTitle, renderSubtitle, actions,
 					{renderSubtitle &&
 						renderSubtitle({
 							align: 'left',
-							color: `gray.${getHue({ colorMode, type: 'text.secondary' })}`,
+							color: getColor({ theme, colorMode, type: 'text.secondary' }),
 							fontSize: 'sm',
 							lineHeight: 'normal',
 							noOfLines: 1
--- src/components/Card/components/CardHeader/types.ts
+++ src/components/Card/components/CardHeader/types.ts
@@ -2,13 +2,10 @@ import { ReactNode } from 'react';
 
 import { StackProps, TextProps } from '@chakra-ui/react';
 
-import { Space } from '../../../../theme/types';
-
-type Omitted = 'children' | 'spacing';
+type Omitted = 'children';
 
 export type CardHeaderProps = {
 	renderTitle?: (props: TextProps) => ReactNode;
 	renderSubtitle?: (props: TextProps) => ReactNode;
 	actions?: ReactNode;
-	spacing?: Space;
 } & Omit<StackProps, Omitted>;
