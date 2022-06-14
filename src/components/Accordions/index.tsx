d89b4e4bd7509c561ac8f2d021837785fc2e64a9
David Scicluna <dave_nostra@hotmail.com>
2022-06-14 22:36:37 +0200

    Feature: Accordions

---
+++ src/components/Accordions/index.tsx
@@ -0,0 +1,109 @@
+import { ReactElement, createContext, forwardRef } from 'react';
+
+import { ColorMode, useColorMode, VStack } from '@chakra-ui/react';
+
+import {
+	accordions as defaultAccordions,
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	isDisabled as defaultIsDisabled,
+	isFullWidth as defaultIsFullWidth,
+	spacing as defaultSpacing
+} from './common/data/defaultPropValues';
+import { AccordionsContext as AccordionsContextType, AccordionsRef, AccordionsProps } from './types';
+
+export const AccordionsContext = createContext<AccordionsContextType>({
+	accordions: defaultAccordions,
+	color: defaultColor,
+	colorMode: defaultColorMode,
+	isDisabled: defaultIsDisabled,
+	isFullWidth: defaultIsFullWidth,
+	spacing: defaultSpacing
+});
+
+const Accordions = forwardRef<AccordionsRef, AccordionsProps>(function Accordions(props, ref): ReactElement {
+	const { colorMode: colorModeHook } = useColorMode();
+
+	// const [openedAccordions, setOpenedAccordions] = useState<AccordionType<D>['id'][]>([]);
+
+	const {
+		children,
+		accordions = defaultAccordions,
+		color = defaultColor,
+		colorMode: colorModeProp,
+		isDisabled = defaultIsDisabled,
+		isFullWidth = defaultIsFullWidth,
+		spacing = defaultSpacing,
+		...rest
+	} = props;
+
+	const colorMode: ColorMode = colorModeProp || colorModeHook;
+
+	// /**
+	//  * This method will check whether the passed id is already opened, if so it will close it
+	//  * Else it will open it
+	//  *
+	//  * @param id string - ID of the Accordion
+	//  */
+	// const handleToggleAccordion = useCallback((id: AccordionType<D>['id']): void => {
+	// 	if (openedAccordions.some((accordion) => accordion === id)) {
+	// 		setOpenedAccordions(openedAccordions.filter((accordion) => accordion !== id));
+	// 	} else {
+	// 		setOpenedAccordions([...openedAccordions, id]);
+	// 	}
+	// }, []);
+
+	// /**
+	//  * This methodd will either open all or close all accordions
+	//  */
+	// const handleToggleAllAccordions = useCallback((): void => {
+	// 	if (accordions.length === openedAccordions.length) {
+	// 		setOpenedAccordions([]);
+	// 	} else {
+	// 		setOpenedAccordions(accordions.map((accordion) => accordion.id));
+	// 	}
+	// }, []);
+
+	return (
+		<AccordionsContext.Provider value={{ accordions, color, colorMode, isDisabled, isFullWidth, spacing }}>
+			<VStack ref={ref} width='100%' spacing={spacing} {...rest}>
+				{children}
+
+				{/* <QuickToggles
+				accordions={accordions.map((accordion) => {
+					return { id: accordion.id, title: accordion.title };
+				})}
+				openedPanels={openedAccordions.length}
+				color={color}
+				colorMode={colorMode}
+				isLoading={isLoading}
+				isDisabled={isError}
+				onToggleAccordion={(id: AccordionType<D>['id']) => setOpenedAccordions(uniq([...openedAccordions, id]))}
+				onToggleAllAccordions={handleToggleAllAccordions}
+			/>
+
+			<VStack width='100%' spacing={2}>
+				{accordions.map((accordion) => (
+					<Accordion
+						{...accordion}
+						key={accordion.id}
+						color={color}
+						colorMode={colorMode}
+						isOpen={openedAccordions.some((openedAccordion) => openedAccordion === accordion.id)}
+						isDisabled={isError || accordion.isDisabled}
+						isLoading={isLoading}
+						onToggle={() => handleToggleAccordion(accordion.id)}
+					>
+						{renderAccordion({
+							...accordion,
+							isOpen: openedAccordions.some((openedAccordion) => openedAccordion === accordion.id)
+						})}
+					</Accordion>
+				))}
+			</VStack> */}
+			</VStack>
+		</AccordionsContext.Provider>
+	);
+});
+
+export default Accordions;
---
+++ src/components/Accordions/types.ts
@@ -0,0 +1,67 @@
+import { ColorMode, StackProps } from '@chakra-ui/react';
+
+import {
+	BoxColor,
+	BoxGradient,
+	BoxTypography,
+	BoxFlexbox,
+	BoxGrid,
+	BoxBackground,
+	BoxBorders,
+	BoxBorderRadius,
+	BoxShadow,
+	BoxFilter,
+	BoxPseudo,
+	BoxOther
+} from '../../common/types/box';
+import { Color } from '../../theme/types';
+
+export type AccordionsRef = HTMLDivElement | null;
+
+export type AccordionsColor = Exclude<Color, 'transparent' | 'black' | 'white' | 'gray'>;
+
+// export type Total = {
+// 	prefix?: string;
+// 	number?: number;
+// 	suffix?: string;
+// };
+
+export type Accordion = {
+	id: string;
+	title: string;
+};
+
+// export type RenderProps<D> = {
+// 	isOpen: boolean;
+// } & Accordion<D>;
+
+type Omitted =
+	// CUI Box Props
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxBorderRadius
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	// CUI Stack Props
+	| 'as'
+	| 'colorScheme'
+	| 'direction';
+
+export type AccordionsProps = {
+	accordions: Accordion[];
+	color: AccordionsColor;
+	colorMode?: ColorMode;
+	isDisabled?: boolean;
+	isFullWidth?: boolean;
+} & Omit<StackProps, Omitted>;
+
+type Picked = 'accordions' | 'color' | 'colorMode' | 'isDisabled' | 'isFullWidth' | 'spacing';
+
+export type AccordionsContext = Pick<AccordionsProps, Picked>;
