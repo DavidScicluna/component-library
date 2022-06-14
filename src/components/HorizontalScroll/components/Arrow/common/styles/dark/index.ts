1b298bcb802c1066a64a4c9d6eb7d113d88e29b5
David Scicluna <dave_nostra@hotmail.com>
2022-06-14 21:57:17 +0200

    Fix: HorizontalScroll - Arrow - Light/Dark Gradient Styling

--- src/components/HorizontalScroll/components/Arrow/common/styles/dark/index.ts
+++ src/components/HorizontalScroll/components/Arrow/common/styles/dark/index.ts
@@ -1,4 +1,4 @@
-import { toRgba, transparentize } from 'color2k';
+import { transparentize } from 'color2k';
 
 import { ArrowDarkStyleProps } from './types';
 
@@ -6,21 +6,26 @@ import { Style } from '../../../../../../../common/types';
 
 export default ({ theme, direction }: ArrowDarkStyleProps): Style => ({
 	backgroundColor: theme.colors.gray[900],
+	background: `linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
+		theme.colors.gray[900],
+		0
+	)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`
 
-	background: [
-		toRgba(theme.colors.gray[900]),
-		`-moz-linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[900],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`,
-		`-webkit-linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[900],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`,
-		`linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[900],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`
-	],
-	filter: `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[900]}",endColorstr="${theme.colors.gray[900]}",GradientType=1)`
+	// background: [
+	// 	toRgba(theme.colors.gray[900]),
+	// 	`-moz-linear-gradient(${direction === 'left' ? 0 : 180}deg, ${transparentize(
+	// 		theme.colors.gray[900],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`,
+	// 	`-webkit-linear-gradient(${direction === 'left' ? 0 : 180}deg, ${transparentize(
+	// 		theme.colors.gray[900],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`,
+	// 	`linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
+	// 		theme.colors.gray[900],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[900], 1)} 100%)`
+	// ],
+	// filter: `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[900]}",
+	// endColorstr="${theme.colors.gray[900]}",GradientType=1)`
 });
--- src/components/HorizontalScroll/components/Arrow/common/styles/light/index.ts
+++ src/components/HorizontalScroll/components/Arrow/common/styles/light/index.ts
@@ -6,21 +6,26 @@ import { Style } from '../../../../../../../common/types';
 
 export default ({ theme, direction }: ArrowLightStyleProps): Style => ({
 	backgroundColor: theme.colors.gray[50],
+	background: `linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
+		theme.colors.gray[50],
+		0
+	)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`
 
-	background: [
-		toRgba(theme.colors.gray[50]),
-		`-moz-linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[50],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`,
-		`-webkit-linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[50],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`,
-		`linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
-			theme.colors.gray[50],
-			0
-		)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`
-	],
-	filter: `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[50]}",endColorstr="${theme.colors.gray[50]}",GradientType=1)`
+	// background: [
+	// 	toRgba(theme.colors.gray[50]),
+	// 	`-moz-linear-gradient(${direction === 'left' ? 0 : 180}deg, ${transparentize(
+	// 		theme.colors.gray[50],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`,
+	// 	`-webkit-linear-gradient(${direction === 'left' ? 0 : 180}deg, ${transparentize(
+	// 		theme.colors.gray[50],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`,
+	// 	`linear-gradient(${direction === 'left' ? 90 : 270}deg, ${transparentize(
+	// 		theme.colors.gray[50],
+	// 		0
+	// 	)} 25%, ${transparentize(theme.colors.gray[50], 1)} 100%)`
+	// ],
+	// filter: `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[50]}"
+	// ,endColorstr="${theme.colors.gray[50]}",GradientType=1)`
 });
