64ca7853a88afe44245aae305e4f7a9aeedd3676
David Scicluna <dave_nostra@hotmail.com>
2022-06-14 22:36:30 +0200

    Feature: Accordions - defaultPropValues

---
+++ src/components/Accordions/common/data/defaultPropValues.ts
@@ -0,0 +1,15 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Space } from '../../../../theme/types';
+import { Accordion, AccordionsColor } from '../../types';
+
+export const accordions: Accordion[] = [];
+
+export const color: AccordionsColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isFullWidth = true;
+
+export const spacing: Space = 2;
