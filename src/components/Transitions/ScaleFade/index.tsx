3bb93c7f6dd863178b1308827a13909a4dd3ffce
David Scicluna <dave_nostra@hotmail.com>
2022-06-18 15:51:04 +0200

    Fix: Transitions - Config

--- src/components/Transitions/ScaleFade/index.tsx
+++ src/components/Transitions/ScaleFade/index.tsx
@@ -7,15 +7,12 @@ import { Transition } from 'framer-motion';
 import { ScaleFadeProps } from './types';
 
 import { useTheme } from '../../../common/hooks';
-import { convertEasingsToArray, convertStringToNumber } from '../../../common/utils';
+import { getConfig } from '../common/utils';
 
 const ScaleFade: FC<ScaleFadeProps> = (props) => {
 	const theme = useTheme();
 
-	const duration = useConst<number>(convertStringToNumber(theme.transition.duration.normal, 'ms') / 1000);
-	const easings = useConst<number[]>(convertEasingsToArray({ easing: theme.transition.easing['ease-in-out'] }));
-
-	const config = useConst<Transition>({ duration, easings });
+	const config = useConst<Transition>(getConfig({ theme }));
 
 	const { children, transition, ...rest } = props;
 	const { enter = { ...config }, exit = { ...config } } = transition || {};
