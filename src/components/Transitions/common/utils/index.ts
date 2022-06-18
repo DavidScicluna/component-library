e9c3032869eecc3d714cf16d89f77f314f506338
David Scicluna <dave_nostra@hotmail.com>
2022-06-18 17:20:47 +0200

    Fix: Transitions - Utils - Added getDuration & getEasings utils

--- src/components/Transitions/common/utils/index.ts
+++ src/components/Transitions/common/utils/index.ts
@@ -1,14 +1,25 @@
 import { Transition } from 'framer-motion';
-import { memoize } from 'lodash';
 
 import { convertEasingsToArray, convertStringToNumber } from '../../../../common/utils';
-import { Theme } from '../../../../theme/types';
+import { Theme, Duration, Ease } from '../../../../theme/types';
+
+type GetDurationProps = { theme: Theme; duration?: Duration };
+
+export const getDuration = ({ theme, duration = 'normal' }: GetDurationProps): number => {
+	return convertStringToNumber(theme.transition.duration[duration], 'ms') / 1000;
+};
+
+type GetEasingsProps = { theme: Theme; easing?: Ease };
+
+export const getEasings = ({ theme, easing = 'ease-in-out' }: GetEasingsProps): number[] => {
+	return convertEasingsToArray({ easing: theme.transition.easing[easing] });
+};
 
 type GetConfigProps = { theme: Theme };
 
-export const getConfig = memoize(({ theme }: GetConfigProps): Transition => {
-	const duration: number = convertStringToNumber(theme.transition.duration.normal, 'ms') / 1000;
-	const easings: number[] = convertEasingsToArray({ easing: theme.transition.easing['ease-in-out'] });
+export const getConfig = ({ theme }: GetConfigProps): Partial<Transition> => {
+	const duration: number = getDuration({ theme });
+	const easings: number[] = getEasings({ theme });
 
 	return { duration, easings };
-});
+};
