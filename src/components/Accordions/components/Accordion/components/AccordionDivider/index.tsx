2c003af2c82e05126ae770d54e7bace62251f9f7
David Scicluna <dave_nostra@hotmail.com>
2022-06-18 17:23:52 +0200

    Fix: Accordions - Accordion - AccordionDivider

--- src/components/Accordions/components/Accordion/components/AccordionDivider/index.tsx
+++ src/components/Accordions/components/Accordion/components/AccordionDivider/index.tsx
@@ -5,24 +5,35 @@ import { AccordionsContext } from '../../../..';
 import { useTheme } from '../../../../../../common/hooks';
 import { getColor } from '../../../../../../common/utils/color';
 import Divider from '../../../../../Divider';
+import { DividerProps } from '../../../../../Divider/types';
 import { color as defaultColor, colorMode as defaultColorMode } from '../../../../common/data/defaultPropValues';
 import { AccordionsContext as AccordionsContextType } from '../../../../types';
-import { isDisabled as defaultIsDisabled } from '../../common/data/defaultPropValues';
+import { isDisabled as defaultIsDisabled, isLight as defaultIsLight } from '../../common/data/defaultPropValues';
 import { AccordionContext as AccordionContextType } from '../../types';
 
-const AccordionDivider: FC = () => {
+const AccordionDivider: FC<DividerProps> = (props) => {
 	const theme = useTheme();
 
 	const { color = defaultColor, colorMode = defaultColorMode } = useContext<AccordionsContextType>(AccordionsContext);
-	const { isDisabled = defaultIsDisabled } = useContext<AccordionContextType>(AccordionContext);
+	const { isDisabled = defaultIsDisabled, isLight = defaultIsLight } =
+		useContext<AccordionContextType>(AccordionContext);
 
 	return (
 		<Divider
+			{...props}
 			backgroundColor={getColor({
 				theme,
 				colorMode,
 				color: isDisabled ? 'gray' : color,
-				type: isDisabled ? 'text.secondary' : 'color'
+				type: isDisabled
+					? isLight
+						? 'divider'
+						: 'text.secondary'
+					: isLight
+					? 'divider'
+					: color === 'gray'
+					? 'text.secondary'
+					: 'color'
 			})}
 		/>
 	);
