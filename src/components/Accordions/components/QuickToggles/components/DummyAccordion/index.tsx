15f69291cdc03cf0118d230d147270d8438850f5
David Scicluna <dave_nostra@hotmail.com>
2022-06-19 14:06:45 +0200

    Fix: Accordions - QuickToggles - DummyAccordion

--- src/components/Accordions/components/QuickToggles/components/DummyAccordion/index.tsx
+++ src/components/Accordions/components/QuickToggles/components/DummyAccordion/index.tsx
@@ -4,22 +4,25 @@ import { useConst } from '@chakra-ui/react';
 
 import { range, sample } from 'lodash';
 
+import { DummyAccordionProps } from './types';
+
 import { AccordionsContext } from '../../../..';
 import Button from '../../../../../Clickable/Button';
 import Skeleton from '../../../../../Skeleton';
 import { color as defaultColor, colorMode as defaultColorMode } from '../../../../common/data/defaultPropValues';
 import { AccordionsContext as AccordionsContextType } from '../../../../types';
+import { size as defaultSize } from '../../common/data/defaultPropValues';
 
 const dummies = range(50, 250, 25);
 
-const DummyAccordion: FC = () => {
-	const { color = defaultColor, colorMode = defaultColorMode } = useContext<AccordionsContextType>(AccordionsContext);
+const DummyAccordion: FC<DummyAccordionProps> = ({ color = defaultColor, size = defaultSize }) => {
+	const { colorMode = defaultColorMode } = useContext<AccordionsContextType>(AccordionsContext);
 
 	const dummy = useConst<number>(sample(dummies) || 100);
 
 	return (
 		<Skeleton width={`${dummy}px`} color={color} colorMode={colorMode} isLoaded>
-			<Button color={color} colorMode={colorMode} isDisabled size='sm' variant='text' sx={{ front: { px: 0 } }}>
+			<Button color={color} colorMode={colorMode} isDisabled size={size} variant='text' sx={{ front: { px: 0 } }}>
 				Dummy Accordion Label
 			</Button>
 		</Skeleton>
---
+++ src/components/Accordions/components/QuickToggles/components/DummyAccordion/types.ts
@@ -0,0 +1,3 @@
+import { QuickTogglesProps } from '../../types';
+
+export type DummyAccordionProps = Pick<QuickTogglesProps, 'color' | 'size'>;
