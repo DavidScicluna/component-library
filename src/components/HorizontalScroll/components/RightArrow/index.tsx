16fe72d69a26ca709e32dd4ae6b183d4e17d415a
David Scicluna <dave_nostra@hotmail.com>
2022-06-26 19:05:13 +0200

    Fix: HorizontalScroll - RightArrow

--- src/components/HorizontalScroll/components/RightArrow/index.tsx
+++ src/components/HorizontalScroll/components/RightArrow/index.tsx
@@ -1,15 +1,23 @@
 import { FC, useContext, useEffect } from 'react';
 
-import { useBoolean } from '@chakra-ui/react';
+import { useConst, useBoolean } from '@chakra-ui/react';
 
 import { VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { useDebounce } from 'usehooks-ts';
 
+import { useTheme } from '../../../../common/hooks';
+import { convertStringToNumber } from '../../../../common/utils';
 import Arrow from '../Arrow';
 
 const RightArrow: FC = () => {
+	const theme = useTheme();
+
 	const { isLastItemVisible, scrollNext, visibleItemsWithoutSeparators } = useContext(VisibilityContext);
 
+	const duration = useConst<number>(convertStringToNumber(theme.transition.duration.normal, 'ms'));
+
 	const [isDisabled, setIsDisabled] = useBoolean();
+	const debouncedIsDisabled = useDebounce<boolean>(isDisabled, duration);
 
 	useEffect(() => {
 		if (visibleItemsWithoutSeparators.length) {
@@ -21,9 +29,7 @@ const RightArrow: FC = () => {
 		}
 	}, [visibleItemsWithoutSeparators, isLastItemVisible]);
 
-	return (
-		<Arrow aria-label='Right Arrow Button' direction='right' isDisabled={isDisabled} onClick={() => scrollNext()} />
-	);
+	return <Arrow direction='right' isDisabled={debouncedIsDisabled} onClick={() => scrollNext()} />;
 };
 
 export default RightArrow;
