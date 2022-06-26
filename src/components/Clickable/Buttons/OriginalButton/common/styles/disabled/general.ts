a48a0b57f7bae0cbf305ec809863bc4d3da84bbf
David Scicluna <dave_nostra@hotmail.com>
2022-06-26 16:55:28 +0200

    Fix: Buttons - OriginalButton - Disabled Styling

--- src/components/Clickable/Buttons/OriginalButton/common/styles/disabled/general.ts
+++ src/components/Clickable/Buttons/OriginalButton/common/styles/disabled/general.ts
@@ -1,7 +1,7 @@
 import { ButtonDisabledStylingProps } from './types';
 
 import { Style } from '../../../../../../../common/types';
-import { isLoading as defaultIsLoading } from '../../data/defaultPropValues';
+import { isLoading as defaultIsLoading } from '../../../../common/data/defaultPropValues';
 
 export default ({ isLoading = defaultIsLoading }: ButtonDisabledStylingProps): Style => ({
 	cursor: 'not-allowed',
