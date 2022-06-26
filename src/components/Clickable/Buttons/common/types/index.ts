dc397dfa629c6c3d7eeff62f650a02768555aa7e
David Scicluna <dave_nostra@hotmail.com>
2022-06-26 16:54:06 +0200

    Feature: CommonButtonProps

---
+++ src/components/Clickable/Buttons/common/types/index.ts
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+
+import { ColorMode } from '@chakra-ui/react';
+
+import { Color } from '../../../../../theme/types';
+
+export type ButtonColor = Exclude<Color, 'transparent'>;
+
+export type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type ButtonVariant = 'contained' | 'outlined' | 'text';
+
+export type CommonButtonProps = {
+	children?: ReactNode;
+	color?: ButtonColor;
+	colorMode?: ColorMode;
+	isFullWidth?: boolean;
+	size?: ButtonSize;
+	variant?: ButtonVariant;
+};
