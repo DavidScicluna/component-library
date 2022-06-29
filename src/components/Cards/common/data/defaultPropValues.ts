0e579a0a0ab275046fcaa0ea30c83bb253047c0c
David Scicluna <dave_nostra@hotmail.com>
2022-06-29 17:55:23 +0200

    Feature: Cards - common defaultPropValues

---
+++ src/components/Cards/common/data/defaultPropValues.ts
@@ -0,0 +1,17 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Space } from '../../../../theme/types';
+import { CardColor, CardVariant } from '../types';
+
+export const color: CardColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isFullWidth = true;
+export const isDivisible = true;
+export const isLight = true;
+// export const isFixed = false;
+
+export const spacing: Space = 2;
+
+export const variant: CardVariant = 'outlined';
