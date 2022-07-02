60836099e5b3c4982932abf76d1a03ceca4bd799
David Scicluna <dave_nostra@hotmail.com>
2022-07-02 15:21:54 +0200

    Fix: Buttons - Button

--- src/components/Clickable/Buttons/OriginalButton/common/styles/button/text.ts
+++ src/components/Clickable/Buttons/OriginalButton/common/styles/button/text.ts
@@ -2,12 +2,17 @@ import { ButtonStyleProps } from './types';
 
 import { Style } from '../../../../../../../common/types';
 import { Radius } from '../../../../../../../theme/types';
+import { size as defaultSize } from '../../../../common/data/defaultPropValues';
+import { getSizeConfig } from '../../../../common/utils';
 
-export default ({ theme }: ButtonStyleProps): Style => {
+export default ({ theme, size = defaultSize }: ButtonStyleProps): Style => {
+	const config = getSizeConfig({ size });
+	const border = config.border;
+
 	const radius: Radius = 'none';
 
 	return {
-		'borderWidth': 0,
+		'borderWidth': `${border}px`,
 		'borderStyle': 'solid',
 		'borderColor': theme.colors.transparent,
 		'borderRadius': theme.radii[radius],
