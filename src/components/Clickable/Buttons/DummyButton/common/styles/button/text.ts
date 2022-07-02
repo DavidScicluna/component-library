c70a0d8642113c835f415fcc88b480dedb6c90f5
David Scicluna <dave_nostra@hotmail.com>
2022-07-02 15:07:05 +0200

    Fix: Buttons - DummyButton - Text Styling

--- src/components/Clickable/Buttons/DummyButton/common/styles/button/text.ts
+++ src/components/Clickable/Buttons/DummyButton/common/styles/button/text.ts
@@ -2,18 +2,24 @@ import { DummyButtonStyleProps } from './types';
 
 import { Style } from '../../../../../../../common/types';
 import { Radius } from '../../../../../../../theme/types';
+import { size as defaultSize } from '../../../../common/data/defaultPropValues';
+import { getSizeConfig } from '../../../../common/utils';
 
-export default ({ theme }: DummyButtonStyleProps): Style => {
-	const radius: Radius = 'none';
+export default ({ theme, size = defaultSize }: DummyButtonStyleProps): Style => {
+	const config = getSizeConfig({ size });
+	const border = config.border;
+
+	const buttonRadius: Radius = 'none';
+	const skeletonRadius: Radius = size === 'xs' || size === 'sm' ? 'xs' : size === 'xl' ? 'base' : 'sm';
 
 	return {
-		'borderWidth': 0,
+		'borderWidth': `${border}px`,
 		'borderStyle': 'solid',
 		'borderColor': theme.colors.transparent,
-		'borderRadius': theme.radii[radius],
+		'borderRadius': theme.radii[buttonRadius],
 
 		'& .ds-cl-dummy-button-skeleton-icon': {
-			borderRadius: theme.radii[radius]
+			borderRadius: theme.radii[skeletonRadius]
 		}
 	};
 };
