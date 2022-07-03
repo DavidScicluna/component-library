8450550f4216a0320d68f9a7169affe687c3822b
David Scicluna <dave_nostra@hotmail.com>
2022-07-03 18:26:10 +0200

    Fix: Accordions - DummyAccordions - DummyQuickToggles - DummyAccordion

--- src/components/Accordions/DummyAccordions/components/DummyQuickToggles/components/DummyAccordion/index.tsx
+++ src/components/Accordions/DummyAccordions/components/DummyQuickToggles/components/DummyAccordion/index.tsx
@@ -5,10 +5,7 @@ import { DummyAccordionProps } from './types';
 import { DummyAccordionsContext } from '../../../..';
 import Button from '../../../../../../Clickable/Buttons/OriginalButton';
 import Skeleton from '../../../../../../Skeleton';
-import {
-	color as defaultColor,
-	colorMode as defaultColorMode
-} from '../../../../../OriginalAccordions/common/data/defaultPropValues';
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../common/data/defaultPropValues';
 import { size as defaultSize } from '../../../../../OriginalAccordions/components/QuickToggles/common/data/defaultPropValues';
 import { DummyAccordionsContext as DummyAccordionsContextType } from '../../../../types';
 
