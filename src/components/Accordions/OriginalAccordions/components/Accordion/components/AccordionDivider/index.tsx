3ceed7cb71548527ad578da53f0316603910732c
David Scicluna <dave_nostra@hotmail.com>
2022-07-03 18:23:51 +0200

    Fix: Accordions - OriginalAccordions - Accordion - AccordionDivider

--- src/components/Accordions/OriginalAccordions/components/Accordion/components/AccordionDivider/index.tsx
+++ src/components/Accordions/OriginalAccordions/components/Accordion/components/AccordionDivider/index.tsx
@@ -6,11 +6,8 @@ import { useTheme } from '../../../../../../../common/hooks';
 import { getColor } from '../../../../../../../common/utils/color';
 import Divider from '../../../../../../Divider';
 import { DividerProps } from '../../../../../../Divider/types';
-import {
-	color as defaultColor,
-	colorMode as defaultColorMode,
-	isDisabled as defaultIsDisabled
-} from '../../../../common/data/defaultPropValues';
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../common/data/defaultPropValues';
+import { isDisabled as defaultIsDisabled } from '../../../../common/data/defaultPropValues';
 import { AccordionsContext as AccordionsContextType } from '../../../../types';
 import { isLight as defaultIsLight } from '../../common/data/defaultPropValues';
 import { AccordionContext as AccordionContextType } from '../../types';
