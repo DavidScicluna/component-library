b620c90bb0910beb54b146f94f9ba066b1bb46da
David Scicluna <dave_nostra@hotmail.com>
2022-07-04 21:30:17 +0200

    Fix: Accordions - OriginalAccordions - Story

--- src/stories/Accordions/OriginalAccordions/index.stories.tsx
+++ src/stories/Accordions/OriginalAccordions/index.stories.tsx
@@ -29,10 +29,6 @@ import { isDisabled as defaultIsDisabled } from '../../../components/Accordions/
 import controls from '../../common/controls';
 import parameters from '../../common/parameters';
 
-const accordions: AccordionType[] = range(0, 15).map((_dummy, index) => {
-	return { id: toString(index), title: `Accordion ${++index}` } as AccordionType;
-});
-
 const colorOptions: AccordionsColor[] = [
 	'gray',
 	'red',
@@ -86,66 +82,75 @@ export default {
 	parameters: { backgrounds: { ...parameters.backgrounds } }
 } as Meta;
 
-export const Accordions: Story = (props: AccordionsProps): ReactElement => (
-	<AccordionsComponent {...props} color='gray' accordions={accordions} p={0}>
+export const Accordions: Story = (props: AccordionsProps<unknown>): ReactElement => (
+	<AccordionsComponent
+		{...props}
+		color='gray'
+		accordions={range(0, 15).map((_dummy, index) => {
+			return { id: toString(index), title: `Accordion ${++index}` } as AccordionType<unknown>;
+		})}
+		p={0}
+	>
 		<AccordionsQuickToggles color={props.color === 'gray' ? 'deep_orange' : props.color} spacing={2} />
 
 		<AccordionsPanel>
-			{accordions.map((accordion) => (
-				<Accordion
-					key={accordion.id}
-					id={accordion.id}
-					header={
-						<AccordionHeader
-							renderTitle={(props) => <Text {...props}>{`${accordion.title} Title`}</Text>}
-							renderSubtitle={(props) => <Text {...props}>{`${accordion.title} Subtitle`}</Text>}
-							actions={
+			{({ accordions }) =>
+				accordions.map((accordion) => (
+					<Accordion
+						key={accordion.id}
+						id={accordion.id}
+						header={
+							<AccordionHeader
+								renderTitle={(props) => <Text {...props}>{`${accordion.title} Title`}</Text>}
+								renderSubtitle={(props) => <Text {...props}>{`${accordion.title} Subtitle`}</Text>}
+								actions={
+									<DummyButton
+										color={props.color === 'gray' ? 'orange' : props.color}
+										colorMode={props.colorMode}
+										size='xs'
+									>
+										Dummy Card Action
+									</DummyButton>
+								}
+							/>
+						}
+						body={
+							<AccordionBody>
+								<VStack width='100%' alignItems='stretch' justifyContent='stretch' spacing={2}>
+									{range(2).map((_dummy, index) => (
+										<Skeleton
+											key={index}
+											colorMode={props.colorMode}
+											isLoaded={false}
+											variant='text'
+											sx={{ width: '100%', flex: 1 }}
+										>
+											<Text width='100%' align='left' fontSize='md' noOfLines={1}>
+												Dummy Text
+											</Text>
+										</Skeleton>
+									))}
+								</VStack>
+							</AccordionBody>
+						}
+						footer={
+							<AccordionFooter>
 								<DummyButton
 									color={props.color === 'gray' ? 'orange' : props.color}
 									colorMode={props.colorMode}
+									isFullWidth
 									size='xs'
+									variant='text'
 								>
-									Dummy Card Action
+									This is the Dummy Card Footer Action
 								</DummyButton>
-							}
-						/>
-					}
-					body={
-						<AccordionBody>
-							<VStack width='100%' alignItems='stretch' justifyContent='stretch' spacing={2}>
-								{range(2).map((_dummy, index) => (
-									<Skeleton
-										key={index}
-										colorMode={props.colorMode}
-										isLoaded={false}
-										variant='text'
-										sx={{ width: '100%', flex: 1 }}
-									>
-										<Text width='100%' align='left' fontSize='md' noOfLines={1}>
-											Dummy Text
-										</Text>
-									</Skeleton>
-								))}
-							</VStack>
-						</AccordionBody>
-					}
-					footer={
-						<AccordionFooter>
-							<DummyButton
-								color={props.color === 'gray' ? 'orange' : props.color}
-								colorMode={props.colorMode}
-								isFullWidth
-								size='xs'
-								variant='text'
-							>
-								This is the Dummy Card Footer Action
-							</DummyButton>
-						</AccordionFooter>
-					}
-					spacing={2}
-					p={props.padding}
-				/>
-			))}
+							</AccordionFooter>
+						}
+						spacing={2}
+						p={props.padding}
+					/>
+				))
+			}
 		</AccordionsPanel>
 	</AccordionsComponent>
 );
