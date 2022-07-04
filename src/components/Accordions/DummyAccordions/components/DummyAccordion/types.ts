a7ba29636ae8b2dcc42e430fdf1bb46a0b239a75
David Scicluna <dave_nostra@hotmail.com>
2022-07-04 21:27:11 +0200

    Fix: Accordions - DummyAccordions - DummyAccordion

--- src/components/Accordions/DummyAccordions/components/DummyAccordion/types.ts
+++ src/components/Accordions/DummyAccordions/components/DummyAccordion/types.ts
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 
 import { AccordionProps } from '../../../OriginalAccordions/components/Accordion/types';
 
+type Omitted = 'id' | 'header' | 'body' | 'footer' | 'isActive' | 'isDisabled' | 'isDivisible' | 'spacing';
+
 export type DummyAccordionProps = {
 	children: ReactNode;
-} & Omit<AccordionProps, 'header' | 'body' | 'footer' | 'isActive' | 'isDisabled' | 'isDivisible' | 'spacing'>;
+} & Omit<AccordionProps<unknown>, Omitted>;
