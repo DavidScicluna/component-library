86c3de3a8a3ab1a20c91722fca02fb76abba4bff
DavidScicluna <dave_nostra@hotmail.com>
2022-07-09 18:10:41 +0200

    Fix: Link - Common Utils

--- src/components/Clickable/Link/common/utils/index.ts
+++ src/components/Clickable/Link/common/utils/index.ts
@@ -1,11 +1,11 @@
-import { ColorMode } from '@chakra-ui/react';
+type GetAmountReturn = {
+	hover: number;
+	active: number;
+};
 
-import { Color, ColorHues } from '../../../../../theme/types';
-
-export const handleHue = (colorMode: ColorMode, color: Color): ColorHues => {
-	if (colorMode === 'light') {
-		return color === 'gray' ? 400 : color === 'white' ? 100 : color === 'black' ? 800 : 500;
-	} else {
-		return color === 'gray' ? 500 : color === 'white' ? 100 : color === 'black' ? 800 : 400;
-	}
+export const getAmount = (): GetAmountReturn => {
+	return {
+		hover: 0.05,
+		active: 0.1
+	};
 };
