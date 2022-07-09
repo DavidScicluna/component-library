134a5d9ba95fb2fab9b43f796c04000d2956f024
DavidScicluna <dave_nostra@hotmail.com>
2022-07-09 18:10:48 +0200

    Fix: Link - External

--- src/components/Clickable/Link/External/index.tsx
+++ src/components/Clickable/Link/External/index.tsx
@@ -1,40 +1,42 @@
 import { FC } from 'react';
 
-import { ColorMode, useColorMode, Link as CUILink } from '@chakra-ui/react';
+import { useColorMode, Link as CUILink } from '@chakra-ui/react';
 
 import { isString, merge } from 'lodash';
 
 import { useTheme } from '../../../../common/hooks';
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	isDisabled as defaultIsDisabled,
+	isFullWidth as defaultIsFullWidth
+} from '../common/data/defaultPropValues';
 import useStyles from '../common/styles';
 import { LinkProps as ExternalProps } from '../common/types';
 
 const External: FC<ExternalProps> = (props) => {
 	const theme = useTheme();
-	const { colorMode: colorModeHook } = useColorMode();
+	const { colorMode: colorModeHook = defaultColorMode } = useColorMode();
 
 	const {
 		children,
-		color = 'gray',
-		colorMode: colorModeProp,
-		isDisabled = false,
-		isFullWidth = false,
+		color = defaultColor,
+		colorMode = colorModeHook,
+		isDisabled = defaultIsDisabled,
+		isFullWidth = defaultIsFullWidth,
 		sx,
 		...rest
 	} = props;
 
-	const colorMode: ColorMode = colorModeProp || colorModeHook;
-
-	const style = useStyles(theme, { color, colorMode, isFullWidth, isString: isString(children) });
-
-	console.log(props);
+	const style = useStyles({ theme, color, colorMode, isFullWidth, isString: isString(children) });
 
 	return (
 		<CUILink
 			{...rest}
 			aria-disabled={isDisabled}
 			isExternal
-			onClick={isDisabled ? (event) => event.preventDefault() : undefined}
-			sx={{ ...merge(style, sx || {}) }}
+			sx={merge(style.link, sx)}
+			_disabled={merge(style.disabled, sx)}
 		>
 			{children}
 		</CUILink>
