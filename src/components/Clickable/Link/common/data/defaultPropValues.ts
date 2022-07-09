ddde53e4d4dff9b4793509e5e187d2afaecb95a8
DavidScicluna <dave_nostra@hotmail.com>
2022-07-09 18:09:00 +0200

    Feature: Link - defaultPropValues

---
+++ src/components/Clickable/Link/common/data/defaultPropValues.ts
@@ -0,0 +1,10 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Color } from '../../../../../theme/types';
+
+export const color: Color = 'transparent';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isFullWidth = false;
