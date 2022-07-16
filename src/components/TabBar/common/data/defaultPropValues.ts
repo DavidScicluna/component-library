0bc58a857181c8326411e00d4f65d0b0de8a96d8
DavidScicluna <dave_nostra@hotmail.com>
2022-07-16 21:37:11 +0200

    Feature: TabBar - defaultPropValues

---
+++ src/components/TabBar/common/data/defaultPropValues.ts
@@ -0,0 +1,9 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { TabBarColor, TabBarDirection } from '../../types';
+
+export const color: TabBarColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const direction: TabBarDirection = 'bottom';
