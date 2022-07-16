273e34015ae4ba3dac29d257a914fa0b04efcee2
DavidScicluna <dave_nostra@hotmail.com>
2022-07-16 21:38:27 +0200

    Fix: TabBar - Tab - Dark Styling

--- src/components/TabBar/components/Tab/common/styles/dark/tab/index.ts
+++ src/components/TabBar/components/Tab/common/styles/dark/tab/index.ts
@@ -3,48 +3,41 @@ import { lighten } from 'color2k';
 import { TabDarkStylingProps } from './types';
 
 import { Style } from '../../../../../../../../common/types';
-import { checkIsTouchDevice } from '../../../../../../../../common/utils';
 import { getHue } from '../../../../../../../../common/utils/color';
-import { Color } from '../../../../../../../../theme/types';
-import { color as defaultColor, isActive as defaultIsActive } from '../../../data/defaultPropValues';
 import { getAmount } from '../../../utils';
 
-const isTouchDevice: boolean = checkIsTouchDevice();
-
-export default ({ theme, color: colorProp = defaultColor, isActive = defaultIsActive }: TabDarkStylingProps): Style => {
+export default ({ theme }: TabDarkStylingProps): Style => {
 	const amount = getAmount();
-	const shade = getHue({ colorMode: 'dark', type: 'color' });
-
-	const color: Color = isActive ? colorProp : 'gray';
+	const shade = getHue({ colorMode: 'dark', type: 'text.secondary' });
 
 	return {
 		'background': theme.colors.transparent,
 		'backgroundColor': theme.colors.transparent,
 		'borderColor': theme.colors.transparent,
-		'color': theme.colors[color][shade],
+		'color': theme.colors.gray[shade],
 
 		'& svg, .ds-cl-icon': {
-			color: theme.colors[color][shade]
+			color: theme.colors.gray[shade]
 		},
 
 		'&:hover': {
 			'background': theme.colors.transparent,
 			'backgroundColor': theme.colors.transparent,
 			'borderColor': theme.colors.transparent,
-			'color': lighten(theme.colors[color][shade], amount.hover),
+			'color': lighten(theme.colors.gray[shade], amount.hover),
 
 			'& svg, .ds-cl-icon': {
-				color: lighten(theme.colors[color][shade], amount.hover)
+				color: lighten(theme.colors.gray[shade], amount.hover)
 			},
 
 			'&:active': {
 				'background': theme.colors.transparent,
 				'backgroundColor': theme.colors.transparent,
 				'borderColor': theme.colors.transparent,
-				'color': lighten(theme.colors[color][shade], amount.active),
+				'color': lighten(theme.colors.gray[shade], amount.active),
 
 				'& svg, .ds-cl-icon': {
-					color: lighten(theme.colors[color][shade], amount.active)
+					color: lighten(theme.colors.gray[shade], amount.active)
 				}
 			}
 		},
@@ -53,16 +46,11 @@ export default ({ theme, color: colorProp = defaultColor, isActive = defaultIsAc
 			'background': theme.colors.transparent,
 			'backgroundColor': theme.colors.transparent,
 			'borderColor': theme.colors.transparent,
-			'color': lighten(theme.colors[color][shade], amount.active),
+			'color': lighten(theme.colors.gray[shade], amount.active),
 
 			'& svg, .ds-cl-icon': {
-				color: lighten(theme.colors[color][shade], amount.active)
+				color: lighten(theme.colors.gray[shade], amount.active)
 			}
-		},
-
-		'&:focus-visible': {
-			outline: !isTouchDevice ? `2px auto ${theme.colors[color][shade]}` : 'none',
-			outlineOffset: !isTouchDevice ? '4px' : 0
 		}
 	};
 };
