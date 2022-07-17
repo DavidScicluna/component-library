1aa13ddd4fe557682814f7b231eaebee69b95b70
DavidScicluna <dave_nostra@hotmail.com>
2022-07-17 17:14:08 +0200

    Feature: SideNavigation - defaultPropValues

---
+++ src/components/Navigation/SideNavigation/common/data/defaultPropValues.ts
@@ -0,0 +1,9 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { SideNavigationColor, SideNavigationMode } from '../../types';
+
+export const color: SideNavigationColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const mode: SideNavigationMode = 'expanded';
