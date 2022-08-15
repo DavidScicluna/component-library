0bc2897681f18b7c2e1edd399eda7b8422a163c7
DavidScicluna <dave_nostra@hotmail.com>
2022-08-15 18:57:56 +0200

    Feature: Stepper - StepList - HorizontalScrollArrows - HorizontalScrollRightArrow

---
+++ src/components/Navigation/Stepper/components/StepList/components/HorizontalScrollArrows/HorizontalScrollRightArrow/index.tsx
@@ -0,0 +1,59 @@
+import { FC, useContext, useEffect } from 'react';
+
+import { useBoolean } from '@chakra-ui/react';
+
+import { useDebounce } from 'usehooks-ts';
+
+import { HorizontalScrollArrowProps as HorizontalScrollRightArrowProps } from '../common/types';
+import { colorMode as defaultColorMode } from '../../../../../common/data/defaultPropValues';
+import { isDisabled as defaultIsDisabled } from '../../../common/data/defaultPropValues';
+import { StepperContext } from '../../../../..';
+import { StepperContext as StepperContextType } from '../../../../../types';
+import HorizontalScrollArrow from '../../../../../../../HorizontalScroll/components/Arrow';
+import { useTheme } from '../../../../../../../../common/hooks';
+import { getColor } from '../../../../../../../../common/utils/color';
+
+const HorizontalScrollRightArrow: FC<HorizontalScrollRightArrowProps> = (props) => {
+	const theme = useTheme();
+
+	const { colorMode = defaultColorMode } = useContext<StepperContextType>(StepperContext);
+
+	const { scroll, isDisabled = defaultIsDisabled } = props;
+	const { getNextItem, isLastItemVisible = false, scrollToItem, visibleItemsWithoutSeparators = [] } = scroll || {};
+
+	const [isVisible, setIsVisible] = useBoolean(true);
+	const debouncedIsVisible = useDebounce<boolean>(isVisible, 250);
+
+	const handleScrollNext = (): void => {
+		const nextItem = getNextItem();
+		scrollToItem(nextItem?.entry?.target, 'smooth', 'center', 'nearest');
+	};
+
+	useEffect(() => {
+		if (visibleItemsWithoutSeparators.length) {
+			if (isLastItemVisible) {
+				setIsVisible.off();
+			} else {
+				setIsVisible.on();
+			}
+		}
+	}, [visibleItemsWithoutSeparators, isLastItemVisible]);
+
+	return (
+		<HorizontalScrollArrow
+			direction='right'
+			isVisible={debouncedIsVisible}
+			isDisabled={isDisabled}
+			onClick={() => handleScrollNext()}
+			sx={{
+				'borderBottomWidth': '2px',
+				'borderBottomStyle': 'solid',
+				'borderBottomColor': getColor({ theme, colorMode, type: 'divider' }),
+
+				'&::before': { borderBottom: 'none' }
+			}}
+		/>
+	);
+};
+
+export default HorizontalScrollRightArrow;
