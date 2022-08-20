32b4feff3bc33b8b41fed4283d5d1dd3d6346e64
DavidScicluna <dave_nostra@hotmail.com>
2022-08-20 16:00:01 +0200

    Fix: ConfirmModal - ConfirmModalBody

--- src/components/Overlay/ConfirmModal/components/ConfirmModalBody/index.tsx
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalBody/index.tsx
@@ -1,14 +1,23 @@
 import { FC } from 'react';
 
-import { ModalBody } from '@chakra-ui/react';
+import { ModalBody, VStack } from '@chakra-ui/react';
 
 import { ConfirmModalBodyProps } from './types';
 
 const ConfirmModalBody: FC<ConfirmModalBodyProps> = ({ children, ...rest }) => {
 	return (
-		<ModalBody width='100%' p={0} m={0} {...rest}>
+		<VStack
+			as={ModalBody}
+			width='100%'
+			alignItems='center'
+			justifyContent='center'
+			p={0}
+			m={0}
+			spacing={0}
+			{...rest}
+		>
 			{children}
-		</ModalBody>
+		</VStack>
 	);
 };
 
