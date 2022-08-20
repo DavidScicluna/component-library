e9ae1bd97b2c1a81a2ff49dc56805bfa0ddc2da6
DavidScicluna <dave_nostra@hotmail.com>
2022-08-20 15:59:30 +0200

    Feature: ConfirmModal - ConfirmModalIcon  - defaultPropValues

---
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalIcon/common/data/defaultPropValues.ts
@@ -0,0 +1,5 @@
+import { ConfirmModalIconColor, ConfirmModalIconVariant } from '../../types';
+
+export const color: ConfirmModalIconColor = 'blue';
+
+export const variant: ConfirmModalIconVariant = 'contained';
