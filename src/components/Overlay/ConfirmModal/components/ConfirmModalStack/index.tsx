ce4ac4a807df7263cab11bf9398242b7c51cce70
DavidScicluna <dave_nostra@hotmail.com>
2022-08-20 15:58:57 +0200

    Fix: ConfirmModal - ConfirmModalStack

--- src/components/Overlay/ConfirmModal/components/ConfirmModalStack/index.tsx
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalStack/index.tsx
@@ -2,13 +2,15 @@ import { FC } from 'react';
 
 import { VStack } from '@chakra-ui/react';
 
-import { spacing as defaultSpacing } from '../../common/data/defaultPropValues';
+import { spacing as defaultSpacing, padding as defaultPadding } from '../../common/data/defaultPropValues';
 
 import { ConfirmModalStackProps } from './types';
 
-const ConfirmModalStack: FC<ConfirmModalStackProps> = ({ children, spacing = defaultSpacing, ...rest }) => {
+const ConfirmModalStack: FC<ConfirmModalStackProps> = (props) => {
+	const { children, spacing = defaultSpacing, p = defaultPadding, ...rest } = props;
+
 	return (
-		<VStack {...rest} width='100%' spacing={spacing}>
+		<VStack width='100%' spacing={spacing} p={p} {...rest}>
 			{children}
 		</VStack>
 	);
