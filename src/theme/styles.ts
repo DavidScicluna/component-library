062aa0e57ff778cd33ab356ccdbbc5eea5d1c3a2
DavidScicluna <dave_nostra@hotmail.com>
2022-09-03 12:05:28 +0200

    Fix: Theme - Styles

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,16 +1,18 @@
-import { Styles, mode } from '@chakra-ui/theme-tools';
+import { Styles, StyleFunctionProps, mode } from '@chakra-ui/theme-tools';
+import { Dict } from '@chakra-ui/utils';
 
+import colors from './foundations/colors';
 import transition from './foundations/transition';
 
 const styles: Styles = {
-	global: (props: Styles['global']) => ({
+	global: (props: StyleFunctionProps | Dict<unknown>) => ({
 		'html': {
 			scrollBehavior: 'smooth'
 		},
 		'body': {
-			color: mode('gray.900', 'gray.50')(props),
-			background: mode('gray.50', 'gray.900')(props),
-			backgroundColor: mode('gray.50', 'gray.900')(props),
+			color: mode(colors.gray[900], colors.gray[50])(props),
+			background: mode(colors.gray[50], colors.gray[900])(props),
+			backgroundColor: mode(colors.gray[50], colors.gray[900])(props),
 
 			transition: 'none',
 			transitionProperty: [
@@ -30,7 +32,7 @@ const styles: Styles = {
 			fontFamily: 'body',
 
 			boxShadow: 'none',
-			outline: '0px transparent',
+			outline: `0px ${colors.transparent}`,
 
 			transition: 'none',
 			transitionProperty: [
