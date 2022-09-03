a6dcd204e9b855ffb23bee22f46cebbf4d228f22
DavidScicluna <dave_nostra@hotmail.com>
2022-09-03 14:10:12 +0200

    Feature: New Utility Common Types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,9 +1,13 @@
 import { CSSObject } from '@chakra-ui/react';
 
 // Utility Types
-export type NonNullable<T> = Exclude<T, null | undefined>; // Remove null and undefined from Type
+export type Undefinable<T> = T | undefined; // Adds undefined to type
 
-export type NoUndefinedField<T> = { [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>> }; // Remove all null and undefined from Type
+export type Nullable<T> = T | null; // Adds null to type
+
+export type NonNullable<T> = Exclude<T, null | undefined>; // Remove null and undefined from type
+
+export type NoUndefinedField<T> = { [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>> }; // Remove null and undefined from all keys type
 
 // Common Component Types
 export type Style = CSSObject;
