db0315869a512522ec3f291460b82d2828645843
DavidScicluna <dave_nostra@hotmail.com>
2022-09-04 15:27:10 +0200

    Feature: ImageEditor - Actions

---
+++ src/components/ImageEditor/components/Actions/index.tsx
@@ -0,0 +1,74 @@
+import { FC, useContext, useCallback } from 'react';
+
+import { useMediaQuery, HStack, VStack, Center } from '@chakra-ui/react';
+
+import { useElementSize } from 'usehooks-ts';
+
+import { colorMode as defaultColorMode } from '../../common/data/defaultPropValues';
+import { ImageEditorContext as ImageEditorContextType } from '../../types';
+import { ImageEditorContext } from '../../';
+import { useTheme } from '../../../../common/hooks';
+import { convertREMToPixels, convertStringToNumber } from '../../../../common/utils';
+import Divider from '../../../Divider';
+
+import Cancel from './components/Cancel';
+import Toolbar from './components/Toolbar';
+import Save from './components/Save';
+import { ActionsProps } from './types';
+
+const Actions: FC<ActionsProps> = ({ onSelectTool, onZoom, onCancel, onSave }) => {
+	const theme = useTheme();
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+
+	const { colorMode = defaultColorMode } = useContext<ImageEditorContextType>(ImageEditorContext);
+
+	const [stackRef, { height: stackHeight }] = useElementSize();
+
+	const [cancelRef, { width: cancelWidth }] = useElementSize();
+	const [saveRef, { width: saveWidth }] = useElementSize();
+
+	const handleScrollWidth = useCallback(() => {
+		const spacing = convertREMToPixels(convertStringToNumber(theme.space[2], 'rem') * 4);
+
+		return `calc(100% - ${cancelWidth + spacing + saveWidth}px)`;
+	}, [theme, cancelWidth, saveWidth]);
+
+	return isMd ? (
+		<HStack
+			ref={stackRef}
+			width='100%'
+			alignItems='center'
+			justifyContent='space-between'
+			divider={<Divider colorMode={colorMode} orientation='vertical' height={`${stackHeight}px`} />}
+			spacing={4}
+		>
+			<Center ref={cancelRef}>
+				<Cancel onCancel={onCancel} />
+			</Center>
+			<Center width={handleScrollWidth()}>
+				<Toolbar onSelectTool={onSelectTool} onZoom={onZoom} />
+			</Center>
+			<Center ref={saveRef}>
+				<Save onSave={onSave} />
+			</Center>
+		</HStack>
+	) : (
+		<VStack
+			width='100%'
+			alignItems='center'
+			justifyContent='space-between'
+			divider={<Divider colorMode={colorMode} />}
+			spacing={4}
+		>
+			<Center width='100%'>
+				<Toolbar onSelectTool={onSelectTool} onZoom={onZoom} />
+			</Center>
+			<HStack width='100%' spacing={2}>
+				<Cancel onCancel={onCancel} />
+				<Save onSave={onSave} />
+			</HStack>
+		</VStack>
+	);
+};
+
+export default Actions;
---
+++ src/components/ImageEditor/components/Actions/types.ts
@@ -0,0 +1,12 @@
+import { ImageEditorMode } from '../../types';
+
+export type OnSelectToolProps = { mode: ImageEditorMode };
+
+export type OnZoomProps = { value: number };
+
+export type ActionsProps = {
+	onSelectTool: (props: OnSelectToolProps) => void;
+	onZoom: (props: OnZoomProps) => void;
+	onCancel: () => void;
+	onSave: () => void;
+};
