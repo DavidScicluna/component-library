9ed349a19099058ec0f739ce0d00817855ce5d7d
DavidScicluna <dave_nostra@hotmail.com>
2022-09-11 18:29:15 +0200

    Feature: Tabs - defaultPropValues

---
+++ src/components/Navigation/Tabs/common/data/defaultPropValues.ts
@@ -0,0 +1,17 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { TabsColor, TabsProps, TabsSize } from '../../types';
+
+export const activeTab = 0;
+
+export const color: TabsColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isFitted = false;
+export const isLazy = true;
+
+export const lazyBehavior: TabsProps['lazyBehavior'] = 'unmount';
+
+export const size: TabsSize = 'md';
