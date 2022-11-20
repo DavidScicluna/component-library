acbd28c44119f9bd16b117a12277b7bb8f48d01d
DavidScicluna <dave_nostra@hotmail.com>
2022-11-20 21:47:35 +0100

    Feature: Forms - DatePicker - defaultPropValues

---
+++ src/components/Forms/DatePicker/common/data/defaultPropValues.ts
@@ -0,0 +1,14 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Space } from '../../../../../theme/types';
+import { DatePickerColor, DatePickerVariant } from '../../types';
+
+export const color: DatePickerColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const format = 'ddd, MMMM DD, YYYY';
+
+export const spacing: Space = 2;
+
+export const variant: DatePickerVariant = 'single';
