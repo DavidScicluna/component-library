674d36286d3f583807dac629e621f9c86473b82c
DavidScicluna <dave_nostra@hotmail.com>
2022-11-20 21:02:13 +0100

    Feature: Forms - DatePicker - Calendar - CalendarDay - utils

---
+++ src/components/Forms/DatePicker/components/Calendar/components/CalendarDay/common/utils/index.ts
@@ -0,0 +1,57 @@
+import dayjs from 'dayjs';
+import { memoize } from 'lodash';
+
+import { Space } from '../../../../../../../../../theme/types';
+import { CalendarDayVariant } from '../../types';
+
+type GetSizeConfigReturn = {
+	width: number; // In Pixels
+	height: number; // In Pixels
+	border: number; // In Pixels
+	spacing: Space; // In Space (Theme) Values
+	offset: number; // In Pixels;
+};
+
+export const getSizeConfig = (): GetSizeConfigReturn => {
+	return {
+		width: 42,
+		height: 42,
+		border: 2,
+		spacing: 2,
+		offset: 4
+	};
+};
+
+type GetAmountReturn = {
+	hover: number;
+	active: number;
+};
+
+export const getAmount = (): GetAmountReturn => {
+	return {
+		hover: 0.05,
+		active: 0.1
+	};
+};
+
+type GetVariantProps = {
+	date: Date;
+	selectedDate?: Date;
+	selectedMonth: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+};
+
+export const getVariant = memoize(({ date: d, selectedDate, selectedMonth }: GetVariantProps): CalendarDayVariant => {
+	const selected = dayjs(selectedDate);
+	const date = dayjs(d);
+	const today = dayjs(new Date());
+
+	if (date.month() !== selectedMonth) {
+		return 'disabled';
+	} else if (selected.isSame(date, 'day')) {
+		return 'selected';
+	} else if (today.isSame(date, 'day')) {
+		return 'today';
+	} else {
+		return 'normal';
+	}
+});
