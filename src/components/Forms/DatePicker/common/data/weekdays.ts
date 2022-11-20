a66a537cffd1da40df276ebf9ba5371c1aa5cb85
DavidScicluna <dave_nostra@hotmail.com>
2022-11-20 21:47:45 +0100

    Feature: Forms - DatePicker - data

---
+++ src/components/Forms/DatePicker/common/data/weekdays.ts
@@ -0,0 +1,3 @@
+export const full: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+export const short: string[] = full.map((month) => month.slice(0, 3));
---
+++ src/components/Forms/DatePicker/common/data/years.ts
@@ -0,0 +1,8 @@
+import { range } from 'lodash';
+
+type Year = number;
+type Years = Year[];
+
+const years: Years = range(1900, 2100);
+
+export default years;
