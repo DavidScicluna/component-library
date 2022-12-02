8a854bc424f7c9b5dc4edcff223f49623195e49a
DavidScicluna <dave_nostra@hotmail.com>
2022-12-02 23:22:46 +0100

    Fix: Navigation - Stepper - StepPanels - Fixed Fade in/out transition

--- src/components/Navigation/Stepper/components/StepPanels/index.tsx
+++ src/components/Navigation/Stepper/components/StepPanels/index.tsx
@@ -1,28 +1,58 @@
 import { FC } from 'react';
 
-import { Center } from '@chakra-ui/react';
+import { useConst, Box } from '@chakra-ui/react';
 
-import { AnimatePresence } from 'framer-motion';
-
-import Fade from '../../../../Transitions/Fade';
+import {
+	getDuration as getTransitionDuration,
+	getEasings as getTransitionEasings,
+	getConfig as getTransitionConfig
+} from '../../../../Transitions/common/utils';
 import { useStepperContext } from '../../common/hooks';
+import { useTheme } from '../../../../../common/hooks';
+import AnimatePresence from '../../../../Transitions/AnimatePresence';
+import { FadeProps } from '../../../../Transitions/Fade/types';
+import Fade from '../../../../Transitions/Fade';
 
 import { StepPanelsProps } from './types';
+
+const StepPanelFade: FC<FadeProps> = ({ children, ...rest }) => {
+	const theme = useTheme();
+
+	const duration = useConst(getTransitionDuration({ theme, duration: 'slow' }));
+	const easing = useConst(getTransitionEasings({ theme }));
+	const config = useConst({ ...getTransitionConfig({ theme }), duration, easing });
+
+	return (
+		<Fade {...rest} transition={{ enter: { ...config }, exit: { ...config } }}>
+			{children}
+		</Fade>
+	);
+};
 
 const StepPanels: FC<StepPanelsProps> = ({ children, ...rest }) => {
 	const { activeStep } = useStepperContext();
 
 	return (
-		<Center {...rest} as={AnimatePresence} width='100%' mode='wait' initial={false}>
-			{children.map(
-				(step, index) =>
-					activeStep === index && (
-						<Center key={`ds-edb-step-panel-${index}`} as={Fade} width='100%' in>
-							{step}
-						</Center>
-					)
-			)}
-		</Center>
+		<Box {...rest} width='100%' display='grid' gridTemplateColumns='minmax(0, 1fr)'>
+			<AnimatePresence>
+				{children.map((step, index) => (
+					<Box
+						as={StepPanelFade}
+						key={`ds-cl-stepper-panels-${index}-panel`}
+						width='100%'
+						height='100%'
+						display='block !important'
+						gridRowStart={1}
+						gridColumnStart={1}
+						layout
+						in={activeStep === index}
+						p={0}
+					>
+						{step}
+					</Box>
+				))}
+			</AnimatePresence>
+		</Box>
 	);
 };
 
