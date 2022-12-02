38b201da52c8ebe59671b03e168c69b4fbe89a99
DavidScicluna <dave_nostra@hotmail.com>
2022-12-02 23:23:02 +0100

    Fix: Navigation - Tabs - TabPanels - Fixed Fade in/out transition

--- src/components/Navigation/Tabs/components/TabPanels/index.tsx
+++ src/components/Navigation/Tabs/components/TabPanels/index.tsx
@@ -2,42 +2,56 @@ import { FC } from 'react';
 
 import { useConst, TabPanels as CUITabPanels, TabPanel } from '@chakra-ui/react';
 
-import { AnimatePresence } from 'framer-motion';
-
+import { useTabsContext } from '../../common/hooks';
+import { FadeProps } from '../../../../Transitions/Fade/types';
+import Fade from '../../../../Transitions/Fade';
+import { useTheme } from '../../../../../common/hooks';
 import {
 	getDuration as getTransitionDuration,
 	getEasings as getTransitionEasings,
 	getConfig as getTransitionConfig
 } from '../../../../Transitions/common/utils';
-import { useTheme } from '../../../../../common/hooks';
-import Fade from '../../../../Transitions/Fade';
-import { useTabsContext } from '../../common/hooks';
+import AnimatePresence from '../../../../Transitions/AnimatePresence';
 
 import { TabPanelsProps } from './types';
 
-const TabPanels: FC<TabPanelsProps> = ({ children, ...rest }) => {
+const TabPanelFade: FC<FadeProps> = ({ children, ...rest }) => {
 	const theme = useTheme();
-
-	const { activeTab } = useTabsContext();
 
 	const duration = useConst(getTransitionDuration({ theme, duration: 'slow' }));
 	const easing = useConst(getTransitionEasings({ theme }));
 	const config = useConst({ ...getTransitionConfig({ theme }), duration, easing });
 
 	return (
-		<CUITabPanels {...rest} as={AnimatePresence} width='100%' mode='wait' initial={false}>
-			{children.map((panel, index) => (
-				<Fade
-					key={`ds-cl-tabs-tab-panel-${index}`}
-					in={activeTab === index}
-					transition={{ enter: { ...config }, exit: { ...config } }}
-					style={{ width: '100%' }}
-				>
-					<TabPanel width='100%' p={0}>
+		<Fade {...rest} transition={{ enter: { ...config }, exit: { ...config } }}>
+			{children}
+		</Fade>
+	);
+};
+
+const TabPanels: FC<TabPanelsProps> = ({ children, ...rest }) => {
+	const { activeTab } = useTabsContext();
+
+	return (
+		<CUITabPanels {...rest} width='100%' display='grid' gridTemplateColumns='minmax(0, 1fr)'>
+			<AnimatePresence>
+				{children.map((panel, index) => (
+					<TabPanel
+						as={TabPanelFade}
+						key={`ds-cl-tabs-panels-${index}-panel`}
+						width='100%'
+						height='100%'
+						display='block !important'
+						gridRowStart={1}
+						gridColumnStart={1}
+						layout
+						in={activeTab === index}
+						p={0}
+					>
 						{panel}
 					</TabPanel>
-				</Fade>
-			))}
+				))}
+			</AnimatePresence>
 		</CUITabPanels>
 	);
 };
