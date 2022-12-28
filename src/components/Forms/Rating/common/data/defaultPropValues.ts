dc696d76ec05d4b354f91756f724acf26ebdfc10
DavidScicluna <dave_nostra@hotmail.com>
2022-12-28 18:21:41 +0100

    Feature: Forms - Rating - defaultPropValues

---
+++ src/components/Forms/Rating/common/data/defaultPropValues.ts
@@ -0,0 +1,19 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { RatingColor, RatingSize, RatingVariant } from '../../types';
+
+export const color: RatingColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const isDisabled = false;
+export const isError = false;
+export const isWarning = false;
+export const isSuccess = false;
+export const isReadOnly = false;
+export const isRequired = false;
+export const isFullWidth = false;
+
+export const size: RatingSize = 'md';
+
+export const variant: RatingVariant = 'outlined';
