8168c16fcab3f3480932ce96d1d87f38d6e06273
DavidScicluna <dave_nostra@hotmail.com>
2022-12-28 18:20:00 +0100

    Feature: Cards - DummyCard - DummyCardHeader - Added render Left/Right panels

--- src/components/Cards/DummyCard/components/DummyCardHeader/index.tsx
+++ src/components/Cards/DummyCard/components/DummyCardHeader/index.tsx
@@ -1,6 +1,6 @@
 import { FC, useCallback } from 'react';
 
-import { HStack, VStack, Center, Text } from '@chakra-ui/react';
+import { HStack, VStack, Center, Box, Text } from '@chakra-ui/react';
 
 import { useElementSize } from 'usehooks-ts';
 
@@ -15,11 +15,23 @@ import { DummyCardHeaderProps } from './types';
 const DummyCardHeader: FC<DummyCardHeaderProps> = (props) => {
 	const theme = useTheme();
 
-	const { colorMode, spacing: spacingHook } = useDummyCardContext();
+	const { color, colorMode, spacing: spacingHook } = useDummyCardContext();
+
+	const [leftRef, { width: leftWidth }] = useElementSize();
+	const [childrenRef, { width: childrenWidth, height: childrenHeight }] = useElementSize();
+	const [rightRef, { width: rightWidth }] = useElementSize();
 
 	const [actionsRef, { width: actionsWidth }] = useElementSize();
 
-	const { hasTitle = true, hasSubtitle = false, actions, spacing = spacingHook, ...rest } = props;
+	const {
+		renderLeft,
+		renderRight,
+		hasTitle = true,
+		hasSubtitle = false,
+		actions,
+		spacing = spacingHook,
+		...rest
+	} = props;
 
 	const handleCalculateTextWidth = useCallback((): string => {
 		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing as Space], 'rem'));
@@ -28,34 +40,49 @@ const DummyCardHeader: FC<DummyCardHeaderProps> = (props) => {
 	}, [theme, spacing, actionsWidth]);
 
 	return (
-		<HStack
-			width='100%'
-			alignItems='center'
-			justifyContent={hasTitle ? 'space-between' : 'flex-end'}
-			spacing={spacing}
-			{...rest}
-		>
-			{hasTitle && (
-				<VStack width={handleCalculateTextWidth()} alignItems='flex-start' spacing={0.5}>
-					{/* Title */}
-					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
-						<Text align='left' fontSize='xl' fontWeight='bold' lineHeight='normal' noOfLines={1}>
-							Card Title
-						</Text>
-					</Skeleton>
-
-					{/* Subtitle */}
-					{hasSubtitle && (
-						<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
-							<Text align='left' fontSize='sm' lineHeight='normal' noOfLines={1}>
-								This is the Card Header Subtitle
-							</Text>
-						</Skeleton>
-					)}
-				</VStack>
+		<HStack width='100%' spacing={spacing} {...rest}>
+			{renderLeft && (
+				<Box ref={leftRef}>
+					{renderLeft({ color, colorMode, width: childrenWidth, height: childrenHeight })}
+				</Box>
 			)}
 
-			{actions && <Center ref={actionsRef}>{actions}</Center>}
+			<HStack
+				ref={childrenRef}
+				width={`calc(100% - ${(renderLeft ? leftWidth : 0) + (renderRight ? rightWidth : 0)}px)`}
+				alignItems='center'
+				justifyContent={hasTitle ? 'space-between' : 'flex-end'}
+				spacing={spacing}
+				{...rest}
+			>
+				{hasTitle && (
+					<VStack width={handleCalculateTextWidth()} alignItems='flex-start' spacing={0.5}>
+						{/* Title */}
+						<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+							<Text align='left' fontSize='xl' fontWeight='bold' lineHeight='normal' noOfLines={1}>
+								Card Title
+							</Text>
+						</Skeleton>
+
+						{/* Subtitle */}
+						{hasSubtitle && (
+							<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+								<Text align='left' fontSize='sm' lineHeight='normal' noOfLines={1}>
+									This is the Card Header Subtitle
+								</Text>
+							</Skeleton>
+						)}
+					</VStack>
+				)}
+
+				{actions && <Center ref={actionsRef}>{actions}</Center>}
+			</HStack>
+
+			{renderRight && (
+				<Box ref={rightRef}>
+					{renderRight({ color, colorMode, width: childrenWidth, height: childrenHeight })}
+				</Box>
+			)}
 		</HStack>
 	);
 };
--- src/components/Cards/DummyCard/components/DummyCardHeader/types.ts
+++ src/components/Cards/DummyCard/components/DummyCardHeader/types.ts
@@ -2,7 +2,17 @@ import { ReactNode } from 'react';
 
 import { StackProps } from '@chakra-ui/react';
 
+import { CommonCardProps } from '../../../common/types';
+
+export type DummyCardHeaderRenderProps = {
+	width?: number; // In Pixels
+	height?: number; // In Pixels
+	// size?: ButtonSize;
+} & Pick<CommonCardProps, 'color' | 'colorMode'>;
+
 export type DummyCardHeaderProps = {
+	renderLeft?: (props: DummyCardHeaderRenderProps) => ReactNode;
+	renderRight?: (props: DummyCardHeaderRenderProps) => ReactNode;
 	hasTitle?: boolean;
 	hasSubtitle?: boolean;
 	actions?: ReactNode;
