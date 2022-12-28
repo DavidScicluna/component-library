56e53556907cdcd0ebf0abce910feedd11f638d8
DavidScicluna <dave_nostra@hotmail.com>
2022-12-28 18:22:31 +0100

    Feature: Forms - Rating - RatingIcon - utils

---
+++ src/components/Forms/Rating/components/RatingIcon/common/utils/index.ts
@@ -0,0 +1,59 @@
+import { memoize } from 'lodash';
+
+import { RatingSize } from '../../../../types';
+import { size as defaultSize } from '../../../../common/data/defaultPropValues';
+import { Space } from '../../../../../../../theme/types';
+
+type Padding = {
+	x: Space; // In Space (Theme) Values
+	y: Space; // In Space (Theme) Values
+};
+
+type GetSizeConfigReturn = {
+	padding: Padding;
+	fontSize: number; // In Pixels;
+};
+
+type GetSizeConfigProps = { size: RatingSize };
+
+export const getSizeConfig = memoize(({ size = defaultSize }: GetSizeConfigProps): GetSizeConfigReturn => {
+	switch (size) {
+		case 'xs':
+			return {
+				padding: { x: 0.5, y: 0.5 },
+				fontSize: 18
+			};
+		case 'sm':
+			return {
+				padding: { x: 0.75, y: 0.75 },
+				fontSize: 21
+			};
+		case 'lg':
+			return {
+				padding: { x: 1.25, y: 1.25 },
+				fontSize: 27
+			};
+		case 'xl':
+			return {
+				padding: { x: 1.5, y: 1.5 },
+				fontSize: 30
+			};
+		default:
+			return {
+				padding: { x: 1, y: 1 },
+				fontSize: 24
+			};
+	}
+});
+
+type GetAmountReturn = {
+	hover: number;
+	active: number;
+};
+
+export const getAmount = (): GetAmountReturn => {
+	return {
+		hover: 0.05,
+		active: 0.1
+	};
+};
