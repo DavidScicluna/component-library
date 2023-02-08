7c01484f8b6081d67e6934627134c4a6ba3249fe
DavidScicluna <dave_nostra@hotmail.com>
2023-02-08 21:14:28 +0100

    Fix: Provider

--- src/components/Provider/types.ts
+++ src/components/Provider/types.ts
@@ -1,3 +1,3 @@
 import { ChakraProviderProps } from '@chakra-ui/react';
 
-export type ProviderProps = Omit<ChakraProviderProps, 'theme'>;
+export type ProviderProps = Omit<ChakraProviderProps, 'theme'> & Partial<Pick<ChakraProviderProps, 'theme'>>;
