462f348fde613798adfde1f53dd0e12bf74629c9
DavidScicluna <dave_nostra@hotmail.com>
2023-04-04 15:44:10 +0200

    Feature: Overlay - StateLabel - StateLabelSubtitle

---
+++ src/components/Overlay/StateLabel/components/StateLabelSubtitle/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import { Text } from '@chakra-ui/react';
+
+import { useTheme } from '../../../../../common/hooks';
+import { getColor } from '../../../../../common/utils/color';
+import { useStateLabelContext } from '../../common/hooks';
+
+import { StateLabelSubtitleProps } from './types';
+
+const StateLabelSubtitle: FC<StateLabelSubtitleProps> = ({ children, ...rest }) => {
+	const theme = useTheme();
+
+	const { colorMode } = useStateLabelContext();
+
+	return (
+		<Text
+			align='center'
+			color={getColor({ theme, colorMode, type: 'text.secondary' })}
+			fontSize={['xs', 'sm']}
+			lineHeight='base'
+			noOfLines={0}
+			{...rest}
+		>
+			{children}
+		</Text>
+	);
+};
+
+export default StateLabelSubtitle;
