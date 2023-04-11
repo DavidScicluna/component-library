005db5bc26333fc2b73bebd473b4c291ff372749
DavidScicluna <dave_nostra@hotmail.com>
2023-04-11 22:15:39 +0200

    Feature: DataDisplay - HorizontalGrids - DummyHorizontalGrid - DummyHorizontalGridScroll

---
+++ src/components/DataDisplay/HorizontalGrids/DummyHorizontalGrid/components/DummyHorizontalGridScroll/types.ts
@@ -0,0 +1,7 @@
+import { StackProps } from '@chakra-ui/react';
+
+import { Space } from '../../../../../../theme/types';
+
+export type DummyHorizontalGridScrollProps = Pick<StackProps, 'children' | 'direction'> & {
+	spacing?: Space;
+};
