276c82440d9a6e57886c6447811a959cf68a4206
DavidScicluna <dave_nostra@hotmail.com>
2023-04-11 22:15:48 +0200

    Feature: DataDisplay - HorizontalGrids - DummyHorizontalGrid

---
+++ src/components/DataDisplay/HorizontalGrids/DummyHorizontalGrid/index.tsx
@@ -0,0 +1,25 @@
+import { FC, createContext } from 'react';
+
+import DummyCard from '../../../Cards/DummyCard';
+import { color as defaultColor, colorMode as defaultColorMode } from '../common/default/props';
+
+import { DummyHorizontalGridContext as DummyHorizontalGridContextType, DummyHorizontalGridProps } from './types';
+
+export const DummyHorizontalGridContext = createContext<DummyHorizontalGridContextType>({
+	color: defaultColor,
+	colorMode: defaultColorMode
+});
+
+const DummyHorizontalGrid: FC<DummyHorizontalGridProps> = (props) => {
+	const { children, color = defaultColor, colorMode = defaultColorMode, ...rest } = props;
+
+	return (
+		<DummyHorizontalGridContext.Provider value={{ color, colorMode }}>
+			<DummyCard {...rest} color={color} colorMode={colorMode}>
+				{children}
+			</DummyCard>
+		</DummyHorizontalGridContext.Provider>
+	);
+};
+
+export default DummyHorizontalGrid;
