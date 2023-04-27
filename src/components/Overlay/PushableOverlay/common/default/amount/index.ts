df055c89ad5bda3371fe34ec3eabc9d55240d17f
DavidScicluna <dave_nostra@hotmail.com>
2023-04-27 20:04:07 +0200

    Feature: Overlay - PushableOverlay - defaults

---
+++ src/components/Overlay/PushableOverlay/common/default/amount/index.ts
@@ -0,0 +1,5 @@
+const back = 0.1;
+const hover = 0.05;
+const active = 0.15;
+
+export { active, back, hover };
---
+++ src/components/Overlay/PushableOverlay/common/default/props/index.ts
@@ -0,0 +1,15 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { Radius } from '../../../../../../theme/types';
+import { PushableOverlayColor, PushableOverlayVariant } from '../../types';
+
+export const borderRadius: Radius = 'base';
+
+export const color: PushableOverlayColor = 'gray';
+
+export const colorMode: ColorMode = 'light';
+
+export const isActive = false;
+export const isDisabled = false;
+
+export const variant: PushableOverlayVariant = 'contained';
