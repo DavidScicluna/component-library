ef2a59a9202ce72ea28527778f13eeb732a5fea9
DavidScicluna <dave_nostra@hotmail.com>
2023-04-29 19:59:24 +0200

    Fix: IconButtons - Types

--- src/components/Clickable/IconButtons/common/types/index.ts
+++ src/components/Clickable/IconButtons/common/types/index.ts
@@ -2,18 +2,18 @@ import { ReactNode } from 'react';
 
 import { ColorMode } from '@chakra-ui/react';
 
-import { Color } from '../../../../../theme/types';
-
-export type IconButtonColor = Exclude<Color, 'transparent'>;
+import { AppColor } from '../../../../../common/types';
 
 export type IconButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type IconButtonVariant = 'contained' | 'outlined' | 'icon';
+export type IconButtonVariant = 'contained' | 'light' | 'outlined' | 'monochrome' | 'icon';
 
 export type CommonIconButtonProps = {
 	children: ReactNode;
-	color?: IconButtonColor;
+	color?: AppColor;
 	colorMode?: ColorMode;
+	isCompact?: boolean;
+	isRound?: boolean;
 	size?: IconButtonSize;
 	variant?: IconButtonVariant;
 };
