75e020c1171cecdff1a9b81747adecb890911e0a
DavidScicluna <dave_nostra@hotmail.com>
2023-04-29 19:58:25 +0200

    Fix: IconButtons - Renamed defaultPropValues

---
+++ src/components/Clickable/IconButtons/common/default/props/index.ts
@@ -0,0 +1,11 @@
+import { IconButtonSize, IconButtonVariant } from '../../types';
+
+export const isActive = false;
+export const isCompact = false;
+export const isDisabled = false;
+export const isLoading = false;
+export const isRound = false;
+
+export const size: IconButtonSize = 'md';
+
+export const variant: IconButtonVariant = 'contained';
