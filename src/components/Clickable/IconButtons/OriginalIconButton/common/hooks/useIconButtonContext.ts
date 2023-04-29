40f85588edaf3f5102968d9aeebe9736ddfeb3c6
DavidScicluna <dave_nostra@hotmail.com>
2023-04-29 20:04:17 +0200

    Fix: IconButtons - OriginalIconButton - useIconButtonContext

--- src/components/Clickable/IconButtons/OriginalIconButton/common/hooks/useIconButtonContext.ts
+++ src/components/Clickable/IconButtons/OriginalIconButton/common/hooks/useIconButtonContext.ts
@@ -1,14 +1,19 @@
 import { useContext } from 'react';
 
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../../common/default/props';
 import { NoUndefinedField } from '../../../../../../common/types';
-import { color as defaultColor, colorMode as defaultColorMode } from '../../../common/data/defaultPropValues';
+import { size as defaultSize } from '../../../common/default/props';
 import { IconButtonContext } from '../..';
-import { IconButtonContext as IconButtonContextType } from '../../types';
+import { IconButtonContext as IconButtonContextType } from '../types';
 
 const useIconButtonContext = (): NoUndefinedField<IconButtonContextType> => {
-	const { color = defaultColor, colorMode = defaultColorMode } = useContext<IconButtonContextType>(IconButtonContext);
+	const {
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		size = defaultSize
+	} = useContext<IconButtonContextType>(IconButtonContext);
 
-	return { color, colorMode };
+	return { color, colorMode, size };
 };
 
 export default useIconButtonContext;
