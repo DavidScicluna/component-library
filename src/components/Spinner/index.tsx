7a3f368f441d8569c1e772c680ba900d7c2a6c66
DavidScicluna <dave_nostra@hotmail.com>
2023-04-29 19:56:15 +0200

    Fix: Spinner - Types & added puff mode

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useMemo } from 'react';
-import { PulseLoader, SyncLoader } from 'react-spinners';
+import { PuffLoader, PulseLoader, SyncLoader } from 'react-spinners';
 
 import { useTheme } from '../../common/hooks';
 import { convertREMToPixels, convertStringToNumber } from '../../common/utils';
 
-import { SpinnerProps } from './types';
+import { SpinnerProps } from './common/types';
 
 const Spinner: FC<SpinnerProps> = (props) => {
 	const theme = useTheme();
@@ -12,10 +12,14 @@ const Spinner: FC<SpinnerProps> = (props) => {
 	const { color, speed = 0.75, mode = 'sync', size = 'xl' } = props;
 
 	const fontSize = useMemo((): number => {
-		return convertREMToPixels(convertStringToNumber(theme.fontSizes[size], 'rem')) / 4;
+		return typeof size === 'number'
+			? size
+			: convertREMToPixels(convertStringToNumber(theme.fontSizes[size], 'rem')) / 4;
 	}, [size]);
 
 	switch (mode) {
+		case 'puff':
+			return <PuffLoader color={color} speedMultiplier={speed} loading size={fontSize} />;
 		case 'sync':
 			return <SyncLoader color={color} speedMultiplier={speed} loading size={fontSize} />;
 		case 'pulse':
