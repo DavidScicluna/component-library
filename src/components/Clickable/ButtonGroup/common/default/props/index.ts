a5d55325e8f4548ad0181a78c3412604c0b82796
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 13:23:35 +0200

    Fix: ButtonGroup - Renamed defaultPropValues

---
+++ src/components/Clickable/ButtonGroup/common/default/props/index.ts
@@ -0,0 +1,9 @@
+import { Space } from '../../../../../../theme/types';
+
+export const children = [];
+
+export const isAttached = true;
+export const isCompact = false;
+export const isRound = false;
+
+export const spacing: Space = 0;
