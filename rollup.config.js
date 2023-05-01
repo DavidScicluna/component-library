1c019a2a0247a5a76e0f58aaf007eaa7029029c7
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 21:28:13 +0200

    Fix: Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -6,41 +6,54 @@ import image from '@rollup/plugin-image';
 import resolve from '@rollup/plugin-node-resolve';
 import strip from '@rollup/plugin-strip';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import gzip from 'rollup-plugin-gzip';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
 import postcss from 'rollup-plugin-postcss';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pj = require('./package.json');
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: pj.main,
-			format: 'cjs',
-			sourcemap: true
-		},
-		{
-			file: pj.module,
-			format: 'esm',
-			sourcemap: true
-		}
-	],
-	plugins: [
-		peerDepsExternal(),
-		strip(),
-		resolve(),
-		commonjs(),
-		beep(),
-		// dynamicImportVars(),
-		eslint(),
-		typescript({
-			include: ['src/**/*'],
-			exclude: ['.vscode', '.storybook', '.yarn', 'build', 'node_modules', 'stories']
-		}),
-		postcss({ extensions: ['.css'] }),
-		image(),
-		gzip()
-	]
-};
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: pj.main,
+				format: 'cjs',
+				sourcemap: true
+			},
+			{
+				file: pj.module,
+				format: 'esm',
+				sourcemap: true
+			}
+		],
+		plugins: [
+			peerDepsExternal(),
+			strip(),
+			resolve(),
+			commonjs(),
+			beep(),
+			// dynamicImportVars(),
+			eslint(),
+			postcss({ extensions: ['.css'] }),
+			typescript({
+				tsconfig: './tsconfig.json',
+				include: ['src/**/*'],
+				exclude: ['.vscode', '.storybook', '.yarn', 'build', 'node_modules', 'stories']
+			}),
+			image(),
+			gzip(),
+			terser()
+		]
+	},
+	{
+		input: 'build/types/index.d.ts',
+		output: [{ file: 'build/index.d.ts', format: 'esm' }],
+		external: [/\.css$/],
+		plugins: [dts()]
+	}
+];
