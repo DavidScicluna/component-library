c947f17ddbdde11a0105b19fd45b1aa828df38f6
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 15:44:18 +0200

    Fix: HorizontalGrids - HorizontalGrid - HorizontalGridFooter

--- src/components/DataDisplay/HorizontalGrids/HorizontalGrid/components/HorizontalGridFooter/index.tsx
+++ src/components/DataDisplay/HorizontalGrids/HorizontalGrid/components/HorizontalGridFooter/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { Center } from '@chakra-ui/react';
 
-import { HorizontalGridFooterProps } from './types';
+import { HorizontalGridFooterProps } from './common/types';
 
 const HorizontalGridFooter: FC<HorizontalGridFooterProps> = ({ children, ...rest }) => {
 	return (
