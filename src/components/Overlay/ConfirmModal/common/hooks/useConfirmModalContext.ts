3708b09b803e7fe2d2e8948b20f10c49392b5be7
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 14:25:09 +0200

    Fix: ConfirmModal - useConfirmModalContext

--- src/components/Overlay/ConfirmModal/common/hooks/useConfirmModalContext.ts
+++ src/components/Overlay/ConfirmModal/common/hooks/useConfirmModalContext.ts
@@ -3,17 +3,18 @@ import { useContext } from 'react';
 import { method as defaultOnClose } from '../../../../../common/default/props';
 import { NoUndefinedField } from '../../../../../common/types';
 import { ConfirmModalContext } from '../..';
-import { ConfirmModalContext as ConfirmModalContextType } from '../../types';
-import { color as defaultColor, colorMode as defaultColorMode } from '../data/defaultPropValues';
+import { color as defaultColor, colorMode as defaultColorMode, spacing as defaultSpacing } from '../default/props';
+import { ConfirmModalContext as ConfirmModalContextType } from '../types';
 
 const useConfirmModalContext = (): NoUndefinedField<ConfirmModalContextType> => {
 	const {
 		color = defaultColor,
 		colorMode = defaultColorMode,
-		onClose = defaultOnClose
+		onClose = defaultOnClose,
+		spacing = defaultSpacing
 	} = useContext<ConfirmModalContextType>(ConfirmModalContext);
 
-	return { color, colorMode, onClose };
+	return { color, colorMode, onClose, spacing };
 };
 
 export default useConfirmModalContext;
