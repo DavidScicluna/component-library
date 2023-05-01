cd1cb3780609a98f4bc7eb2460813ad1b189ea2c
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 15:44:10 +0200

    Fix: HorizontalGrids - HorizontalGrid - HorizontalGridBody

--- src/components/DataDisplay/HorizontalGrids/HorizontalGrid/components/HorizontalGridBody/index.tsx
+++ src/components/DataDisplay/HorizontalGrids/HorizontalGrid/components/HorizontalGridBody/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import CardBody from '../../../../../Cards/components/CardBody';
 
-import { HorizontalGridBodyProps } from './types';
+import { HorizontalGridBodyProps } from './common/types';
 
 const HorizontalGridBody: FC<HorizontalGridBodyProps> = ({ children, ...rest }) => {
 	return <CardBody {...rest}>{children}</CardBody>;
