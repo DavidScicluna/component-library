966b9433a3e87556b8ac9a8f523809f5cac50a41
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 16:13:43 +0200

    Removed Tag Story

--- src/stories/Clickable/Tag/types.ts
+++
@@ -1,7 +0,0 @@
-import { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
-
-import Tag from '../../../components/DataDisplay/Tag';
-
-export type Meta = StorybookMeta<typeof Tag>;
-
-export type Story = StorybookStory<typeof Tag>;
