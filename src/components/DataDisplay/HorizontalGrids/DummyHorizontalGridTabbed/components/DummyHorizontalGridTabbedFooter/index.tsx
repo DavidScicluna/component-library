2334c2d906abb1658b0555158975846ff430b599
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 15:57:15 +0200

    Fix: HorizontalGrids - DummyHorizontalGridTabbed - DummyHorizontalGridTabbedFooter

--- src/components/DataDisplay/HorizontalGrids/DummyHorizontalGridTabbed/components/DummyHorizontalGridTabbedFooter/index.tsx
+++ src/components/DataDisplay/HorizontalGrids/DummyHorizontalGridTabbed/components/DummyHorizontalGridTabbedFooter/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { Center } from '@chakra-ui/react';
 
-import { DummyHorizontalGridTabbedFooterProps } from './types';
+import { DummyHorizontalGridTabbedFooterProps } from './common/types';
 
 const DummyHorizontalGridTabbedFooter: FC<DummyHorizontalGridTabbedFooterProps> = ({ children, ...rest }) => {
 	return (
