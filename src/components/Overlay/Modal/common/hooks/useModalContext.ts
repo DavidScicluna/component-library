336178634a5500be980047a994312d579c177c78
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 14:06:58 +0200

    Fix: Modal - useModalContext

--- src/components/Overlay/Modal/common/hooks/useModalContext.ts
+++ src/components/Overlay/Modal/common/hooks/useModalContext.ts
@@ -3,13 +3,13 @@ import { useContext } from 'react';
 import { method as defaultOnClose } from '../../../../../common/default/props';
 import { NoUndefinedField } from '../../../../../common/types';
 import { ModalContext } from '../..';
-import { ModalContext as ModalContextType } from '../../types';
 import {
 	color as defaultColor,
 	colorMode as defaultColorMode,
 	size as defaultSize,
 	spacing as defaultSpacing
-} from '../data/defaultPropValues';
+} from '../default/props';
+import { ModalContext as ModalContextType } from '../types';
 
 const useModalContext = (): NoUndefinedField<ModalContextType> => {
 	const {
