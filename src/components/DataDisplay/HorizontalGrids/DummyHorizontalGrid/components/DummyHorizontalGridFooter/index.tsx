2132948cea87a8a57c16a5739599fe8df743fc53
DavidScicluna <dave_nostra@hotmail.com>
2023-05-01 16:07:46 +0200

    Fix: HorizontalGrids - DummyHorizontalGrid - DummyHorizontalGridFooter

--- src/components/DataDisplay/HorizontalGrids/DummyHorizontalGrid/components/DummyHorizontalGridFooter/index.tsx
+++ src/components/DataDisplay/HorizontalGrids/DummyHorizontalGrid/components/DummyHorizontalGridFooter/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { Center } from '@chakra-ui/react';
 
-import { DummyHorizontalGridFooterProps } from './types';
+import { DummyHorizontalGridFooterProps } from './common/types';
 
 const DummyHorizontalGridFooter: FC<DummyHorizontalGridFooterProps> = ({ children, ...rest }) => {
 	return (
