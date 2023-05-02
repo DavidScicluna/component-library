7dc704deb59f85f4501b03abd09a275a34e4ede3
DavidScicluna <dave_nostra@hotmail.com>
2023-05-02 21:15:57 +0200

    Fix: Cards - DummyCard

--- src/components/Cards/DummyCard/index.tsx
+++ src/components/Cards/DummyCard/index.tsx
@@ -1,29 +1,28 @@
 import { createContext, forwardRef, ReactElement } from 'react';
 
-import { Center, useColorMode, VStack } from '@chakra-ui/react';
+import { useColorMode, VStack } from '@chakra-ui/react';
 
-import merge from 'lodash-es/merge';
+import { merge } from 'lodash-es';
 
 import { useTheme } from '../../../common/hooks';
+import PushableOverlay from '../../Overlay/PushableOverlay';
 import {
 	color as defaultColor,
 	colorMode as defaultColorMode,
 	isDivisible as defaultIsDivisible,
-	isFullWidth as defaultIsFullWidth,
-	isLight as defaultIsLight,
 	spacing as defaultSpacing,
 	variant as defaultVariant
-} from '../common/data/defaultPropValues';
+} from '../common/default/props';
+import useStyles from '../common/styles';
 
-import useStyles from './common/styles';
+import { DummyCardContext as DummyCardContextType, DummyCardProps, DummyCardRef } from './common/types';
 import DummyCardDivider from './components/DummyCardDivider';
-import { DummyCardContext as DummyCardContextType, DummyCardProps, DummyCardRef } from './types';
 
 export const DummyCardContext = createContext<DummyCardContextType>({
 	color: defaultColor,
 	colorMode: defaultColorMode,
-	isLight: defaultIsLight,
-	spacing: defaultSpacing
+	spacing: defaultSpacing,
+	variant: defaultVariant
 });
 
 const DummyCard = forwardRef<DummyCardRef, DummyCardProps>(function DummyCard(props, ref): ReactElement {
@@ -34,41 +33,43 @@ const DummyCard = forwardRef<DummyCardRef, DummyCardProps>(function DummyCard(pr
 		children,
 		color = defaultColor,
 		colorMode = colorModeHook,
-		isFullWidth = defaultIsFullWidth,
 		isDivisible = defaultIsDivisible,
-		isLight = defaultIsLight,
 		spacing = defaultSpacing,
 		variant = defaultVariant,
 		sx,
 		...rest
 	} = props;
 
-	const style = useStyles({
-		theme,
-		color,
-		colorMode,
-		isFullWidth,
-		isLight,
-		variant
-	});
+	const style = useStyles({ theme, isClickable: false, isDisabled: true, isFixed: false });
 
 	return (
-		<DummyCardContext.Provider value={{ color, colorMode, isLight, spacing }}>
-			<Center {...rest} ref={ref} as='div' aria-disabled sx={merge(style.card, sx)}>
+		<DummyCardContext.Provider value={{ color, colorMode, spacing, variant }}>
+			<PushableOverlay
+				{...rest}
+				ref={ref}
+				as='div'
+				tabIndex={0}
+				borderRadius={variant === 'transparent' ? 'none' : 'xl'}
+				color={color}
+				colorMode={colorMode}
+				isDisabled
+				isPushable={false}
+				variant={variant}
+				sx={merge(style.card, sx)}
+				_disabled={style.disabled}
+				_active={style.active}
+			>
 				<VStack
 					width='100%'
 					height='100%'
-					position='relative'
-					zIndex={1}
-					flex={1}
-					divider={isDivisible && variant !== 'contained' ? <DummyCardDivider /> : undefined}
-					spacing={spacing}
 					overflowY='hidden'
 					overflowX='hidden'
+					divider={isDivisible ? <DummyCardDivider /> : undefined}
+					spacing={spacing}
 				>
 					{children}
 				</VStack>
-			</Center>
+			</PushableOverlay>
 		</DummyCardContext.Provider>
 	);
 });
--- src/components/Cards/DummyCard/types.ts
+++
@@ -1,7 +0,0 @@
-import { CommonCardProps } from '../common/types';
-
-export type DummyCardRef = HTMLDivElement | null;
-
-export type DummyCardProps = CommonCardProps;
-
-export type DummyCardContext = Pick<DummyCardProps, 'color' | 'colorMode' | 'isLight' | 'spacing'>;
