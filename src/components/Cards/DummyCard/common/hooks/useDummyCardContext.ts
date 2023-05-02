3ab0f53f15d51eeca174ecb5397ec8ea0b8bd5b4
DavidScicluna <dave_nostra@hotmail.com>
2023-05-02 21:15:28 +0200

    Fix: Cards - DummyCard - useDummyCardContext

--- src/components/Cards/DummyCard/common/hooks/useDummyCardContext.ts
+++ src/components/Cards/DummyCard/common/hooks/useDummyCardContext.ts
@@ -4,21 +4,21 @@ import { NoUndefinedField } from '../../../../../common/types';
 import {
 	color as defaultColor,
 	colorMode as defaultColorMode,
-	isLight as defaultIsLight,
-	spacing as defaultSpacing
-} from '../../../common/data/defaultPropValues';
+	spacing as defaultSpacing,
+	variant as defaultVariant
+} from '../../../common/default/props';
 import { DummyCardContext } from '../..';
-import { DummyCardContext as DummyCardContextType } from '../../types';
+import { DummyCardContext as DummyCardContextType } from '../types';
 
 const useDummyCardContext = (): NoUndefinedField<DummyCardContextType> => {
 	const {
 		color = defaultColor,
 		colorMode = defaultColorMode,
-		isLight = defaultIsLight,
-		spacing = defaultSpacing
+		spacing = defaultSpacing,
+		variant = defaultVariant
 	} = useContext<DummyCardContextType>(DummyCardContext);
 
-	return { color, colorMode, isLight, spacing };
+	return { color, colorMode, spacing, variant };
 };
 
 export default useDummyCardContext;
