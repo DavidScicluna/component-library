cfa0545d6d254de764f43587c770544e205ad8e8
DavidScicluna <dave_nostra@hotmail.com>
2023-05-02 21:14:03 +0200

    Fix: Cards - Renamed defaultPropValues

--- src/components/Cards/common/default/props/index.ts
+++ src/components/Cards/common/default/props/index.ts
@@ -1,15 +1,17 @@
 import { ColorMode } from '@chakra-ui/react';
 
-import { Space } from '../../../../theme/types';
-import { CardColor, CardVariant } from '../types';
+import { Space } from '../../../../../theme/types';
+import { CardColor, CardVariant } from '../../types';
 
 export const color: CardColor = 'gray';
 
 export const colorMode: ColorMode = 'light';
 
-export const isFullWidth = true;
+export const isActive = false;
+export const isClickable = false;
+export const isDisabled = false;
 export const isDivisible = true;
-export const isLight = true;
+export const isFixed = false;
 
 export const spacing: Space = 2;
 
