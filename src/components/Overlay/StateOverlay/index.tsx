cc32190121f75a6f94b37af9472b4a2d11164f33
DavidScicluna <dave_nostra@hotmail.com>
2023-05-04 22:47:30 +0200

    Fix: StateOverlay

--- src/components/Overlay/StateOverlay/index.tsx
+++ src/components/Overlay/StateOverlay/index.tsx
@@ -22,14 +22,7 @@ const StateOverlay: FC<StateOverlayProps> = (props) => {
 	return (
 		<Grid {...rest} templateColumns='1fr' alignItems='center' justifyContent='center' gap={0}>
 			{renderEmpty && (
-				<GridItem
-					as={Fade}
-					position='relative'
-					zIndex={1}
-					gridRowStart={1}
-					gridColumnStart={1}
-					in={state === 'empty'}
-				>
+				<GridItem as={Fade} zIndex={1} rowStart={1} colStart={1} in={state === 'empty'}>
 					{hasGlass ? (
 						<Glass width='100%' height='100%'>
 							{renderEmpty({ state })}
@@ -41,14 +34,7 @@ const StateOverlay: FC<StateOverlayProps> = (props) => {
 			)}
 
 			{renderError && (
-				<GridItem
-					as={Fade}
-					position='relative'
-					zIndex={1}
-					gridRowStart={1}
-					gridColumnStart={1}
-					in={state === 'error'}
-				>
+				<GridItem as={Fade} zIndex={1} rowStart={1} colStart={1} in={state === 'error'}>
 					{hasGlass ? (
 						<Glass width='100%' height='100%'>
 							{renderError({ state })}
@@ -60,14 +46,7 @@ const StateOverlay: FC<StateOverlayProps> = (props) => {
 			)}
 
 			{renderSpinner && (
-				<GridItem
-					as={Fade}
-					position='relative'
-					zIndex={1}
-					gridRowStart={1}
-					gridColumnStart={1}
-					in={state === 'loading'}
-				>
+				<GridItem as={Fade} zIndex={1} rowStart={1} colStart={1} in={state === 'loading'}>
 					{hasGlass ? (
 						<Glass width='100%' height='100%'>
 							{renderSpinner({ state })}
@@ -80,7 +59,6 @@ const StateOverlay: FC<StateOverlayProps> = (props) => {
 
 			<GridItem
 				as={Fade}
-				position='relative'
 				zIndex={0}
 				gridRowStart={1}
 				gridColumnStart={1}
