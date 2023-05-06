c3ed11bf03406f48521361a0703e247d46aef894
DavidScicluna <dave_nostra@hotmail.com>
2023-05-06 15:14:25 +0200

    Fix: Transitions

--- src/components/Transitions/common/utils/index.ts
+++ src/components/Transitions/common/utils/index.ts
@@ -1,29 +1,34 @@
 import { Transition, Tween } from 'framer-motion';
+import memoize from 'micro-memoize';
 
 import { convertEasingsToArray, convertStringToNumber } from '../../../../common/utils';
 import { Duration, Ease, Theme } from '../../../../theme/types';
 
 type GetDelayDurationProps = { theme: Theme; duration?: Duration };
 
-export const getDelay = ({ theme, duration = 'normal' }: GetDelayDurationProps): NonNullable<Transition['delay']> => {
-	return convertStringToNumber(theme.transition.duration[duration], 'ms') / 1000;
-};
+export const getDelay = memoize(
+	({ theme, duration = 'normal' }: GetDelayDurationProps): NonNullable<Transition['delay']> => {
+		return convertStringToNumber(theme.transition.duration[duration], 'ms') / 1000;
+	}
+);
 
-export const getDuration = ({ theme, duration = 'normal' }: GetDelayDurationProps): NonNullable<Tween['duration']> => {
-	return convertStringToNumber(theme.transition.duration[duration], 'ms') / 1000;
-};
+export const getDuration = memoize(
+	({ theme, duration = 'normal' }: GetDelayDurationProps): NonNullable<Tween['duration']> => {
+		return convertStringToNumber(theme.transition.duration[duration], 'ms') / 1000;
+	}
+);
 
 type GetEasingsProps = { theme: Theme; easing?: Ease };
 
-export const getEasings = ({ theme, easing = 'ease-in-out' }: GetEasingsProps): number[] => {
+export const getEasings = memoize(({ theme, easing = 'ease-in-out' }: GetEasingsProps): number[] => {
 	return convertEasingsToArray({ easing: theme.transition.easing[easing] });
-};
+});
 
 type GetConfigProps = { theme: Theme };
 
-export const getConfig = ({ theme }: GetConfigProps): Partial<Transition> => {
+export const getConfig = memoize(({ theme }: GetConfigProps): Partial<Transition> => {
 	const duration: Tween['duration'] = getDuration({ theme });
 	const easings: number[] = getEasings({ theme });
 
 	return { duration, easings };
-};
+});
