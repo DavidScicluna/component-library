5c5a661cb0301f06134a31995be85d7659f761cf
DavidScicluna <dave_nostra@hotmail.com>
2023-05-07 14:52:26 +0200

    Fix: Accordions - DummyAccordions - useDummyAccordionsContext

--- src/components/Accordions/DummyAccordions/common/hooks/useDummyAccordionsContext.ts
+++ src/components/Accordions/DummyAccordions/common/hooks/useDummyAccordionsContext.ts
@@ -1,26 +1,21 @@
 import { useContext } from 'react';
 
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../common/default/props';
 import { NoUndefinedField } from '../../../../../common/types';
-import {
-	color as defaultColor,
-	colorMode as defaultColorMode,
-	isFullWidth as defaultIsFullWidth,
-	spacing as defaultSpacing
-} from '../../../common/data/defaultPropValues';
+import { spacing as defaultSpacing } from '../../../common/default/props';
 import { DummyAccordionsContext } from '../..';
-import { accordions as defaultAccordions } from '../../common/data/defaultPropValues';
-import { DummyAccordionsContext as DummyAccordionsContextType } from '../../types';
+import { accordions as defaultAccordions } from '../../common/default/props';
+import { DummyAccordionsContext as DummyAccordionsContextType } from '../types';
 
 const useDummyAccordionsContext = (): NoUndefinedField<DummyAccordionsContextType> => {
 	const {
 		accordions = defaultAccordions,
 		color = defaultColor,
 		colorMode = defaultColorMode,
-		isFullWidth = defaultIsFullWidth,
 		spacing = defaultSpacing
 	} = useContext<DummyAccordionsContextType>(DummyAccordionsContext);
 
-	return { accordions, color, colorMode, isFullWidth, spacing };
+	return { accordions, color, colorMode, spacing };
 };
 
 export default useDummyAccordionsContext;
