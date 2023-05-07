4064723488cdcff993add8d759bf7b9e41ccd8f3
DavidScicluna <dave_nostra@hotmail.com>
2023-05-07 16:24:51 +0200

    Fix: Accordions - DummyAccordions - DummyQuickToggles

--- src/components/Accordions/DummyAccordions/components/DummyQuickToggles/index.tsx
+++ src/components/Accordions/DummyAccordions/components/DummyQuickToggles/index.tsx
@@ -1,96 +1,52 @@
-import { FC, useCallback } from 'react';
+import { FC } from 'react';
 
-import { Center, HStack, Text } from '@chakra-ui/react';
-
-import { useElementSize } from 'usehooks-ts';
+import { Grid, GridItem, useMediaQuery } from '@chakra-ui/react';
 
 import { useTheme } from '../../../../../common/hooks';
-import { convertREMToPixels, convertStringToNumber } from '../../../../../common/utils';
-import { getColor } from '../../../../../common/utils/color';
-import { Space } from '../../../../../theme/types';
 import Divider from '../../../../Divider';
-import HorizontalScroll from '../../../../HorizontalScroll';
 import { useDummyAccordionsContext } from '../../common/hooks';
 
 import { size as defaultSize } from './common/default/props';
 import { DummyQuickTogglesProps } from './common/types';
 import DummyAccordionButton from './components/DummyAccordionButton';
 import DummyToggleAllButton from './components/DummyToggleAllButton';
+import DummyToggleButtons from './components/DummyToggleButtons';
 
 const DummyQuickToggles: FC<DummyQuickTogglesProps> = (props) => {
 	const theme = useTheme();
 
-	const { accordions, color: defaultColor, colorMode, spacing: defaultSpacing } = useDummyAccordionsContext();
+	const [isMd] = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
 
-	const [borderRef, { width: borderWidth }] = useElementSize();
-	const [textRef, { width: textWidth }] = useElementSize();
-	const [toggleRef, { width: toggleWidth }] = useElementSize();
+	const { accordions, color: defaultColor, colorMode, spacing: defaultSpacing } = useDummyAccordionsContext();
 
 	const { color = defaultColor, spacing = defaultSpacing, size = defaultSize } = props;
 
-	const handleAccordionsWidth = useCallback((): string => {
-		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing as Space], 'rem') * 2);
-
-		return `calc(100% - ${toggleWidth + spacingWidth + borderWidth}px)`;
-	}, [theme.space, toggleWidth, borderWidth, spacing]);
-
-	const handleScrollWidth = useCallback((): string => {
-		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing as Space], 'rem'));
-
-		return `calc(100% - ${textWidth + spacingWidth}px)`;
-	}, [theme.space, textWidth, spacing]);
-
 	return (
-		<HStack
+		<Grid
 			width='100%'
+			templateColumns={isMd ? '100%' : 'minmax(0, 1fr) min-content min-content'}
+			templateRows={isMd ? 'repeat(3, max-content)' : 'minmax(0, 1fr)'}
 			alignItems='stretch'
+			justifyItems='stretch'
 			justifyContent='stretch'
-			divider={
-				<Center ref={borderRef} alignItems='stretch' justifyContent='stretch' border='none'>
-					<Divider colorMode={colorMode} orientation='vertical' />
-				</Center>
-			}
-			spacing={spacing}
+			gap={spacing}
 		>
-			<HStack width={handleAccordionsWidth()} alignItems='stretch' justifyContent='stretch' spacing={spacing}>
-				<Center ref={textRef}>
-					<Text
-						align='left'
-						color={getColor({ theme, colorMode, type: 'text.secondary' })}
-						fontSize={size}
-						whiteSpace='nowrap'
-						noOfLines={0}
-					>
-						Jump to:
-					</Text>
-				</Center>
+			<GridItem>
+				<DummyToggleButtons size={size} spacing={spacing}>
+					{accordions.map((accordion) => (
+						<DummyAccordionButton {...accordion} key={accordion.id} color={color} size={size} />
+					))}
+				</DummyToggleButtons>
+			</GridItem>
 
-				<Center width={handleScrollWidth()} height='100%'>
-					<HorizontalScroll
-						colorMode={colorMode}
-						isDisabled
-						renderDivider={({ padding }) => (
-							<Text
-								align='left'
-								color={getColor({ theme, colorMode, type: 'text.secondary' })}
-								fontSize='md'
-								px={padding}
-							>
-								•
-							</Text>
-						)}
-					>
-						{accordions.map((_dummy, index: number) => (
-							<DummyAccordionButton key={index} color={color} size={size} />
-						))}
-					</HorizontalScroll>
-				</Center>
-			</HStack>
+			<GridItem>
+				<Divider colorMode={colorMode} orientation={isMd ? 'horizontal' : 'vertical'} />
+			</GridItem>
 
-			<Center ref={toggleRef}>
-				<DummyToggleAllButton color={color} size={size} />
-			</Center>
-		</HStack>
+			<GridItem>
+				<DummyToggleAllButton color={color} isFullWidth={isMd} size={size} />
+			</GridItem>
+		</Grid>
 	);
 };
 
