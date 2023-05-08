a0fc86857e939fddefbfe21a90ca849d81fab22f
DavidScicluna <dave_nostra@hotmail.com>
2023-05-08 21:19:47 +0200

    Feature: HorizontalScroll - Arrows

---
+++ src/components/HorizontalScroll/components/Arrows/common/types/index.ts
@@ -0,0 +1,8 @@
+import { Nullable } from '../../../../../../common/types';
+import { HorizontalScrollAPIContext, HorizontalScrollProps } from '../../../../common/types';
+
+export type ArrowsProps = Pick<HorizontalScrollProps, 'LeftArrow' | 'RightArrow'> & {
+	scroll: HorizontalScrollAPIContext;
+};
+
+export type ArrowsRef = Nullable<HTMLDivElement>;
