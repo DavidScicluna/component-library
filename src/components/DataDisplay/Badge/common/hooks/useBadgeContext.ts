fa6b3bd18c06743b42c771adc1eb2bc251a6b72b
DavidScicluna <dave_nostra@hotmail.com>
2023-05-09 22:54:02 +0200

    Fix: Badge - useBadgeContext

--- src/components/DataDisplay/Badge/common/hooks/useBadgeContext.ts
+++ src/components/DataDisplay/Badge/common/hooks/useBadgeContext.ts
@@ -1,26 +1,20 @@
 import { useContext } from 'react';
 
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../common/default/props';
 import { NoUndefinedField } from '../../../../../common/types';
 import { BadgeContext } from '../..';
-import { BadgeContext as BadgeContextType } from '../../types';
-import {
-	color as defaultColor,
-	colorMode as defaultColorMode,
-	isLight as defaultIsLight,
-	size as defaultSize,
-	variant as defaultVariant
-} from '../data/defaultPropValues';
+import { size as defaultSize, variant as defaultVariant } from '../default/props';
+import { BadgeContext as BadgeContextType } from '../types';
 
 const useBadgeContext = (): NoUndefinedField<BadgeContextType> => {
 	const {
 		color = defaultColor,
 		colorMode = defaultColorMode,
-		isLight = defaultIsLight,
 		size = defaultSize,
 		variant = defaultVariant
 	} = useContext<BadgeContextType>(BadgeContext);
 
-	return { color, colorMode, isLight, size, variant };
+	return { color, colorMode, size, variant };
 };
 
 export default useBadgeContext;
