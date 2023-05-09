780b953b4173f127cbc91ba9143704748fc1cb16
DavidScicluna <dave_nostra@hotmail.com>
2023-05-09 23:03:03 +0200

    Fix: Badge - Added default values for CloseIconButton

--- src/components/DataDisplay/Badge/components/BadgeCloseIconButton/index.tsx
+++ src/components/DataDisplay/Badge/components/BadgeCloseIconButton/index.tsx
@@ -6,11 +6,18 @@ import { useBadgeContext } from '../../common/hooks';
 import { BadgeCloseIconButtonProps } from './common/types';
 
 const BadgeCloseIconButton: FC<BadgeCloseIconButtonProps> = (props) => {
-	const { color: defaultColor, colorMode } = useBadgeContext();
+	const { color: defaultColor, colorMode, size: defaultSize } = useBadgeContext();
 
-	const { color = defaultColor, ...rest } = props;
+	const {
+		color = defaultColor,
+		size = defaultSize === 'xs' || defaultSize === 'sm' || defaultSize === 'md' || defaultSize === 'lg'
+			? defaultSize
+			: 'xl',
+		variant = 'icon',
+		...rest
+	} = props;
 
-	return <CloseIconButton {...rest} color={color} colorMode={colorMode} />;
+	return <CloseIconButton {...rest} color={color} colorMode={colorMode} variant={variant} size={size} />;
 };
 
 export default BadgeCloseIconButton;
