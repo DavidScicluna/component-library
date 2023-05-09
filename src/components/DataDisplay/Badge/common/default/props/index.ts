1515ea9582049106cf5fa93cc67ade6ebe70dfa9
DavidScicluna <dave_nostra@hotmail.com>
2023-05-09 22:53:52 +0200

    Fix: Badge - Renamed defaultPropValues

---
+++ src/components/DataDisplay/Badge/common/default/props/index.ts
@@ -0,0 +1,13 @@
+import { BadgeSize, BadgeVariant } from '../../types';
+
+export const isActive = false;
+export const isClickable = false;
+export const isCompact = false;
+export const isDisabled = false;
+export const isFullWidth = false;
+export const isFixed = false;
+export const isRound = false;
+
+export const size: BadgeSize = 'xl';
+
+export const variant: BadgeVariant = 'contained';
