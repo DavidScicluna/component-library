a39fc18b477d3170af16644ed85a28cf22b493a7
DavidScicluna <dave_nostra@hotmail.com>
2023-05-11 22:23:56 +0200

    Fix: Badge - BadgeCloseIconButton

--- src/components/DataDisplay/Badge/components/BadgeCloseIconButton/index.tsx
+++ src/components/DataDisplay/Badge/components/BadgeCloseIconButton/index.tsx
@@ -1,23 +1,41 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
+import { useTheme } from '../../../../../common/hooks';
 import CloseIconButton from '../../../../Clickable/IconButtons/CloseIconButton';
+import { IconButtonSize } from '../../../../Clickable/IconButtons/common/types';
 import { useBadgeContext } from '../../common/hooks';
 
 import { BadgeCloseIconButtonProps } from './common/types';
 
 const BadgeCloseIconButton: FC<BadgeCloseIconButtonProps> = (props) => {
+	const theme = useTheme();
+
 	const { color: defaultColor, colorMode, size: defaultSize } = useBadgeContext();
 
-	const {
-		color = defaultColor,
-		size = defaultSize === 'xs' || defaultSize === 'sm' || defaultSize === 'md' || defaultSize === 'lg'
-			? defaultSize
-			: 'xl',
-		variant = 'icon',
-		...rest
-	} = props;
+	const { color = defaultColor, ...rest } = props;
 
-	return <CloseIconButton {...rest} color={color} colorMode={colorMode} variant={variant} size={size} />;
+	const size = useMemo((): IconButtonSize => {
+		return defaultSize === 'xs' || defaultSize === 'sm' || defaultSize === 'md' || defaultSize === 'lg'
+			? defaultSize
+			: 'xl';
+	}, [defaultSize]);
+
+	return (
+		<CloseIconButton
+			{...rest}
+			color={color}
+			colorMode={colorMode}
+			size={size}
+			variant='icon'
+			sx={{
+				'*, *::before, *::after': {
+					background: theme.colors.transparent,
+					backgroundColor: theme.colors.transparent,
+					borderColor: theme.colors.transparent
+				}
+			}}
+		/>
+	);
 };
 
 export default BadgeCloseIconButton;
