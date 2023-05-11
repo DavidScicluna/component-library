e71d78bf9ac1604b66d83d455e39e12ef397200f
DavidScicluna <dave_nostra@hotmail.com>
2023-05-11 22:23:42 +0200

    Fix: Icon

--- src/components/DataDisplay/Icon/common/default/sizes/index.tsx
+++ src/components/DataDisplay/Icon/common/default/sizes/index.tsx
@@ -1,1 +1,3 @@
-export const border = 2;
+export const border = 2; // In Pixels
+
+export const dimensions = 24; // In Pixels
--- src/components/DataDisplay/Icon/index.tsx
+++ src/components/DataDisplay/Icon/index.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, ReactElement } from 'react';
 
-import { Center } from '@chakra-ui/react';
+import { Center, useConst } from '@chakra-ui/react';
 
 import { merge } from 'lodash-es';
 
@@ -9,6 +9,7 @@ import { useProviderContext } from '../../Provider/common/hooks';
 import Skeleton from '../../Skeleton';
 
 import { category as defaultCategory, variant as defaultVariant } from './common/default/props';
+import { dimensions as defaultDimensions } from './common/default/sizes';
 import { useCheckIconFontStatus } from './common/hooks';
 import useStyles from './common/styles';
 import { IconProps, IconRef } from './common/types';
@@ -40,6 +41,8 @@ const Icon = forwardRef<IconRef, IconProps>(function Icon(props, ref): ReactElem
 
 	const style = useStyles({ theme, color, colorMode, variant });
 
+	const dimensions = useConst<string>(`${defaultDimensions}px`);
+
 	return (
 		<Skeleton color={color} colorMode={colorMode} isLoaded={hasIconLoaded[category]} variant='circle'>
 			<Center
@@ -47,12 +50,12 @@ const Icon = forwardRef<IconRef, IconProps>(function Icon(props, ref): ReactElem
 				ref={ref}
 				as='span'
 				className={`material-icons${category === 'outlined' ? '-outlined' : ''} ds-cl-icon`}
-				w={w || width || '24px'}
-				width={w || width || '24px'}
-				h={h || height || '24px'}
-				height={h || height || '24px'}
-				maxWidth={maxW || maxWidth || w || width || '24px'}
-				maxHeight={maxH || maxHeight || h || height || '24px'}
+				w={w || width || dimensions}
+				width={w || width || dimensions}
+				h={h || height || dimensions}
+				height={h || height || dimensions}
+				maxWidth={maxW || maxWidth || w || width || dimensions}
+				maxHeight={maxH || maxHeight || h || height || dimensions}
 				sx={variant !== 'unstyled' ? merge(style.icon, sx) : sx}
 			>
 				{icon}
