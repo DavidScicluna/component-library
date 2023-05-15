0cb0812d32c9367c2590a15eb0b33ed4c034df28
DavidScicluna <dave_nostra@hotmail.com>
2023-05-15 23:03:03 +0200

    Feature: PasswordStrengths - Types

---
+++ src/components/DataDisplay/PasswordStrengths/common/types/index.ts
@@ -0,0 +1,5 @@
+export type Strength = 'weak' | 'average' | 'strong' | 'excellent';
+export type Strengths = Strength[];
+
+export type StrengthType = 'lowercase' | 'uppercase' | 'symbol' | 'number';
+export type StrengthTypes = StrengthType[];
