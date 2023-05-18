fdc0faf939fad6bc1cce45cb31b377000e52eea5
DavidScicluna <dave_nostra@hotmail.com>
2023-05-18 22:07:42 +0200

    Feature: TabBars - HorizontalScrollArrows - HorizontalScrollRightArrow

---
+++ src/components/Navigation/TabBars/components/HorizontalScrollArrows/HorizontalScrollRightArrow/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+
+import {
+	useGetHorizontalScrollAPIContext,
+	useHorizontalScrollArrowState
+} from '../../../../../DataDisplay/HorizontalScroll/common/hooks';
+import HorizontalScrollOverlayArrow from '../../../../../DataDisplay/HorizontalScroll/components/OverlayArrow';
+
+const HorizontalScrollRightArrow: FC = () => {
+	const scroll = useGetHorizontalScrollAPIContext();
+	const { getNextItem, scrollToItem } = scroll || {};
+
+	const { isVisible } = useHorizontalScrollArrowState({ direction: 'right', scroll });
+
+	const handleScrollNext = (): void => {
+		const nextItem = getNextItem();
+		scrollToItem(nextItem?.entry?.target, 'smooth', 'nearest', 'nearest');
+	};
+
+	return <HorizontalScrollOverlayArrow direction='right' isVisible={isVisible} onClick={handleScrollNext} />;
+};
+
+export default HorizontalScrollRightArrow;
