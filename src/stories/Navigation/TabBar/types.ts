5e7f447bb180c4d2fd220b1e2abaf973cbc72f77
DavidScicluna <dave_nostra@hotmail.com>
2023-05-18 22:11:46 +0200

    Fix: Removed TabBar Story

--- src/stories/Navigation/TabBar/types.ts
+++
@@ -1,7 +0,0 @@
-import { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
-
-import TabBar from '../../../components/Navigation/TabBar';
-
-export type Meta = StorybookMeta<typeof TabBar>;
-
-export type Story = StorybookStory<typeof TabBar>;
