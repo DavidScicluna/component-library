42ec164c7c0015a08c4bf7031619438c0f5cc1ee
DavidScicluna <dave_nostra@hotmail.com>
2023-05-18 21:57:27 +0200

    Fix: Removed Old TabBar

--- src/components/Navigation/TabBar/common/data/defaultPropValues.ts
+++
@@ -1,9 +0,0 @@
-import { ColorMode } from '@chakra-ui/react';
-
-import { TabBarColor, TabBarDirection } from '../../types';
-
-export const color: TabBarColor = 'blue';
-
-export const colorMode: ColorMode = 'light';
-
-export const direction: TabBarDirection = 'bottom';
--- src/components/Navigation/TabBar/components/Tab/common/data/defaultPropValues.ts
+++
@@ -1,2 +0,0 @@
-export const isDisabled = false;
-export const isActive = false;
