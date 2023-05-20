7fcda90d25da60cf84a8e59bd80722e1c63a7fe3
DavidScicluna <dave_nostra@hotmail.com>
2023-05-20 14:57:30 +0200

    Feature: ColorSwitcher - ColorSwitcherScroll - ColorSwitcherScrollItem

---
+++ src/components/DataDisplay/ColorSwitcher/components/ColorSwitcherScroll/components/ColorSwitcherScrollItem/common/types/index.ts
@@ -0,0 +1,48 @@
+import { MouseEvent as ME } from 'react';
+
+import { CenterProps } from '@chakra-ui/react';
+
+import { Nullable } from '../../../../../../../../../common/types';
+import {
+	BoxBackground,
+	BoxBorders,
+	BoxColor,
+	BoxFilter,
+	BoxFlexbox,
+	BoxGradient,
+	BoxGrid,
+	BoxMargin,
+	BoxOther,
+	BoxPosition,
+	BoxPseudo,
+	BoxShadow,
+	BoxTypography
+} from '../../../../../../../../../common/types/box';
+import { TooltipProps } from '../../../../../../../../Overlay/Tooltip/common/types';
+import { ColorSwitcherContext } from '../../../../../../common/types';
+
+export type ColorSwitcherScrollItemMouseEvent = ME<HTMLDivElement, globalThis.MouseEvent>;
+
+type Omitted =
+	| BoxMargin
+	| BoxColor
+	| BoxGradient
+	| BoxTypography
+	| BoxFlexbox
+	| BoxGrid
+	| BoxBackground
+	| BoxBorders
+	| BoxPosition
+	| BoxShadow
+	| BoxFilter
+	| BoxPseudo
+	| BoxOther
+	| 'onChange';
+
+export type ColorSwitcherScrollItemProps = Pick<ColorSwitcherContext, 'color' | 'colorMode' | 'onChange'> & {
+	isActive?: boolean;
+	hasTooltip?: boolean;
+} & Omit<CenterProps, Omitted> &
+	Pick<TooltipProps, 'placement'>;
+
+export type ColorSwitcherScrollItemRef = Nullable<HTMLDivElement>;
---
+++ src/components/DataDisplay/ColorSwitcher/components/ColorSwitcherScroll/components/ColorSwitcherScrollItem/index.tsx
@@ -0,0 +1,101 @@
+import { forwardRef, ReactElement } from 'react';
+
+import { Center } from '@chakra-ui/react';
+
+import { capitalize, startCase } from 'lodash-es';
+
+import { useBoolean, useTheme } from '../../../../../../../common/hooks';
+import { getColor } from '../../../../../../../common/utils/color';
+import Tooltip from '../../../../../../Overlay/Tooltip';
+import ScaleFade from '../../../../../../Transitions/ScaleFade';
+import Icon from '../../../../../Icon';
+import { useColorSwitcherContext } from '../../../../common/hooks';
+
+import {
+	ColorSwitcherScrollItemMouseEvent,
+	ColorSwitcherScrollItemProps,
+	ColorSwitcherScrollItemRef
+} from './common/types';
+
+const ColorSwitcherScrollItem = forwardRef<ColorSwitcherScrollItemRef, ColorSwitcherScrollItemProps>(
+	function ColorSwitcherScrollItem(props, ref): ReactElement {
+		const theme = useTheme();
+
+		const { color: defaultColor, colorMode: defaultColorMode } = useColorSwitcherContext();
+
+		const {
+			color = defaultColor,
+			colorMode = defaultColorMode,
+			hasTooltip = true,
+			placement = 'top',
+			isActive = false,
+			onChange,
+			onClick,
+			onMouseEnter,
+			onMouseLeave,
+			...rest
+		} = props;
+
+		const [isHovering, setIsHovering] = useBoolean();
+
+		const handleClick = (event: ColorSwitcherScrollItemMouseEvent): void => {
+			onChange({ color });
+
+			if (onClick) {
+				onClick(event);
+			}
+		};
+
+		const handleMouseEnter = (event: ColorSwitcherScrollItemMouseEvent): void => {
+			if (hasTooltip) {
+				setIsHovering.on();
+			}
+
+			if (onMouseEnter) {
+				onMouseEnter(event);
+			}
+		};
+
+		const handleMouseLeave = (event: ColorSwitcherScrollItemMouseEvent): void => {
+			if (hasTooltip) {
+				setIsHovering.off();
+			}
+
+			if (onMouseLeave) {
+				onMouseLeave(event);
+			}
+		};
+
+		return (
+			<Tooltip
+				aria-label={`${color} (tooltip)`}
+				colorMode={colorMode}
+				label={startCase(capitalize(color))}
+				placement={placement}
+				isOpen={hasTooltip && isHovering}
+				isDisabled={!hasTooltip}
+			>
+				<Center
+					{...rest}
+					ref={ref}
+					onClick={handleClick}
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+					background={getColor({ theme, colorMode, color, type: 'color' })}
+				>
+					<ScaleFade in={isActive} unmountOnExit={false}>
+						<Icon
+							color={colorMode === 'light' ? 'white' : 'black'}
+							colorMode={colorMode}
+							icon='check'
+							category='filled'
+							variant='transparent'
+						/>
+					</ScaleFade>
+				</Center>
+			</Tooltip>
+		);
+	}
+);
+
+export default ColorSwitcherScrollItem;
