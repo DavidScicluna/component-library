41f92f8f9cb099e43a56824e9c2b3e8704030e50
DavidScicluna <dave_nostra@hotmail.com>
2023-05-21 16:17:41 +0200

    Fix: Provider

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,16 +1,18 @@
-import { createContext, FC } from 'react';
+import { createContext, FC, useState } from 'react';
 
-import { ChakraProvider, ColorModeScript, useConst } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript as CUIColorModeScript, useConst } from '@chakra-ui/react';
 
 import { merge } from 'lodash-es';
 
 import { color as defaultColor, colorMode as defaultColorMode } from '../../common/default/props';
+import { AppColorMode } from '../../common/types';
 import defaultTheme from '../../theme';
 import { Theme } from '../../theme/types';
 import TransitionsProvider from '../Transitions/TransitionsProvider';
 
-import { useCheckColorMode } from './common/hooks';
 import { ProviderContext as ProviderContextType, ProviderProps } from './common/types';
+import ColorModeScript from './components/ColorModeScript';
+import ColorScript from './components/ColorScript';
 
 export const ProviderContext = createContext<ProviderContextType>({
 	color: defaultColor,
@@ -18,19 +20,24 @@ export const ProviderContext = createContext<ProviderContextType>({
 });
 
 const Provider: FC<ProviderProps> = (props) => {
-	const { children, color = defaultColor, colorMode: colorModeProp = defaultColorMode, theme: themeProp } = props;
+	const { children, color = defaultColor, colorMode: initialColorMode = defaultColorMode, theme: themeProp } = props;
 
 	const theme = useConst<Theme>(merge(defaultTheme, themeProp) as Theme);
 
-	const colorMode = useCheckColorMode({ colorMode: colorModeProp });
+	const [colorMode, setColorMode] = useState<AppColorMode>(defaultColorMode);
 
 	// TODO: Remove console.log
 	console.log(theme);
 
 	return (
 		<ChakraProvider theme={theme} resetCSS>
-			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<CUIColorModeScript type='cookie' initialColorMode={initialColorMode} />
 			<TransitionsProvider>
+				<ColorScript initialColor={color} />
+				<ColorModeScript
+					initialColorMode={initialColorMode}
+					onSetColorMode={(colorMode) => setColorMode(colorMode)}
+				/>
 				<ProviderContext.Provider value={{ color, colorMode }}>{children}</ProviderContext.Provider>
 			</TransitionsProvider>
 		</ChakraProvider>
