821b0dab4e6d5b178999e26549efa7f161697ab3
DavidScicluna <dave_nostra@hotmail.com>
2023-05-26 20:20:12 +0200

    Feature: useGetColor hook

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,6 +1,7 @@
 import useBoolean from './useBoolean';
 import useConst from './useConst';
 import useDebounce from './useDebounce';
+import useGetColor from './useGetColor';
 import useOs from './useOs';
 import useTheme from './useThemeTyped';
 
---
+++ src/common/hooks/useGetColor.ts
@@ -0,0 +1,23 @@
+import { useMemo } from 'react';
+
+import { useDSCLProviderContext, useTheme } from '../..';
+import { CommonThemeProps } from '../types';
+import { getColor, GetColorProps } from '../utils/color';
+
+type UseGetColorProps = Pick<GetColorProps, 'type'> & CommonThemeProps;
+
+const useGetColor = (props: UseGetColorProps): string => {
+	const theme = useTheme();
+
+	const { color: defaultColor, colorMode: defaultColorMode } = useDSCLProviderContext();
+
+	const { color = defaultColor, colorMode = defaultColorMode, type } = props;
+
+	const string = useMemo<string>(() => {
+		return getColor({ theme, colorMode, color, type });
+	}, []);
+
+	return string;
+};
+
+export default useGetColor;
