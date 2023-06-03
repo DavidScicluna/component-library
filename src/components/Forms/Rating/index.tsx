c6fba3fc60b280c895b757462fd275757181d382
DavidScicluna <dave_nostra@hotmail.com>
2023-06-03 16:54:25 +0200

    Fix: Rating - Default props

--- src/components/Forms/Rating/index.tsx
+++ src/components/Forms/Rating/index.tsx
@@ -6,40 +6,49 @@ import { range } from 'lodash-es';
 
 import { Undefinable } from '../../../common/types';
 import { useProviderContext } from '../../Provider/common/hooks';
+import {
+	isDisabled as defaultIsDisabled,
+	isError as defaultIsError,
+	isReadOnly as defaultIsReadOnly,
+	isRequired as defaultIsRequired,
+	isSuccess as defaultIsSuccess,
+	isWarning as defaultIsWarning,
+	size as defaultSize
+} from '../FormControl/common/default/props';
 import { useFormControlContext } from '../FormControl/common/hooks';
 
 import { RatingProps, RatingRef } from './common/types';
 import RatingIcon from './components/RatingIcon';
 
 const Rating = forwardRef<RatingRef, RatingProps>(function Rating(props, ref): ReactElement {
-	const { color: appColor, colorMode: appColorMode } = useProviderContext();
+	const { color: defaultColor, colorMode: defaultColorMode } = useProviderContext();
 
 	const {
-		color: defaultColor = appColor,
-		colorMode: defaultColorMode = appColorMode,
-		isDisabled: defaultIsDisabled,
-		isError: defaultIsError,
-		isRequired: defaultIsRequired,
-		isReadOnly: defaultIsReadOnly,
-		isSuccess: defaultIsSuccess,
-		isWarning: defaultIsWarning,
-		size: defaultSize
+		color: formControlColor,
+		colorMode: formControlColorMode,
+		isDisabled: formControlIsDisabled,
+		isError: formControlIsError,
+		isReadOnly: formControlIsReadOnly,
+		isRequired: formControlIsRequired,
+		isSuccess: formControlIsSuccess,
+		isWarning: formControlIsWarning,
+		size: formControlSize
 	} = useFormControlContext();
 
 	const {
-		color = defaultColor,
-		colorMode = defaultColorMode,
+		color = formControlColor || defaultColor,
+		colorMode = formControlColorMode || defaultColorMode,
 		icons,
 		hasTrailingHover = true,
-		isDisabled = defaultIsDisabled,
-		isError = defaultIsError,
-		isRequired = defaultIsRequired,
-		isReadOnly = defaultIsReadOnly,
-		isSuccess = defaultIsSuccess,
-		isWarning = defaultIsWarning,
+		isDisabled = formControlIsDisabled || defaultIsDisabled,
+		isError = formControlIsError || defaultIsError,
+		isRequired = formControlIsRequired || defaultIsRequired,
+		isReadOnly = formControlIsReadOnly || defaultIsReadOnly,
+		isSuccess = formControlIsSuccess || defaultIsSuccess,
+		isWarning = formControlIsWarning || defaultIsWarning,
 		count = 10,
 		onChange,
-		size = defaultSize,
+		size = formControlSize || defaultSize,
 		value,
 		...rest
 	} = props;
