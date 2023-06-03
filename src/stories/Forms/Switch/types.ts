bc2035f2f4ec971d87e30d36a05aac20dfe9fdb9
DavidScicluna <dave_nostra@hotmail.com>
2023-06-03 14:31:45 +0200

    Feature: Switch Story

---
+++ src/stories/Forms/Switch/types.ts
@@ -0,0 +1,7 @@
+import { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
+
+import Switch from '../../../components/Forms/Switch';
+
+export type Meta = StorybookMeta<typeof Switch>;
+
+export type Story = StorybookStory<typeof Switch>;
