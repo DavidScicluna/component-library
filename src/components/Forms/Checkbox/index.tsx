4c3fed2f2a2df058a81ca28222d15ae8e10c570e
DavidScicluna <dave_nostra@hotmail.com>
2023-06-03 16:52:13 +0200

    Fix: Checkbox - Default props

--- src/components/Forms/Checkbox/index.tsx
+++ src/components/Forms/Checkbox/index.tsx
@@ -12,7 +12,15 @@ import { Radius } from '../../../theme/types';
 import Icon from '../../DataDisplay/Icon';
 import PushableOverlay from '../../Overlay/PushableOverlay';
 import { useProviderContext } from '../../Provider/common/hooks';
-import { size as defaultSize } from '../FormControl/common/default/props';
+import {
+	isDisabled as defaultIsDisabled,
+	isError as defaultIsError,
+	isReadOnly as defaultIsReadOnly,
+	isRequired as defaultIsRequired,
+	isSuccess as defaultIsSuccess,
+	isWarning as defaultIsWarning,
+	size as defaultSize
+} from '../FormControl/common/default/props';
 import { useFormControlContext } from '../FormControl/common/hooks';
 
 import {
@@ -35,40 +43,40 @@ export const CheckboxContext = createContext<CheckboxContextType>({
 const Checkbox = forwardRef<CheckboxRef, CheckboxProps>(function Checkbox(props, ref): ReactElement {
 	const theme = useTheme();
 
-	const { color: appColor, colorMode: appColorMode } = useProviderContext();
+	const { color: defaultColor, colorMode: defaultColorMode } = useProviderContext();
 
 	const {
-		color: defaultColor = appColor,
-		colorMode: defaultColorMode = appColorMode,
-		isDisabled: defaultIsDisabled,
-		isError: defaultIsError,
-		isReadOnly: defaultIsReadOnly,
-		isRequired: defaultIsRequired,
-		isSuccess: defaultIsSuccess,
-		isWarning: defaultIsWarning,
-		size: defaultSize
+		color: formControlColor,
+		colorMode: formControlColorMode,
+		isDisabled: formControlIsDisabled,
+		isError: formControlIsError,
+		isReadOnly: formControlIsReadOnly,
+		isRequired: formControlIsRequired,
+		isSuccess: formControlIsSuccess,
+		isWarning: formControlIsWarning,
+		size: formControlSize
 	} = useFormControlContext();
 
 	const [checkboxRef, { width: checkboxWidth, height: checkboxHeight }] = useElementSize();
 
 	const {
-		color = defaultColor,
-		colorMode = defaultColorMode,
+		color = formControlColor || defaultColor,
+		colorMode = formControlColorMode || defaultColorMode,
 		renderLeft,
 		renderRight,
 		defaultChecked = defaultIsChecked,
 		isChecked: isCheckedProp = defaultIsChecked,
 		isCompact = defaultIsCompact,
-		isDisabled: isDisabledProp = defaultIsDisabled,
-		isError = defaultIsError,
+		isDisabled: isDisabledProp = formControlIsDisabled || defaultIsDisabled,
+		isError = formControlIsError || defaultIsError,
 		isIndeterminate: isIndeterminateProp = defaultIsIndeterminate,
-		isReadOnly: isReadOnlyProp = defaultIsReadOnly,
-		isRequired: isRequiredProp = defaultIsRequired,
+		isReadOnly: isReadOnlyProp = formControlIsReadOnly || defaultIsReadOnly,
+		isRequired: isRequiredProp = formControlIsRequired || defaultIsRequired,
 		isRound = defaultIsRound,
-		isSuccess = defaultIsSuccess,
-		isWarning = defaultIsWarning,
+		isSuccess = formControlIsSuccess || defaultIsSuccess,
+		isWarning = formControlIsWarning || defaultIsWarning,
 		onChange,
-		size = defaultSize,
+		size = formControlSize || defaultSize,
 		variant = defaultVariant,
 		sx,
 		...rest
