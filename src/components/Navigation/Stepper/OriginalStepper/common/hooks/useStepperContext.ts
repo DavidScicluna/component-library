993bb9dbcbd16e0ae9be8bf5b2c65c4103965c9a
DavidScicluna <dave_nostra@hotmail.com>
2023-06-11 22:15:10 +0200

    Feature: Stepper - OriginalStepper - useStepperContext hook

---
+++ src/components/Navigation/Stepper/OriginalStepper/common/hooks/useStepperContext.ts
@@ -0,0 +1,52 @@
+import { useContext } from 'react';
+
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode,
+	method as defaultOnChange
+} from '../../../../../../common/default/props';
+import { NoUndefinedField } from '../../../../../../common/types';
+import {
+	activeStep as defaultActiveStep,
+	isConsecutively as defaultIsConsecutively,
+	isDisabled as defaultIsDisabled,
+	isFitted as defaultIsFitted,
+	// orientation as defaultOrientation,
+	size as defaultSize,
+	totalSteps as defaultTotalSteps,
+	variant as defaultVariant
+} from '../../../common/default/props';
+import { StepperContext } from '../..';
+import { StepperContext as StepperContextType } from '../types';
+
+const useStepperContext = (): NoUndefinedField<StepperContextType> => {
+	const {
+		activeStep = defaultActiveStep,
+		totalSteps = defaultTotalSteps,
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		isConsecutively = defaultIsConsecutively,
+		isDisabled = defaultIsDisabled,
+		isFitted = defaultIsFitted,
+		// orientation = defaultOrientation,
+		onChange = defaultOnChange,
+		size = defaultSize,
+		variant = defaultVariant
+	} = useContext<StepperContextType>(StepperContext);
+
+	return {
+		activeStep,
+		totalSteps,
+		color,
+		colorMode,
+		isConsecutively,
+		isDisabled,
+		isFitted,
+		// orientation,
+		onChange,
+		size,
+		variant
+	};
+};
+
+export default useStepperContext;
