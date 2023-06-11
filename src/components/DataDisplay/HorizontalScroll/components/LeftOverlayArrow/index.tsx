bdbf311152b8de19b96cbf061a97685550d0d79f
DavidScicluna <dave_nostra@hotmail.com>
2023-06-11 20:39:52 +0200

    Fix: HorizontalScroll - LeftOverlayArrow - Added scrollAmount prop

--- src/components/DataDisplay/HorizontalScroll/components/LeftOverlayArrow/index.tsx
+++ src/components/DataDisplay/HorizontalScroll/components/LeftOverlayArrow/index.tsx
@@ -4,13 +4,29 @@ import { VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { useHorizontalScrollArrowState } from '../../common/hooks';
 import OverlayArrow from '../OverlayArrow';
 
-const LeftOverlayArrow: FC = () => {
+import { LeftOverlayArrowProps } from './common/types';
+
+const LeftOverlayArrow: FC<LeftOverlayArrowProps> = ({ scrollAmount = 'multiple' }) => {
 	const scroll = useContext(VisibilityContext);
-	const { scrollPrev } = scroll;
+	const { getPrevItem, scrollToItem, scrollPrev } = scroll;
 
 	const { isVisible } = useHorizontalScrollArrowState({ direction: 'left', scroll });
 
-	return <OverlayArrow direction='left' isVisible={isVisible} onClick={() => scrollPrev()} />;
+	const handleScrollPrev = (): void => {
+		switch (scrollAmount) {
+			case 'single': {
+				const prevItem = getPrevItem();
+				scrollToItem(prevItem?.entry?.target, 'smooth', 'nearest', 'nearest');
+				break;
+			}
+			case 'multiple': {
+				scrollPrev();
+				break;
+			}
+		}
+	};
+
+	return <OverlayArrow direction='left' isVisible={isVisible} onClick={handleScrollPrev} />;
 };
 
 export default LeftOverlayArrow;
