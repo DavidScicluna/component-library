abea6d828dd6ddebfa1c24295a2ffad944c3ab69
DavidScicluna <dave_nostra@hotmail.com>
2023-06-11 22:17:54 +0200

    Feature: Stepper - OriginalStepper - StepList

---
+++ src/components/Navigation/Stepper/OriginalStepper/components/StepList/index.tsx
@@ -0,0 +1,113 @@
+import { FC } from 'react';
+
+import { Center, Grid, GridItem } from '@chakra-ui/react';
+
+import { compact } from 'lodash-es';
+import { useElementSize } from 'usehooks-ts';
+
+import { useGetColor } from '../../../../../../common/hooks';
+import HorizontalScroll from '../../../../../DataDisplay/HorizontalScroll';
+import { useSetHorizontalScrollAPIContext } from '../../../../../DataDisplay/HorizontalScroll/common/hooks';
+import LeftOverlayArrow from '../../../../../DataDisplay/HorizontalScroll/components/LeftOverlayArrow';
+import RightOverlayArrow from '../../../../../DataDisplay/HorizontalScroll/components/RightOverlayArrow';
+import { useStepperContext } from '../../common/hooks';
+
+import { StepListProps } from './common/types';
+import Step from './components/Step';
+
+const StepList: FC<StepListProps> = (props) => {
+	const { activeStep, color, colorMode, isFitted, onChange, variant } = useStepperContext();
+
+	const { steps = [], renderLeft, renderRight, ...rest } = props;
+
+	const [childrenRef, { width: childrenWidth, height: childrenHeight }] = useElementSize();
+
+	const [scroll, setScroll] = useSetHorizontalScrollAPIContext();
+
+	const borderColor = useGetColor({ color: 'gray', colorMode, type: 'divider' });
+
+	const handleScrollToStep = (index: number): void => {
+		if (scroll) {
+			const scrollElement = scroll.getItemByIndex(index);
+
+			if (scrollElement) {
+				scroll.scrollToItem?.(scrollElement, 'smooth', 'center', 'nearest');
+			}
+		}
+	};
+
+	const handleStepClick = (index: number): void => {
+		if (activeStep !== index) {
+			if (onChange && typeof onChange === 'function') {
+				onChange({ index });
+			}
+
+			handleScrollToStep(index);
+		}
+	};
+
+	return (
+		<Grid
+			{...rest}
+			width='100%'
+			height='100%'
+			templateColumns={compact([renderLeft ? 'auto' : null, '1fr', renderRight ? 'auto' : null]).join(' ')}
+			templateRows='1fr'
+			autoFlow='row'
+			alignItems='stretch'
+			alignContent='stretch'
+			justifyItems='stretch'
+			justifyContent='stretch'
+			gap={0}
+			borderTopWidth={`${variant === 'outlined' ? 2 : 0}px`}
+			borderBottomWidth={`${variant === 'outlined' ? 2 : 0}px`}
+			borderStyle='solid'
+			borderColor={borderColor}
+			sx={{
+				'& .react-horizontal-scrolling-menu--inner-wrapper': { width: '100%' },
+				'& .react-horizontal-scrolling-menu--item': isFitted ? { width: '100%' } : {}
+			}}
+		>
+			{renderLeft ? (
+				<GridItem>
+					<Center width='100%' height='100%'>
+						{renderLeft({ color, colorMode, width: childrenWidth, height: childrenHeight })}
+					</Center>
+				</GridItem>
+			) : null}
+
+			<GridItem ref={childrenRef}>
+				<HorizontalScroll
+					width='100%'
+					height='100%'
+					colorMode={colorMode}
+					LeftArrow={LeftOverlayArrow}
+					RightArrow={RightOverlayArrow}
+					onInit={setScroll}
+					onUpdate={setScroll}
+				>
+					{steps.map((step, index) => (
+						<Step
+							{...step}
+							key={`ds-cl-stepper-step-${index}`}
+							panelId={`ds-cl-stepper-step-${index}`}
+							index={index}
+							isSelected={step.isSelected || index === activeStep}
+							onSelect={handleStepClick}
+						/>
+					))}
+				</HorizontalScroll>
+			</GridItem>
+
+			{renderRight ? (
+				<GridItem>
+					<Center width='100%' height='100%'>
+						{renderRight({ color, colorMode, width: childrenWidth, height: childrenHeight })}
+					</Center>
+				</GridItem>
+			) : null}
+		</Grid>
+	);
+};
+
+export default StepList;
