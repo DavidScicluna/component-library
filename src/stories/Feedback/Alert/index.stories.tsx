9b2c5d6d8642fd5fe6754bf8b2c992607d9ceaa0
DavidScicluna <dave_nostra@hotmail.com>
2023-06-15 17:27:04 +0200

    Fix: Alert Story

--- src/stories/Feedback/Alert/index.stories.tsx
+++ src/stories/Feedback/Alert/index.stories.tsx
@@ -4,13 +4,22 @@ import { HStack } from '@chakra-ui/react';
 
 import { capitalize } from 'lodash-es';
 
-import { Alert as AlertComponent, AlertProps, AlertStatus, DummyButton } from '../../..';
+import { Alert as AlertComponent, AlertDuration, AlertProps, AlertStatus, DummyButton } from '../../..';
+import {
+	duration as defaultDuration,
+	spacing as defaultSpacing
+} from '../../../components/Feedback/Alert/common/default/props';
 import controls from '../../common/controls';
 import parameters from '../../common/parameters';
 
 import { Meta, Story } from './types';
 
 const defaultStatus: AlertStatus = 'info';
+
+const durationOptions: AlertDuration[] = [
+	5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17,
+	17.5, 18, 18.5, 19, 19.5, 20
+];
 const statusOptions: AlertStatus[] = ['info', 'warning', 'success', 'error'];
 
 export default {
@@ -18,6 +27,15 @@ export default {
 	component: AlertComponent,
 	argTypes: {
 		colorMode: { ...controls.theme.colorMode },
+		duration: {
+			name: 'Duration',
+			type: 'number',
+			defaultValue: defaultDuration,
+			defaultOption: defaultDuration,
+			// description: '',
+			options: [...durationOptions],
+			control: 'select'
+		},
 		label: {
 			name: 'Label',
 			type: 'string',
@@ -32,7 +50,7 @@ export default {
 			// description: '',
 			control: 'text'
 		},
-		spacing: { ...controls.theme.spacing, defaultValue: 2 },
+		spacing: { ...controls.theme.spacing, defaultValue: defaultSpacing },
 		status: {
 			name: 'Status',
 			type: 'string',
