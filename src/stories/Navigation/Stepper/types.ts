d0a052df080441efe4b3380caf356c89da090dd3
DavidScicluna <dave_nostra@hotmail.com>
2023-06-27 20:38:15 +0200

    Feature: Stepper Story

---
+++ src/stories/Navigation/Stepper/types.ts
@@ -0,0 +1,7 @@
+import { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
+
+import Stepper from '../../../components/Navigation/Stepper/OriginalStepper';
+
+export type Meta = StorybookMeta<typeof Stepper>;
+
+export type Story = StorybookStory<typeof Stepper>;
