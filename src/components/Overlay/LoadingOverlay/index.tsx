9d4b82b07d4cce282c1b361dfd9e08deb5b2cf62
DavidScicluna <dave_nostra@hotmail.com>
2023-06-27 20:37:58 +0200

    Fix: LoadingOverlay

--- src/components/Overlay/LoadingOverlay/index.tsx
+++ src/components/Overlay/LoadingOverlay/index.tsx
@@ -31,40 +31,39 @@ const LoadingOverlay = forwardRef<LoadingOverlayRef, LoadingOverlayProps>(functi
 	const background = useGetColor({ color, colorMode, type: colorMode === 'light' ? 'darkest' : 'lightest' });
 
 	return (
-		<Grid
-			{...rest}
-			ref={ref}
-			templateColumns='1fr'
-			templateRows='1fr'
-			alignItems='stretch'
-			justifyItems='stretch'
-			justifyContent='stretch'
-			overflow='overlay'
-			gap={0}
-		>
-			<GridItem as={Fade} zIndex={1} rowStart={1} colStart={1} in={isLoading}>
-				{hasGlass ? (
-					<Glass
-						width='100%'
-						height='100%'
-						sx={{ background: hasBackdrop ? background : theme.colors.transparent }}
-					>
-						{renderSpinner()}
-					</Glass>
-				) : (
-					<Center
-						width='100%'
-						height='100%'
-						sx={{ background: hasBackdrop ? background : theme.colors.transparent }}
-					>
-						{renderSpinner()}
-					</Center>
-				)}
-			</GridItem>
-			<GridItem rowStart={1} colStart={1}>
-				{children}
-			</GridItem>
-		</Grid>
+		<Center {...rest} ref={ref} display='inline-block'>
+			<Grid
+				templateColumns='1fr'
+				templateRows='1fr'
+				alignItems='stretch'
+				justifyItems='stretch'
+				justifyContent='stretch'
+				gap={0}
+			>
+				<GridItem as={Fade} zIndex={1} rowStart={1} colStart={1} in={isLoading}>
+					{hasGlass ? (
+						<Glass
+							width='100%'
+							height='100%'
+							sx={{ background: hasBackdrop ? background : theme.colors.transparent }}
+						>
+							{renderSpinner()}
+						</Glass>
+					) : (
+						<Center
+							width='100%'
+							height='100%'
+							sx={{ background: hasBackdrop ? background : theme.colors.transparent }}
+						>
+							{renderSpinner()}
+						</Center>
+					)}
+				</GridItem>
+				<GridItem rowStart={1} colStart={1}>
+					{children}
+				</GridItem>
+			</Grid>
+		</Center>
 	);
 });
 
