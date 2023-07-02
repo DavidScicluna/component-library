1af15da977770d6199fbb8a1c2ef533f19e6da06
DavidScicluna <dave_nostra@hotmail.com>
2023-07-02 18:58:52 +0200

    Fix: Divider

--- src/components/DataDisplay/Divider/index.tsx
+++ src/components/DataDisplay/Divider/index.tsx
@@ -29,7 +29,9 @@ const Divider = forwardRef<DividerRef, DividerProps>(function Divider(props, ref
 		return {
 			flex: '1 1 0%',
 
-			borderTopWidth: orientation === 'horizontal' ? `${size}px` : '0px',
+			borderWidth: `${[orientation === 'horizontal' ? size : 0, 0, 0, 0]
+				.map((w) => `${w}px`)
+				.join(' ')} !important`,
 			borderTopStyle: variant,
 			borderTopColor: borderColor
 		};
@@ -40,14 +42,14 @@ const Divider = forwardRef<DividerRef, DividerProps>(function Divider(props, ref
 			{...rest}
 			ref={ref}
 			width={orientation === 'horizontal' ? '100%' : `${size}px`}
-			height={orientation === 'vertical' ? '100%' : `${size}px`}
+			height={orientation === 'vertical' ? 'auto' : `${size}px`}
 			display='flex'
 			alignItems='center'
 			justifyContent={placement === 'left' ? 'flex-start' : placement === 'right' ? 'flex-end' : 'center'}
 			sx={
 				orientation === 'vertical'
 					? {
-							borderLeftWidth: `${size}px`,
+							borderWidth: `${[0, 0, 0, size].map((w) => `${w}px`).join(' ')} !important`,
 							borderLeftStyle: variant,
 							borderLeftColor: borderColor
 					  }
@@ -55,12 +57,12 @@ const Divider = forwardRef<DividerRef, DividerProps>(function Divider(props, ref
 			}
 			_before={
 				orientation === 'horizontal' && (placement === 'right' || placement === 'center')
-					? { ...styles, content: '""', mr: spacing }
+					? { ...styles, content: '""', mr: children ? spacing : 0 }
 					: undefined
 			}
 			_after={
 				orientation === 'horizontal' && (placement === 'left' || placement === 'center')
-					? { ...styles, content: '""', ml: spacing }
+					? { ...styles, content: '""', ml: children ? spacing : 0 }
 					: undefined
 			}
 		>
