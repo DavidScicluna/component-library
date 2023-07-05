e270a9422b8fa60b4c77a98c1bc4f8e82b50cd72
DavidScicluna <dave_nostra@hotmail.com>
2023-07-05 22:34:47 +0200

    Feature: Added ResponsiveValue utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,7 +1,8 @@
+import { isArray, isObject } from 'lodash-es';
 import memoize from 'micro-memoize';
 
 import { FontSize, LineHeight, Theme } from '../../theme/types';
-import { OS } from '../types';
+import { Breakpoint, OS, ResponsiveValue } from '../types';
 
 /**
  * This method will get the OS type of the user's device
@@ -85,6 +86,7 @@ export const getFontSizeHeight = memoize((props: GetFontSizeHeightProps): number
 
 type ConvertEasingsToArrayProps = { easing: string };
 
+// TODO: Convert all utils if only 1 param dont use object
 export const convertEasingsToArray = memoize(({ easing }: ConvertEasingsToArrayProps): number[] =>
 	easing
 		.replaceAll('cubic-bezier', '')
@@ -94,3 +96,92 @@ export const convertEasingsToArray = memoize(({ easing }: ConvertEasingsToArrayP
 		.split(',')
 		.map((number) => Number(number))
 );
+
+/**
+ * This method will check which type the responsive value is if its an array, object or default value
+ *
+ * @param value - ResponsiveValue
+ * @returns - 'array' | 'object' | 'default'
+ */
+export const checkResponsiveValue = memoize(<D>(value: ResponsiveValue<D>): 'array' | 'object' | 'default' => {
+	if (isArray(value)) {
+		return 'array';
+	} else if (isObject(value)) {
+		return 'object';
+	} else {
+		return 'default';
+	}
+});
+
+/**
+ * This method will return the appropriate item depending on current dimensions of the users device
+ *
+ * @param value - ResponsiveValue
+ * @returns - The item with the array or object that aligns with the breakpoint
+ */
+export const getResponsiveValue = memoize(<D>(value: ResponsiveValue<D>): D => {
+	const breakpoint: Breakpoint = globalThis?.window?.matchMedia('(min-width: 1536px)')
+		? '2xl'
+		: globalThis?.window?.matchMedia('(min-width: 1280px)')
+		? 'xl'
+		: globalThis?.window?.matchMedia('(min-width: 1024px)')
+		? 'lg'
+		: globalThis?.window?.matchMedia('(min-width: 768px)')
+		? 'md'
+		: globalThis?.window?.matchMedia('(min-width: 640px)')
+		? 'sm'
+		: 'xs';
+
+	if (isArray(value)) {
+		switch (value.length) {
+			case 1:
+				return value[0] as D;
+			case 2:
+				return (breakpoint === 'xs' ? value[0] : value[1]) as D;
+			case 3:
+				return (breakpoint === 'xs' ? value[0] : breakpoint === 'sm' ? value[1] : value[2]) as D;
+			case 4:
+				return (
+					breakpoint === 'xs'
+						? value[0]
+						: breakpoint === 'sm'
+						? value[1]
+						: breakpoint === 'md'
+						? value[2]
+						: value[3]
+				) as D;
+			case 5:
+				return (
+					breakpoint === 'xs'
+						? value[0]
+						: breakpoint === 'sm'
+						? value[1]
+						: breakpoint === 'md'
+						? value[2]
+						: breakpoint === 'lg'
+						? value[3]
+						: value[4]
+				) as D;
+			case 6:
+				return (
+					breakpoint === 'xs'
+						? value[0]
+						: breakpoint === 'sm'
+						? value[1]
+						: breakpoint === 'md'
+						? value[2]
+						: breakpoint === 'lg'
+						? value[3]
+						: breakpoint === 'xl'
+						? value[4]
+						: value[5]
+				) as D;
+		}
+	} else if (isObject(value)) {
+		return value[breakpoint];
+	} else {
+		return value;
+	}
+
+	return value as D;
+});
