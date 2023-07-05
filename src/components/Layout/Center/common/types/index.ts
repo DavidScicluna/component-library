2f6e5a8510784cddf3cc987c7cbf28b5d7806da9
DavidScicluna <dave_nostra@hotmail.com>
2023-07-05 22:37:56 +0200

    Feature: Center

---
+++ src/components/Layout/Center/common/types/index.ts
@@ -0,0 +1,5 @@
+import { BoxProps, BoxRef } from '../../../Box/common/types';
+
+export type CenterProps = BoxProps;
+
+export type CenterRef = BoxRef;
---
+++ src/components/Layout/Center/index.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, ReactElement } from 'react';
+
+import classNames from 'classnames';
+
+import Box from '../Box';
+
+import { CenterProps, CenterRef } from './common/types';
+
+const Center = forwardRef<CenterRef, CenterProps>(function Center(props, ref): ReactElement {
+	const { children, className, ...rest } = props;
+
+	return (
+		<Box
+			{...rest}
+			ref={ref}
+			className={classNames('flex', 'flex-row', 'flex-nowrap', 'items-center', 'justify-center', 'gap-0', {
+				[className || '']: !!className
+			})}
+		>
+			{children}
+		</Box>
+	);
+});
+
+export default Center;
