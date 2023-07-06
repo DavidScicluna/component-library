4cd719b2548bc98b82964b49b08b2ab727b56e40
DavidScicluna <dave_nostra@hotmail.com>
2023-07-06 22:54:01 +0200

    Fix: Container - size responsive value

--- src/components/Layout/Container/index.tsx
+++ src/components/Layout/Container/index.tsx
@@ -1,22 +1,16 @@
-import { forwardRef, ReactElement, useMemo } from 'react';
+import { forwardRef, ReactElement } from 'react';
 
 import classNames from 'classnames';
 
-import { checkResponsiveValue, getResponsiveValue } from '../../../common/utils';
+import { useGetResponsiveValue } from '../../../common/hooks';
 import Box from '../Box';
 
 import { ContainerProps, ContainerRef, ContainerSize } from './common/types';
 
 const Container = forwardRef<ContainerRef, ContainerProps>(function Container(props, ref): ReactElement {
-	const { children, className = '', centerContent = true, isFluid = false, size = '2xl', ...rest } = props;
+	const { children, className = '', centerContent = true, isFluid = false, size: s = '2xl', ...rest } = props;
 
-	const sizeClassName = useMemo(() => {
-		const s =
-			checkResponsiveValue<ContainerSize>(size) !== 'default'
-				? getResponsiveValue<ContainerSize>(size)
-				: (size as ContainerSize);
-		return `max-w-screen-${s}`;
-	}, [size]);
+	const size = useGetResponsiveValue<ContainerSize>(s);
 
 	return (
 		<Box
@@ -24,7 +18,7 @@ const Container = forwardRef<ContainerRef, ContainerProps>(function Container(pr
 			ref={ref}
 			className={classNames('container', {
 				'w-full': isFluid,
-				[sizeClassName]: !isFluid,
+				[`max-w-screen-${size}`]: !isFluid,
 				'mx-auto': centerContent,
 				[className]: !!className
 			})}
