7d334a696c8b5ae4350d8ec23d3f63a37660bcaf
DavidScicluna <dave_nostra@hotmail.com>
2023-07-06 23:03:23 +0200

    Fix: Center - Added spacing

--- src/components/Layout/Center/common/types/index.ts
+++ src/components/Layout/Center/common/types/index.ts
@@ -1,5 +1,8 @@
+import { ResponsiveValue, Space } from '../../../../../common/types';
 import { BoxProps, BoxRef } from '../../../Box/common/types';
 
-export type CenterProps = BoxProps;
+export type CenterProps = BoxProps & {
+	spacing?: ResponsiveValue<Space>;
+};
 
 export type CenterRef = BoxRef;
--- src/components/Layout/Center/index.tsx
+++ src/components/Layout/Center/index.tsx
@@ -2,20 +2,32 @@ import { forwardRef, ReactElement } from 'react';
 
 import classNames from 'classnames';
 
+import { useGetResponsiveValue } from '../../../common/hooks';
+import { Space } from '../../../common/types';
 import Box from '../Box';
 
 import { CenterProps, CenterRef } from './common/types';
 
 const Center = forwardRef<CenterRef, CenterProps>(function Center(props, ref): ReactElement {
-	const { children, className = '', ...rest } = props;
+	const { children, className = '', spacing: s = 2, ...rest } = props;
+
+	const spacing = useGetResponsiveValue<Space>(s);
 
 	return (
 		<Box
 			{...rest}
 			ref={ref}
-			className={classNames('flex', 'flex-row', 'flex-nowrap', 'items-center', 'justify-center', 'gap-0', {
-				[className]: !!className
-			})}
+			className={classNames(
+				'flex',
+				'flex-row',
+				'flex-nowrap',
+				'items-center',
+				'justify-center',
+				`gap-${spacing}`,
+				{
+					[className]: !!className
+				}
+			)}
 		>
 			{children}
 		</Box>
