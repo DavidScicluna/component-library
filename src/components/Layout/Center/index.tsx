9feb838057072e246d5c888713946a47bc2b30f4
DavidScicluna <dave_nostra@hotmail.com>
2023-07-06 22:12:42 +0200

    Fix: Center

--- src/components/Layout/Center/index.tsx
+++ src/components/Layout/Center/index.tsx
@@ -7,14 +7,14 @@ import Box from '../Box';
 import { CenterProps, CenterRef } from './common/types';
 
 const Center = forwardRef<CenterRef, CenterProps>(function Center(props, ref): ReactElement {
-	const { children, className, ...rest } = props;
+	const { children, className = '', ...rest } = props;
 
 	return (
 		<Box
 			{...rest}
 			ref={ref}
 			className={classNames('flex', 'flex-row', 'flex-nowrap', 'items-center', 'justify-center', 'gap-0', {
-				[className || '']: !!className
+				[className]: !!className
 			})}
 		>
 			{children}
