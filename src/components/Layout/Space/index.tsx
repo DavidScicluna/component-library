d8944445c6ab55088e3df62f79ef3886bc94fe3d
DavidScicluna <dave_nostra@hotmail.com>
2023-07-06 22:54:48 +0200

    Fix: Space - width & height responsive value

--- src/components/Layout/Space/index.tsx
+++ src/components/Layout/Space/index.tsx
@@ -1,37 +1,25 @@
-import { forwardRef, ReactElement, useMemo } from 'react';
+import { forwardRef, ReactElement } from 'react';
 
 import classNames from 'classnames';
 
-import { checkResponsiveValue, getResponsiveValue } from '../../../common/utils';
+import { useGetResponsiveValue } from '../../../common/hooks';
 import Box from '../Box';
 
 import { SpaceDimension, SpaceProps, SpaceRef } from './common/types';
 
 const Space = forwardRef<SpaceRef, SpaceProps>(function Space(props, ref): ReactElement {
-	const { className = '', width = 0, height = 0, ...rest } = props;
+	const { className = '', width: w = 0, height: h = 0, ...rest } = props;
 
-	const widthClassName = useMemo(() => {
-		const w =
-			checkResponsiveValue<SpaceDimension>(width) !== 'default'
-				? getResponsiveValue<SpaceDimension>(width)
-				: (width as SpaceDimension);
-		return `w-${w}`;
-	}, [width]);
-	const heightClassName = useMemo(() => {
-		const h =
-			checkResponsiveValue<SpaceDimension>(height) !== 'default'
-				? getResponsiveValue<SpaceDimension>(height)
-				: (height as SpaceDimension);
-		return `h-${h}`;
-	}, [height]);
+	const width = useGetResponsiveValue<SpaceDimension>(w);
+	const height = useGetResponsiveValue<SpaceDimension>(h);
 
 	return (
 		<Box
 			{...rest}
 			ref={ref}
 			className={classNames({
-				[widthClassName]: !!width,
-				[heightClassName]: !!height,
+				[`w-${width}`]: !!w,
+				[`h-${height}`]: !!h,
 				[className]: !!className
 			})}
 		/>
