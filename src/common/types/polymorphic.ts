5c35de79012c213ef1e41d80bd805e1a4ad003f0
DavidScicluna <dave_nostra@hotmail.com>
2023-07-08 11:55:58 +0200

    Fix: polymorphic types - Added the ability to pass props

--- src/common/types/polymorphic.ts
+++ src/common/types/polymorphic.ts
@@ -1,12 +1,19 @@
-import { ComponentPropsWithoutRef, ComponentPropsWithRef, ElementType } from 'react';
+import { ComponentPropsWithoutRef, ComponentPropsWithRef, ElementType, HTMLAttributes } from 'react';
 
 // TODO: Maybe move to default/constants
 export const __DEFAULT_POLYMORPHIC_ELEMENT__: ElementType = 'div';
+export const __DEFAULT_POLYMORPHIC_OBJECT__ = {};
 
 export type PolymorphicRef<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> =
 	ComponentPropsWithRef<Element>['ref'];
 
-export type PolymorphicComponentProps<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> =
-	ComponentPropsWithoutRef<Element> & {
-		as?: Element;
-	};
+type PolymorphicProps<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> = HTMLAttributes<
+	PolymorphicRef<Element>
+> & {
+	as?: Element;
+} & ComponentPropsWithoutRef<Element>;
+
+export type PolymorphicComponentProps<
+	Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__,
+	Props extends object = typeof __DEFAULT_POLYMORPHIC_OBJECT__
+> = Props & PolymorphicProps<Element>;
