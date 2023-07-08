dfd4565cd6ccd3e04923fdcf806567878716e63e
DavidScicluna <dave_nostra@hotmail.com>
2023-07-08 11:56:52 +0200

    Fix: Box - polymorphic types

--- src/components/Layout/Box/common/types/index.ts
+++ src/components/Layout/Box/common/types/index.ts
@@ -1,14 +1,17 @@
-import { ElementType, HTMLAttributes } from 'react';
+import { ElementType } from 'react';
 
 import { Nullable } from '../../../../../common/types';
 import {
 	__DEFAULT_POLYMORPHIC_ELEMENT__,
+	__DEFAULT_POLYMORPHIC_OBJECT__,
 	PolymorphicComponentProps,
 	PolymorphicRef
 } from '../../../../../common/types/polymorphic';
 
-export type BoxProps<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> = HTMLAttributes<Element> &
-	PolymorphicComponentProps<Element>;
+export type BoxProps<
+	Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__,
+	Props extends object = typeof __DEFAULT_POLYMORPHIC_OBJECT__
+> = PolymorphicComponentProps<Element, Props>;
 
 export type BoxRef<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> = Nullable<
 	PolymorphicRef<Element>
--- src/components/Layout/Box/index.tsx
+++ src/components/Layout/Box/index.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { ElementType, forwardRef, ReactElement } from 'react';
 
-import { __DEFAULT_POLYMORPHIC_ELEMENT__ } from '../../../common/types/polymorphic';
+import { __DEFAULT_POLYMORPHIC_ELEMENT__, __DEFAULT_POLYMORPHIC_OBJECT__ } from '../../../common/types/polymorphic';
 
 import { BoxProps, BoxRef } from './common/types';
 
-const Box = forwardRef(function Box<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__>(
-	props: BoxProps<Element>,
-	ref: BoxRef<Element>
-): ReactElement {
+const Box = forwardRef(function Box<
+	Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__,
+	Props extends object = typeof __DEFAULT_POLYMORPHIC_OBJECT__
+>(props: BoxProps<Element, Props>, ref: BoxRef<Element>): ReactElement {
 	const { children, as: Component = __DEFAULT_POLYMORPHIC_ELEMENT__, ...rest } = props;
 
 	return (
