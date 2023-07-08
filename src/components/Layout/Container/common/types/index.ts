9dfd30642f60b14d07c35855532423ba6ad45df8
DavidScicluna <dave_nostra@hotmail.com>
2023-07-08 11:57:13 +0200

    Fix: Container - polymorphic types

--- src/components/Layout/Container/common/types/index.ts
+++ src/components/Layout/Container/common/types/index.ts
@@ -1,12 +1,17 @@
+import { ElementType } from 'react';
+
 import { Breakpoint, ResponsiveValue } from '../../../../../common/types';
 import { BoxProps, BoxRef } from '../../../Box/common/types';
 
 export type ContainerSize = Exclude<Breakpoint, 'xs'> | number;
 
-export type ContainerProps = BoxProps & {
-	centerContent?: boolean;
-	isFluid?: ResponsiveValue<boolean>;
-	size?: ResponsiveValue<ContainerSize>;
-};
+export type ContainerProps<Element extends ElementType> = BoxProps<
+	Element,
+	{
+		centerContent?: boolean;
+		isFluid?: ResponsiveValue<boolean>;
+		size?: ResponsiveValue<ContainerSize>;
+	}
+>;
 
-export type ContainerRef = BoxRef;
+export type ContainerRef<Element extends ElementType> = BoxRef<Element>;
--- src/components/Layout/Container/index.tsx
+++ src/components/Layout/Container/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, ReactElement } from 'react';
+import { ElementType, forwardRef, ReactElement } from 'react';
 
 import classNames from 'classnames';
 
@@ -7,14 +7,17 @@ import Box from '../Box';
 
 import { ContainerProps, ContainerRef, ContainerSize } from './common/types';
 
-const Container = forwardRef<ContainerRef, ContainerProps>(function Container(props, ref): ReactElement {
+const Container = forwardRef(function Container<Element extends ElementType>(
+	props: ContainerProps<Element>,
+	ref: ContainerRef<Element>
+): ReactElement {
 	const { children, className = '', centerContent = true, isFluid = false, size: s = '2xl', ...rest } = props;
 
 	const size = useGetResponsiveValue<ContainerSize>(s);
 
 	return (
 		<Box
-			{...rest}
+			{...(rest as ContainerProps<Element>)}
 			ref={ref}
 			className={classNames('container', {
 				'w-full': isFluid,
