170ca551bed8b86cc81d65a3a10e38a09ba7fbaa
DavidScicluna <dave_nostra@hotmail.com>
2023-07-11 22:12:23 +0200

    Fix: default props

--- src/common/default/props/index.ts
+++ src/common/default/props/index.ts
@@ -1,7 +1,24 @@
-import { localStorageColorKey, localStorageColorModeKey } from '../../keys';
-import { AppColor, AppColorMode } from '../../types';
+import {
+	__KEY_LOCALSTORAGE_COLOR__,
+	__KEY_LOCALSTORAGE_COLORMODE__,
+	__KEY_SESSIONSTORAGE_HAS_FILLEDICON_LOADED__,
+	__KEY_SESSIONSTORAGE_HAS_OUTLINEDICON_LOADED__,
+	__KEY_SESSIONSTORAGE_HAS_TWOTONEICON_LOADED__
+} from '../../keys';
+import { AppColor, AppColorMode } from '../../types/theme';
 
-export const color: AppColor = (localStorage.getItem(localStorageColorKey) || 'gray') as AppColor;
-export const colorMode: AppColorMode = (localStorage.getItem(localStorageColorModeKey) || 'light') as AppColorMode;
+export const __DEFAULT_APP_COLOR__: AppColor = (localStorage.getItem(__KEY_LOCALSTORAGE_COLOR__) || 'blue') as AppColor;
+export const __DEFAULT_APP_COLORMODE__: AppColorMode = (localStorage.getItem(__KEY_LOCALSTORAGE_COLORMODE__) ||
+	'light') as AppColorMode;
 
-export const method = () => undefined;
+export const __DEFAULT_HAS_FILLEDICON_LOADED__ = (sessionStorage.getItem(
+	__KEY_SESSIONSTORAGE_HAS_FILLEDICON_LOADED__
+) || false) as boolean;
+export const __DEFAULT_HAS_OUTLINEDICON_LOADED__ = (sessionStorage.getItem(
+	__KEY_SESSIONSTORAGE_HAS_OUTLINEDICON_LOADED__
+) || false) as boolean;
+export const __DEFAULT_HAS_TWOTONEICON_LOADED__ = (sessionStorage.getItem(
+	__KEY_SESSIONSTORAGE_HAS_TWOTONEICON_LOADED__
+) || false) as boolean;
+
+export const __DEFAULT_METHOD__ = () => undefined;
