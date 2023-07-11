456f7ee86e6fdffde30550646299fa8c09746180
DavidScicluna <dave_nostra@hotmail.com>
2023-07-11 22:12:08 +0200

    Fix: Keys

--- src/common/keys/index.ts
+++ src/common/keys/index.ts
@@ -1,2 +1,6 @@
-export const localStorageColorKey = 'ds-cl-theme-color';
-export const localStorageColorModeKey = 'ds-cl-theme-color-mode';
+export const __KEY_LOCALSTORAGE_COLOR__ = 'ds-cl-theme-color';
+export const __KEY_LOCALSTORAGE_COLORMODE__ = 'ds-cl-theme-color-mode';
+
+export const __KEY_SESSIONSTORAGE_HAS_FILLEDICON_LOADED__ = 'ds-cl-font-has-filled-icon-loaded';
+export const __KEY_SESSIONSTORAGE_HAS_OUTLINEDICON_LOADED__ = 'ds-cl-font-has-outlined-icon-loaded';
+export const __KEY_SESSIONSTORAGE_HAS_TWOTONEICON_LOADED__ = 'ds-cl-font-has-twotone-icon-loaded';
