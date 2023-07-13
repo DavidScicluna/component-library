0b022692c30a854b68bb530106e5c878f6a028b5
DavidScicluna <dave_nostra@hotmail.com>
2023-07-13 21:01:57 +0200

    Removed useOs & useGetThemeAppearance hooks & scripts

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -3,8 +3,6 @@ import useConst from './useConst';
 import useDebounce from './useDebounce';
 import useGetColor from './useGetColor';
 import useGetResponsiveValue from './useGetResponsiveValue';
-import useGetThemeAppearance from './useGetThemeAppearance';
-import useOs from './useOs';
 import useTheme from './useTheme';
 
 export {
