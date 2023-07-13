cb803a7a7a09ddf66d7fa8773d46dda1bc53fd48
DavidScicluna <dave_nostra@hotmail.com>
2023-07-13 21:02:43 +0200

    Fix: useDebounce hook

--- src/common/hooks/useDebounce.ts
+++ src/common/hooks/useDebounce.ts
@@ -1,21 +1,18 @@
 import { useEffect, useState } from 'react';
 
-import { Duration } from '../../theme/types';
+import theme from '../theme';
+import { Duration } from '../types/theme';
 import { convertStringToNumber } from '../utils';
-
-import useTheme from './useThemeTyped';
 
 // https://usehooks-ts.com/react-hook/use-debounce
 
 const useDebounce = <T>(value: T, delay: Duration = 'normal'): T => {
-	const theme = useTheme();
-
 	const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			setDebouncedValue(value);
-		}, convertStringToNumber(theme.transition.duration[delay], 'ms'));
+		}, convertStringToNumber(theme.transitionDuration[delay], 'ms'));
 
 		return () => clearTimeout(timer);
 	}, [value, delay]);
