dc7e20761015f3a5193e023c3adc3f1c738f57fd
DavidScicluna <dave_nostra@hotmail.com>
2023-07-14 21:48:32 +0200

    Feature: tailwind & postcss config files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import theme from './src/common/theme';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: { ...theme },
+	plugins: []
+};
