010b87406589b15148c8fe2b68d3f3cf0a9fc066
DavidScicluna <dave_nostra@hotmail.com>
2023-07-14 21:47:31 +0200

    Fix: classes - Added flex & sizing

---
+++ src/common/classes/flex.ts
@@ -0,0 +1,35 @@
+import { AlignItemsObj, FlexDirectionObj, FlexWrapObj, JustifyContentObj } from '../types/classes';
+
+const alignItems: AlignItemsObj = {
+	'flex-start': 'items-start',
+	'flex-end': 'items-end',
+	'center': 'items-center',
+	'baseline': 'items-baseline',
+	'stretch': 'items-stretch'
+};
+
+const direction: FlexDirectionObj = {
+	'row': 'flex-row',
+	'row-reverse': 'flex-row-reverse',
+	'column': 'flex-col',
+	'column-reverse': 'flex-col-reverse'
+};
+
+const wrap: FlexWrapObj = {
+	'wrap': 'flex-wrap',
+	'wrap-reverse': 'flex-wrap-reverse',
+	'nowrap': 'flex-nowrap'
+};
+
+const justifyContent: JustifyContentObj = {
+	'normal': 'justify-normal',
+	'flex-start': 'justify-start',
+	'flex-end': 'justify-end',
+	'center': 'justify-center',
+	'space-between': 'justify-between',
+	'space-around': 'justify-around',
+	'space-evenly': 'justify-evenly',
+	'stretch': 'justify-stretch'
+};
+
+export default { alignItems, direction, wrap, justifyContent };
--- src/common/classes/index.ts
+++ src/common/classes/index.ts
@@ -1,6 +1,8 @@
 import backgrounds from './backgrounds';
 import borders from './borders';
+import flex from './flex';
+import sizing from './sizing';
 import spacing from './spacing';
 import typography from './typography';
 
-export default { backgrounds, borders, spacing, typography };
+export default { backgrounds, borders, flex, sizing, spacing, typography };
