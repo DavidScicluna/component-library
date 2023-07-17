2a84b9143f8c02aa3d0b1b7d7adbe724bd7e8f91
DavidScicluna <dave_nostra@hotmail.com>
2023-07-17 23:03:00 +0200

    Feature: animation types

---
+++ src/common/types/animation.ts
@@ -0,0 +1,7 @@
+import { Transition } from 'framer-motion';
+
+export type AnimationDelay = number;
+export type AnimationDuration = number;
+export type AnimationEasing = number[];
+
+export type AnimationConfig = Partial<Transition>;
