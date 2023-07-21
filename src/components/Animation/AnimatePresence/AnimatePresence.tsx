84ddf852c04b505d60db09ab87aaa988c8cf9186
DavidScicluna <dave_nostra@hotmail.com>
2023-07-21 21:49:46 +0200

    Fix: AnimatePresence

--- src/components/Animation/AnimatePresence/AnimatePresence.tsx
+++ src/components/Animation/AnimatePresence/AnimatePresence.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
+import { ReactElement } from 'react';
 
 import { AnimatePresence as AP } from 'framer-motion';
 
 import { AnimatePresenceProps } from './common/types';
 
-const AnimatePresence: FC<AnimatePresenceProps> = ({ children, ...rest }) => (
+const AnimatePresence = ({ children, ...rest }: AnimatePresenceProps): ReactElement => (
 	<AP {...rest} mode='wait' initial={false}>
 		{children}
 	</AP>
