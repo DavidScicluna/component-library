129ccaecd98816e9cd3b0181074fe524339f145b
DavidScicluna <dave_nostra@hotmail.com>
2023-07-22 13:28:34 +0200

    Fix: Fade unmountOnExit bug

--- src/components/Animation/Fade/Fade.tsx
+++ src/components/Animation/Fade/Fade.tsx
@@ -1,8 +1,8 @@
 import { forwardRef, ReactElement } from 'react';
 
 import { motion } from 'framer-motion';
+import { pick } from 'lodash-es';
 
-import { useConst } from '../../../common/hooks';
 import AnimatePresence from '../AnimatePresence';
 import {
 	__DEFAULT_TRANSITION__,
@@ -11,43 +11,40 @@ import {
 	__DEFAULT_TRANSITION_IN__,
 	__DEFAULT_TRANSITION_UNMOUNT_ON_EXIT__
 } from '../common/constants';
-import { TransitionProps, Variants } from '../common/types';
 
 import { FadeProps, FadeRef } from './common/types';
 
 const config = __DEFAULT_TRANSITION_CONFIG__;
 
+const variants = {
+	enter: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
+		opacity: 1,
+		transition: { ...transition.enter },
+		transitionEnd: { ...transitionEnd.enter }
+	}),
+	exit: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
+		opacity: 0,
+		transition: { ...transition.exit },
+		transitionEnd: { ...transitionEnd.exit }
+	})
+};
+
 const Fade = forwardRef<FadeRef, FadeProps>(function Fade(props, ref): ReactElement {
 	const {
 		in: isOpen = __DEFAULT_TRANSITION_IN__,
 		unmountOnExit = __DEFAULT_TRANSITION_UNMOUNT_ON_EXIT__,
-		transition,
-		transitionEnd,
 		...rest
 	} = props;
 
 	const animate = isOpen || unmountOnExit ? 'enter' : 'exit';
 	const isVisible = unmountOnExit ? isOpen && unmountOnExit : true;
 
-	const custom = { transition, transitionEnd };
-
-	const variants = useConst<Variants<TransitionProps>>({
-		enter: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
-			opacity: 1,
-			transition: { ...transition.enter },
-			transitionEnd: { ...transitionEnd.enter }
-		}),
-		exit: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
-			opacity: 0,
-			transition: { ...transition.exit },
-			transitionEnd: { ...transitionEnd.exit }
-		})
-	});
+	const custom = pick({ ...rest }, ['transition', 'transitionEnd']);
 
 	return (
 		<AnimatePresence custom={custom}>
 			{isVisible ? (
-				<motion.div ref={ref} custom={custom} animate={animate} variants={variants} {...config} {...rest} />
+				<motion.div {...rest} {...config} ref={ref} custom={custom} animate={animate} variants={variants} />
 			) : null}
 		</AnimatePresence>
 	);
