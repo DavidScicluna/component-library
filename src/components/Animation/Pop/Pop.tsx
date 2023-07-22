f2dbb74a322fcd89594f203e4ebb469ceb60f91b
DavidScicluna <dave_nostra@hotmail.com>
2023-07-22 13:32:35 +0200

    Fix: Pop unmountOnExit bug

--- src/components/Animation/Pop/Pop.tsx
+++ src/components/Animation/Pop/Pop.tsx
@@ -1,6 +1,7 @@
-import { forwardRef, ReactElement, useMemo } from 'react';
+import { forwardRef, ReactElement } from 'react';
 
 import { motion } from 'framer-motion';
+import { pick } from 'lodash-es';
 
 import AnimatePresence from '../AnimatePresence';
 import {
@@ -10,53 +11,51 @@ import {
 	__DEFAULT_TRANSITION_IN__,
 	__DEFAULT_TRANSITION_UNMOUNT_ON_EXIT__
 } from '../common/constants';
-import { TransitionProps, Variants } from '../common/types';
 
 import { __DEFAULT_POP_INITIAL_SCALE__, __DEFAULT_POP_IS_REVERSED__ } from './common/constants';
 import { PopProps, PopRef } from './common/types';
 
 const config = __DEFAULT_TRANSITION_CONFIG__;
 
+const variants = {
+	enter: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
+		opacity: 1,
+		scale: 1,
+		transformOrigin: 'center',
+		transition: { ...transition.enter },
+		transitionEnd: { ...transitionEnd.enter }
+	}),
+	exit: ({
+		transition = __DEFAULT_TRANSITION__,
+		transitionEnd = __DEFAULT_TRANSITION_END__,
+		initialScale = __DEFAULT_POP_INITIAL_SCALE__,
+		isReversed = __DEFAULT_POP_IS_REVERSED__
+	} = {}) => ({
+		...(isReversed
+			? { scale: initialScale, transitionEnd: { ...transitionEnd.exit } }
+			: { transitionEnd: { scale: initialScale, ...transitionEnd.exit } }),
+		opacity: 0,
+		transformOrigin: 'center',
+		transition: { ...transition.exit }
+	})
+};
+
 const Pop = forwardRef<PopRef, PopProps>(function Pop(props, ref): ReactElement {
 	const {
-		initialScale = __DEFAULT_POP_INITIAL_SCALE__,
-		isReversed = __DEFAULT_POP_IS_REVERSED__,
 		in: isOpen = __DEFAULT_TRANSITION_IN__,
 		unmountOnExit = __DEFAULT_TRANSITION_UNMOUNT_ON_EXIT__,
-		transition,
-		transitionEnd,
 		...rest
 	} = props;
 
 	const animate = isOpen || unmountOnExit ? 'enter' : 'exit';
 	const isVisible = unmountOnExit ? isOpen && unmountOnExit : true;
 
-	const custom = { transition, transitionEnd };
-
-	const variants = useMemo<Variants<TransitionProps>>(() => {
-		return {
-			enter: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
-				opacity: 1,
-				scale: 1,
-				transformOrigin: 'center center',
-				transition: { ...transition.enter },
-				transitionEnd: { ...transitionEnd.enter }
-			}),
-			exit: ({ transition = __DEFAULT_TRANSITION__, transitionEnd = __DEFAULT_TRANSITION_END__ } = {}) => ({
-				...(isReversed
-					? { scale: initialScale, transitionEnd: { ...transitionEnd.exit } }
-					: { transitionEnd: { pop: initialScale, ...transitionEnd.exit } }),
-				opacity: 0,
-				transformOrigin: 'center center',
-				transition: { ...transition.exit }
-			})
-		};
-	}, [initialScale, isReversed]);
+	const custom = pick({ ...rest }, ['transition', 'transitionEnd', 'initialScale', 'isReversed']);
 
 	return (
 		<AnimatePresence custom={custom}>
 			{isVisible ? (
-				<motion.div ref={ref} custom={custom} animate={animate} variants={variants} {...config} {...rest} />
+				<motion.div {...rest} {...config} ref={ref} custom={custom} animate={animate} variants={variants} />
 			) : null}
 		</AnimatePresence>
 	);
