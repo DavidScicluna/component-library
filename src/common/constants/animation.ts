faeb15bb4dc0be1cddb99dd6a9e631e42b31f388
DavidScicluna <dave_nostra@hotmail.com>
2023-07-23 23:11:56 +0200

    Fix: animation constants

--- src/common/constants/animation.ts
+++ src/common/constants/animation.ts
@@ -1,14 +1,17 @@
 import theme from '@common/theme';
 import type { AnimationDelay, AnimationDuration, AnimationEasing } from '@common/types/animation';
-import { convertEasingsToArray, convertStringToNumber } from '@common/utils';
+import { convertStringToNumber } from '@common/utils';
 
 import { __DEFAULT_DURATION__, __DEFAULT_EASING__ } from './props';
-// import { getAnimationDelay, getAnimationDuration, getAnimationEasings } from '../utils/animation';
 
 export const __DEFAULT_ANIMATION_DELAY__: AnimationDelay =
 	convertStringToNumber(theme.transitionDuration[__DEFAULT_DURATION__], 'ms') / 1000;
 export const __DEFAULT_ANIMATION_DURATION__: AnimationDuration =
 	convertStringToNumber(theme.transitionDuration[__DEFAULT_DURATION__], 'ms') / 1000;
-export const __DEFAULT_ANIMATION_EASING__: AnimationEasing = convertEasingsToArray({
-	easing: theme.transitionTimingFunction[__DEFAULT_EASING__]
-});
+export const __DEFAULT_ANIMATION_EASING__: AnimationEasing = theme.transitionTimingFunction[__DEFAULT_EASING__]
+	.replaceAll('cubic-bezier', '')
+	.replaceAll('(', '')
+	.replaceAll(')', '')
+	.replaceAll(' ', '')
+	.split(',')
+	.map((number) => Number(number));
