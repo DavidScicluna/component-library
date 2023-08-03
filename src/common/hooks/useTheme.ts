8377556abf705896a80c566e05192317eeafed4d
DavidScicluna <dave_nostra@hotmail.com>
2023-08-03 20:22:48 +0200

    Fix: useTheme hook

--- src/common/hooks/useTheme.ts
+++ src/common/hooks/useTheme.ts
@@ -1,26 +1,57 @@
 import theme from '@common/theme';
+import type { ZIndexObj } from '@common/types/classes';
+import type {
+	ColorsObj,
+	DelaysObj,
+	DurationsObj,
+	EasingsObj,
+	FontSizesObj,
+	FontWeightsObj,
+	LineHeightsObj,
+	RadiiObj,
+	SpacingObj
+} from '@common/types/theme';
 
 import useConst from './useConst';
 
-const useTheme = () => {
+type UseThemeReturn = {
+	borderRadius: RadiiObj;
+	colors: ColorsObj;
+	fontSizes: FontSizesObj;
+	fontWeights: FontWeightsObj;
+	lineHeights: LineHeightsObj;
+	transition: {
+		delay: DelaysObj;
+		duration: DurationsObj;
+		easing: EasingsObj;
+	};
+	spacing: SpacingObj;
+	zIndex: ZIndexObj;
+};
+
+const useTheme = (): UseThemeReturn => {
 	const {
 		borderRadius,
 		colors,
-		fontFamily,
-		transitionTimingFunction,
-		transitionDuration,
-		transitionProperty,
-		spacing
+		fontSizes,
+		fontWeights,
+		lineHeights,
+		transitionDelay: delay,
+		transitionDuration: duration,
+		transitionTimingFunction: easing,
+		spacing,
+		zIndex
 	} = useConst(theme);
 
 	return {
 		borderRadius,
 		colors,
-		fontFamily,
-		transitionTimingFunction,
-		transitionDuration,
-		transitionProperty,
-		spacing
+		fontSizes,
+		fontWeights,
+		lineHeights,
+		transition: { delay, duration, easing },
+		spacing,
+		zIndex
 	};
 };
 
