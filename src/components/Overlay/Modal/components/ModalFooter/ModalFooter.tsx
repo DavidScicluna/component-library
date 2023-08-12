43a4203d52aef94550e36b26faa1bffcf5bc6510
DavidScicluna <dave_nostra@hotmail.com>
2023-08-12 18:48:00 +0200

    Fix: Modal - ModalFooter

---
+++ src/components/Overlay/Modal/components/ModalFooter/ModalFooter.tsx
@@ -0,0 +1,70 @@
+import type { ElementType, ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASS_PREFIX__, __DEFAULT_CLASSNAME__ } from '@common/constants';
+import { useMediaQuery } from '@common/hooks';
+
+import { Stack } from '@components/Layout';
+
+import { useModalContext } from '../../common/hooks';
+
+import type { ModalFooterProps, ModalFooterRef } from './common/types';
+
+const ModalFooter = forwardRef(function ModalFooter<Element extends ElementType>(
+	props: ModalFooterProps<Element>,
+	ref: ModalFooterRef<Element>
+): ReactElement {
+	const isSm = useMediaQuery({ breakpoint: 'sm', type: 'width', direction: 'max' });
+
+	const { color, colorMode, onClose, spacing: __DEFAULT_MODAL_FOOTER_SPACING__ } = useModalContext();
+
+	const {
+		className = __DEFAULT_CLASSNAME__,
+		renderCancel,
+		renderAction,
+		direction = isSm ? 'column-reverse' : 'row',
+		alignItems = 'center',
+		justifyContent = isSm ? 'center' : 'space-between',
+		spacing = __DEFAULT_MODAL_FOOTER_SPACING__,
+		...rest
+	} = props;
+
+	return (
+		<Stack<Element>
+			{...rest}
+			ref={ref}
+			className={classNames(`${__DEFAULT_CLASS_PREFIX__}-modal-footer`, { [className]: !!className })}
+			w='100%'
+			h='auto'
+			direction={direction}
+			alignItems={alignItems}
+			justifyContent={justifyContent}
+			spacing={spacing}
+		>
+			{renderCancel
+				? renderCancel({
+						color: 'gray',
+						colorMode,
+						isFullWidth: isSm || !renderAction,
+						onClick: typeof onClose === 'function' ? () => onClose() : undefined,
+						size: 'md',
+						variant: 'monochrome'
+				  })
+				: null}
+
+			{renderAction
+				? renderAction({
+						color,
+						colorMode,
+						isFullWidth: isSm || !renderCancel,
+						size: 'md',
+						variant: 'contained'
+				  })
+				: null}
+		</Stack>
+	);
+});
+
+export default ModalFooter;
---
+++ src/components/Overlay/Modal/components/ModalFooter/index.ts
@@ -0,0 +1,13 @@
+import type {
+	ModalFooterProps,
+	ModalFooterRef,
+	ModalFooterRenderCancelProps,
+	ModalFooterRenderProps
+} from './common/types';
+import ModalFooter from './ModalFooter';
+
+// Components
+export { ModalFooter };
+
+// Component Types
+export type { ModalFooterProps, ModalFooterRef, ModalFooterRenderCancelProps, ModalFooterRenderProps };
