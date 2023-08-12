be0489b173ef8cc663b9d5356bb9ee8fdc590aa1
DavidScicluna <dave_nostra@hotmail.com>
2023-08-12 18:49:15 +0200

    Feature: Modal - ModalSubtitle

---
+++ src/components/Overlay/Modal/components/ModalSubtitle/ModalSubtitle.tsx
@@ -0,0 +1,59 @@
+import type { ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASS_PREFIX__, __DEFAULT_CLASSNAME__ } from '@common/constants';
+import { useGetColor } from '@common/hooks';
+
+import { useModalContext } from '@components/Overlay/Modal/common/hooks';
+import { Text } from '@components/Typography';
+
+import type {
+	ModalSubtitleDefaultElement,
+	ModalSubtitleElement,
+	ModalSubtitleProps,
+	ModalSubtitleRef
+} from './common/types';
+
+const ModalSubtitle = forwardRef(function ModalSubtitle<
+	Element extends ModalSubtitleElement = ModalSubtitleDefaultElement
+>(props: ModalSubtitleProps<Element>, ref: ModalSubtitleRef<Element>): ReactElement {
+	const { colorMode } = useModalContext();
+
+	const __DEFAULT_MODAL_TITLE_COLOR__ = useGetColor({
+		color: 'gray',
+		colorMode,
+		colorType: 'default',
+		hueType: 'text.secondary',
+		classType: 'text'
+	});
+
+	const {
+		children,
+		className = __DEFAULT_CLASSNAME__,
+		align = 'left',
+		color = __DEFAULT_MODAL_TITLE_COLOR__,
+		fontSize = 'sm',
+		fontWeight = 'normal',
+		lineClamp = 1,
+		...rest
+	} = props;
+
+	return (
+		<Text<Element>
+			{...rest}
+			ref={ref}
+			className={classNames(`${__DEFAULT_CLASS_PREFIX__}-modal-subtitle`, { [className]: !!className })}
+			align={align}
+			color={color}
+			fontSize={fontSize}
+			fontWeight={fontWeight}
+			lineClamp={lineClamp}
+		>
+			{children}
+		</Text>
+	);
+});
+
+export default ModalSubtitle;
---
+++ src/components/Overlay/Modal/components/ModalSubtitle/index.ts
@@ -0,0 +1,13 @@
+import type {
+	ModalSubtitleDefaultElement,
+	ModalSubtitleElement,
+	ModalSubtitleProps,
+	ModalSubtitleRef
+} from './common/types';
+import ModalSubtitle from './ModalSubtitle';
+
+// Components
+export { ModalSubtitle };
+
+// Component Types
+export type { ModalSubtitleDefaultElement, ModalSubtitleElement, ModalSubtitleProps, ModalSubtitleRef };
