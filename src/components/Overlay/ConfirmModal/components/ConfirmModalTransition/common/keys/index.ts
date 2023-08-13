1f5fd4a2c38f90c85a3f4ee0eb7460a0d6799640
DavidScicluna <dave_nostra@hotmail.com>
2023-08-13 20:37:14 +0200

    Feature: ConfirmModal - ConfirmModalTransition

---
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalTransition/common/keys/index.ts
@@ -0,0 +1,3 @@
+import type { ConfirmModalTransitionProps } from '../types';
+
+export const __KEYS_CONFIRM_MODAL_TRANSITION__: (keyof ConfirmModalTransitionProps)[] = ['transition', 'transitionEnd'];
---
+++ src/components/Overlay/ConfirmModal/components/ConfirmModalTransition/index.ts
@@ -0,0 +1,24 @@
+import {
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_INITIAL_SCALE__,
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_TRANSITION__,
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_VARIANT__
+} from './common/constants';
+import type {
+	ConfirmModalTransitionProps,
+	ConfirmModalTransitionRef,
+	ConfirmModalTransitionVariant
+} from './common/types';
+import ConfirmModalTransition from './ConfirmModalTransition';
+
+// Constants
+export const constants = {
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_INITIAL_SCALE__,
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_TRANSITION__,
+	__DEFAULT_CONFIRM_MODAL_TRANSITION_VARIANT__
+};
+
+// Components
+export { ConfirmModalTransition };
+
+// Component Types
+export type { ConfirmModalTransitionProps, ConfirmModalTransitionRef, ConfirmModalTransitionVariant };
