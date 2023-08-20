7f2176f996f8e51258e43e2c52bf875554bcd1a3
DavidScicluna <dave_nostra@hotmail.com>
2023-08-20 11:36:19 +0200

    Fix: Removed StateLabel replaced it with Message

--- src/components/Overlay/StateLabel/common/default/props/index.ts
+++
@@ -1,5 +0,0 @@
-import { Space } from '../../../../../../theme/types';
-
-export const spacing: Space = 4;
-
-export const padding: Space = 4;
--- src/components/Overlay/StateLabel/common/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-import useStateLabelContext from './useStateLabelContext';
-
-export { useStateLabelContext };
