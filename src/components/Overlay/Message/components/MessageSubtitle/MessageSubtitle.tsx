db401f456777cb7c8c4fd387b94ceaee433b40ab
DavidScicluna <dave_nostra@hotmail.com>
2023-08-20 11:38:49 +0200

    Feature: Message - MessageSubtitle

---
+++ src/components/Overlay/Message/components/MessageSubtitle/MessageSubtitle.tsx
@@ -0,0 +1,60 @@
+import type { ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASS_PREFIX__, __DEFAULT_CLASSNAME__ } from '@common/constants';
+import { useGetColor } from '@common/hooks';
+
+import { Text } from '@components/Typography';
+
+import { useMessageContext } from '../../common/hooks';
+
+import type {
+	MessageSubtitleDefaultElement,
+	MessageSubtitleElement,
+	MessageSubtitleProps,
+	MessageSubtitleRef
+} from './common/types';
+
+const MessageSubtitle = forwardRef(function MessageSubtitle<
+	Element extends MessageSubtitleElement = MessageSubtitleDefaultElement
+>(props: MessageSubtitleProps<Element>, ref: MessageSubtitleRef<Element>): ReactElement {
+	const { colorMode } = useMessageContext();
+
+	const __DEFAULT_MESSAGE_SUBTITLE_COLOR__ = useGetColor({
+		color: 'gray',
+		colorMode,
+		colorType: 'default',
+		hueType: 'text.secondary',
+		classType: 'text'
+	});
+
+	const {
+		children,
+		className = __DEFAULT_CLASSNAME__,
+		align = 'center',
+		color = __DEFAULT_MESSAGE_SUBTITLE_COLOR__,
+		fontSize = ['xs', 'sm'],
+		fontWeight = 'normal',
+		lineClamp = 'none',
+		...rest
+	} = props;
+
+	return (
+		<Text<Element>
+			{...rest}
+			ref={ref}
+			className={classNames(`${__DEFAULT_CLASS_PREFIX__}-message-subtitle`, { [className]: !!className })}
+			align={align}
+			color={color}
+			fontSize={fontSize}
+			fontWeight={fontWeight}
+			lineClamp={lineClamp}
+		>
+			{children}
+		</Text>
+	);
+});
+
+export default MessageSubtitle;
---
+++ src/components/Overlay/Message/components/MessageSubtitle/index.ts
@@ -0,0 +1,13 @@
+import type {
+	MessageSubtitleDefaultElement,
+	MessageSubtitleElement,
+	MessageSubtitleProps,
+	MessageSubtitleRef
+} from './common/types';
+import MessageSubtitle from './MessageSubtitle';
+
+// Components
+export { MessageSubtitle };
+
+// Component Types
+export type { MessageSubtitleDefaultElement, MessageSubtitleElement, MessageSubtitleProps, MessageSubtitleRef };
