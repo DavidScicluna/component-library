4d49e3ee179b33fe84507c17051ef8d5620cc935
DavidScicluna <dave_nostra@hotmail.com>
2023-08-28 23:14:19 +0200

    Fix: Animation constants

--- src/components/Animation/common/constants/index.ts
+++ src/components/Animation/common/constants/index.ts
@@ -1,32 +1,17 @@
-import type { AnimationEasing } from '@common/types/animation';
-import type { Ease, EasingsArr } from '@common/types/theme';
-import { getAnimationDuration, getAnimationEasings } from '@common/utils/animation';
+import {
+	__DEFAULT_ANIMATION_EASINGS__,
+	__DEFAULT_ANIMATION_IN__,
+	__DEFAULT_ANIMATION_TRANSITION__,
+	__DEFAULT_ANIMATION_TRANSITION_CONFIG__,
+	__DEFAULT_ANIMATION_TRANSITION_END__,
+	__DEFAULT_ANIMATION_UNMOUNT_ON_EXIT__
+} from './props';
 
-export const __DEFAULT_ANIMATION_IN__ = false;
-export const __DEFAULT_ANIMATION_UNMOUNT_ON_EXIT__ = true;
-
-export const __DEFAULT_ANIMATION_EASINGS__ = (['ease-in', 'ease-out', 'ease-in-out'] as EasingsArr)
-	.map((ease) => ({ key: ease, value: getAnimationEasings(ease) }))
-	.reduce((easings, ease) => ({ ...easings, [ease.key]: ease.value }), {}) as Record<Ease, AnimationEasing>;
-
-export const __DEFAULT_ANIMATION_TRANSITION__ = {
-	enter: {
-		duration: getAnimationDuration(),
-		ease: __DEFAULT_ANIMATION_EASINGS__['ease-out']
-	},
-	exit: {
-		duration: getAnimationDuration(),
-		ease: __DEFAULT_ANIMATION_EASINGS__['ease-in']
-	}
-};
-
-export const __DEFAULT_ANIMATION_TRANSITION_END__ = {
-	enter: {},
-	exit: {}
-};
-
-export const __DEFAULT_ANIMATION_TRANSITION_CONFIG__ = {
-	initial: 'exit',
-	animate: 'enter',
-	exit: 'exit'
+export {
+	__DEFAULT_ANIMATION_EASINGS__,
+	__DEFAULT_ANIMATION_IN__,
+	__DEFAULT_ANIMATION_TRANSITION__,
+	__DEFAULT_ANIMATION_TRANSITION_CONFIG__,
+	__DEFAULT_ANIMATION_TRANSITION_END__,
+	__DEFAULT_ANIMATION_UNMOUNT_ON_EXIT__
 };
--- src/components/Overlay/components/ConfirmModal/components/ConfirmModalTransition/common/constants/props.ts
+++ src/components/Overlay/components/ConfirmModal/components/ConfirmModalTransition/common/constants/props.ts
@@ -1,10 +1,12 @@
 import { getAnimationDuration } from '@common/utils';
 
 // TODO: Go over all /common/constants and extract them from root constants object instead of directly import
-import { __DEFAULT_TRANSITION_EASINGS__ } from '@components/Animation/common/constants';
+import { constants } from '@components/Animation';
 import { __DEFAULT_CONFIRM_MODAL_DURATION__ } from '@components/Overlay/components/ConfirmModal/common/constants';
 
 import type { ConfirmModalTransitionVariant } from '../types';
+
+const { __DEFAULT_ANIMATION_EASINGS__ } = constants;
 
 export const __DEFAULT_CONFIRM_MODAL_TRANSITION_INITIAL_SCALE__ = 0.75;
 
@@ -13,10 +15,10 @@ export const __DEFAULT_CONFIRM_MODAL_TRANSITION_VARIANT__: ConfirmModalTransitio
 export const __DEFAULT_CONFIRM_MODAL_TRANSITION_TRANSITION__ = {
 	enter: {
 		duration: getAnimationDuration(__DEFAULT_CONFIRM_MODAL_DURATION__),
-		ease: __DEFAULT_TRANSITION_EASINGS__['ease-out']
+		ease: __DEFAULT_ANIMATION_EASINGS__['ease-out']
 	},
 	exit: {
 		duration: getAnimationDuration(__DEFAULT_CONFIRM_MODAL_DURATION__),
-		ease: __DEFAULT_TRANSITION_EASINGS__['ease-in']
+		ease: __DEFAULT_ANIMATION_EASINGS__['ease-in']
 	}
 };
