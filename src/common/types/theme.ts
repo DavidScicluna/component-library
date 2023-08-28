f8c4446ee37eb1c368cc3b3358305d21a90bd1d4
DavidScicluna <dave_nostra@hotmail.com>
2023-08-28 21:16:33 +0200

    Fix: Types - Theme - Updated type names

--- src/common/types/theme.ts
+++ src/common/types/theme.ts
@@ -1,19 +1,32 @@
-// TODO: Replace all types to have Theme included in the name
+export type ThemeAppColor = Exclude<ThemeColor, 'transparent' | 'black' | 'white' | 'gray'>;
+export type ThemeAppColorArr = Array<ThemeAppColor>;
+export type ThemeAppColorObj = Record<ThemeAppColor, string>;
 
-export type AspectRatio = 'auto' | 'square' | 'video' | 'widescreen' | 'portrait' | 'standard' | 'vertical';
-export type AspectRatioArr = Array<AspectRatio>;
-export type AspectRatioObj = Record<AspectRatio, string>;
+export type ThemeAppColorMode = Exclude<ThemeColorMode, 'system'>;
 
-export type AppColor = Exclude<Color, 'transparent' | 'black' | 'white' | 'gray'>;
-export type AppColors = Array<AppColor>;
+export type ThemeAppearanceProps = Partial<{
+	color: ThemeColor;
+	colorMode: ThemeAppColorMode;
+}>;
 
-export type AppColorMode = Exclude<ColorMode, 'system'>;
+export type ThemeAppAppearanceProps = Partial<{
+	color: ThemeAppColor;
+	colorMode: ThemeAppColorMode;
+}>;
 
-export type BorderWidth = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-export type BorderWidthArr = Array<BorderWidth>;
-export type BorderWidthObj = Record<BorderWidth, string>;
+export type ThemeAspectRatio = 'auto' | 'square' | 'video' | 'widescreen' | 'portrait' | 'standard' | 'vertical';
+export type ThemeAspectRatioArr = Array<ThemeAspectRatio>;
+export type ThemeAspectRatioObj = Record<ThemeAspectRatio, string>;
 
-export type Color =
+export type ThemeBreakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+export type ThemeBreakpointArr = Array<ThemeBreakpoint>;
+export type ThemeBreakpointObj = Record<ThemeBreakpoint, string>;
+
+export type ThemeBorderWidth = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type ThemeBorderWidthArr = Array<ThemeBorderWidth>;
+export type ThemeBorderWidthObj = Record<ThemeBorderWidth, string>;
+
+export type ThemeColor =
 	| 'transparent'
 	| 'black'
 	| 'white'
@@ -33,50 +46,47 @@ export type Color =
 	| 'yellow'
 	| 'orange'
 	| 'deep_orange';
-export type ColorsArr = Array<Color>;
-export type ColorsObj = Record<Color, string | Record<ColorHue, string>>;
+export type ThemeColorArr = Array<ThemeColor>;
+export type ThemeColorObj = Record<ThemeColor, string | Record<ThemeColorHue, string>>;
 
-export type ColorHue = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
-export type ColorHuesArr = Array<ColorHue>;
-export type ColorHuesObj = Record<ColorHue, string>;
+export type ThemeColorHue = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
+export type ThemeColorHueArr = Array<ThemeColorHue>;
+export type ThemeColorHueObj = Record<ThemeColorHue, string>;
 
-export type ColorMode = 'light' | 'system' | 'dark';
+export type ThemeColorMode = 'light' | 'system' | 'dark';
+export type ThemeColorModeArr = Array<ThemeColorMode>;
+export type ThemeColorModeObj = Record<ThemeColorMode, string>;
 
-export type CommonThemeProps = Partial<{
-	color: Color;
-	colorMode: AppColorMode;
-}>;
+export type ThemeDelay = 'ultra-fast' | 'faster' | 'fast' | 'normal' | 'slow' | 'slower' | 'ultra-slow';
+export type ThemeDelayArr = Array<ThemeDelay>;
+export type ThemeDelayObj = Record<ThemeDelay, string>;
 
-export type CommonAppThemeProps = Partial<{
-	color: AppColor;
-	colorMode: AppColorMode;
-}>;
+export type ThemeDuration = 'ultra-fast' | 'faster' | 'fast' | 'normal' | 'slow' | 'slower' | 'ultra-slow';
+export type ThemeDurationArr = Array<ThemeDuration>;
+export type ThemeDurationObj = Record<ThemeDuration, string>;
 
-export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-export type BreakpointsArr = Array<Breakpoint>;
-export type BreakpointsObj = Record<Breakpoint, string>;
+export type ThemeEase = 'linear' | 'ease-in' | 'ease-out' | 'ease-in-out';
+export type ThemeEaseArr = Array<ThemeEase>;
+export type ThemeEaseObj = Record<ThemeEase, string>;
 
-export type Radius = 'none' | 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'full';
-export type RadiiArr = Array<Radius>;
-export type RadiiObj = Record<Radius, string>;
+export type ThemeFontSize =
+	| 'xs'
+	| 'sm'
+	| 'md'
+	| 'lg'
+	| 'xl'
+	| '2xl'
+	| '3xl'
+	| '4xl'
+	| '5xl'
+	| '6xl'
+	| '7xl'
+	| '8xl'
+	| '9xl';
+export type ThemeFontSizeArr = Array<ThemeFontSize>;
+export type ThemeFontSizeObj = Record<ThemeFontSize, string>;
 
-export type Ease = 'linear' | 'ease-in' | 'ease-out' | 'ease-in-out';
-export type EasingsArr = Array<Ease>;
-export type EasingsObj = Record<Ease, string>;
-
-export type Delay = 'ultra-fast' | 'faster' | 'fast' | 'normal' | 'slow' | 'slower' | 'ultra-slow';
-export type DelaysArr = Array<Delay>;
-export type DelaysObj = Record<Delay, string>;
-
-export type Duration = 'ultra-fast' | 'faster' | 'fast' | 'normal' | 'slow' | 'slower' | 'ultra-slow';
-export type DurationsArr = Array<Duration>;
-export type DurationsObj = Record<Duration, string>;
-
-export type FontSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl' | '8xl' | '9xl';
-export type FontSizesArr = Array<FontSize>;
-export type FontSizesObj = Record<FontSize, string>;
-
-export type FontWeight =
+export type ThemeFontWeight =
 	| 'thin'
 	| 'extralight'
 	| 'light'
@@ -86,30 +96,32 @@ export type FontWeight =
 	| 'bold'
 	| 'extrabold'
 	| 'black';
-export type FontWeightsArr = Array<FontWeight>;
-export type FontWeightsObj = Record<FontWeight, string | number>;
+export type ThemeFontWeightArr = Array<ThemeFontWeight>;
+export type ThemeFontWeightObj = Record<ThemeFontWeight, string | number>;
 
-export type TextAlign = 'left' | 'center' | 'right' | 'justify' | 'start' | 'end';
-export type TextAlignsArr = Array<TextAlign>;
-export type TextAlignsObj = Record<TextAlign, string>;
+export type ThemeLineHeight =
+	| 'none'
+	| 'tight'
+	| 'snug'
+	| 'normal'
+	| 'relaxed'
+	| 'loose'
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10;
+export type ThemeLineHeightArr = Array<ThemeLineHeight>;
+export type ThemeLineHeightObj = Record<ThemeLineHeight, string | number>;
 
-export type TextTransform = 'uppercase' | 'lowercase' | 'capitalize' | 'normal';
-export type TextTransformsArr = Array<TextTransform>;
-export type TextTransformsObj = Record<TextTransform, string>;
+export type ThemeRadius = 'none' | 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'full';
+export type ThemeRadiusArr = Array<ThemeRadius>;
+export type ThemeRadiusObj = Record<ThemeRadius, string>;
 
-export type LineHeight = 'none' | 'tight' | 'snug' | 'normal' | 'relaxed' | 'loose' | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-export type LineHeightsArr = Array<LineHeight>;
-export type LineHeightsObj = Record<LineHeight, string | number>;
-
-export type Whitespace = 'normal' | 'nowrap' | 'pre' | 'pre-line' | 'pre-wrap' | 'break-spaces';
-export type WhitespacesArr = Array<Whitespace>;
-export type WhitespacesObj = Record<Whitespace, string>;
-
-export type WordBreak = 'normal' | 'words' | 'all' | 'keep';
-export type WordBreaksArr = Array<WordBreak>;
-export type WordBreaksObj = Record<WordBreak, string>;
-
-export type Space =
+export type ThemeSpacing =
 	| 0
 	| 0.25
 	| 0.5
@@ -210,5 +222,21 @@ export type Space =
 	| 24.25
 	| 24.5
 	| 24.75;
-export type SpacingArr = Array<Space>;
-export type SpacingObj = Record<Space, string>;
+export type ThemeSpacingArr = Array<ThemeSpacing>;
+export type ThemeSpacingObj = Record<ThemeSpacing, string>;
+
+export type ThemeTextAlign = 'left' | 'center' | 'right' | 'justify' | 'start' | 'end';
+export type ThemeTextAlignArr = Array<ThemeTextAlign>;
+export type ThemeTextAlignObj = Record<ThemeTextAlign, string>;
+
+export type ThemeTextTransform = 'uppercase' | 'lowercase' | 'capitalize' | 'normal';
+export type ThemeTextTransformArr = Array<ThemeTextTransform>;
+export type ThemeTextTransformObj = Record<ThemeTextTransform, string>;
+
+export type ThemeWhitespace = 'normal' | 'nowrap' | 'pre' | 'pre-line' | 'pre-wrap' | 'break-spaces';
+export type ThemeWhitespaceArr = Array<ThemeWhitespace>;
+export type ThemeWhitespaceObj = Record<ThemeWhitespace, string>;
+
+export type ThemeWordBreak = 'normal' | 'words' | 'all' | 'keep';
+export type ThemeWordBreakArr = Array<ThemeWordBreak>;
+export type ThemeWordBreakObj = Record<ThemeWordBreak, string>;
