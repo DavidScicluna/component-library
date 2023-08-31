f30bb22a8e15e2a6df58c6ff42e2199448d340b8
DavidScicluna <dave_nostra@hotmail.com>
2023-08-31 22:36:07 +0200

    Fix: Constants - Amount - Active

--- src/common/constants/amount.ts
+++ src/common/constants/amount.ts
@@ -1,4 +1,4 @@
-export const __DEFAULT_AMOUNT_ACTIVE__ = 0.11;
+export const __DEFAULT_AMOUNT_ACTIVE__ = 0.2;
 export const __DEFAULT_AMOUNT_BACK__ = 0.05;
 export const __DEFAULT_AMOUNT_HOVER__ = 0.1;
 export const __DEFAULT_AMOUNT_PLACEHOLDER__ = 0;
