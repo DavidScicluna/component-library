7d389edda536e112c387a06752ff21fabe740dc1
DavidScicluna <dave_nostra@hotmail.com>
2023-09-02 21:46:24 +0200

    Fix: Moved Message from Overlay to DataDisplay folder

--- src/components/DataDisplay/components/Message/Message.tsx
+++ src/components/DataDisplay/components/Message/Message.tsx
@@ -16,6 +16,7 @@ export const MessageContext = createContext<MessageContextType<PolymorphicDefaul
 	spacing: __DEFAULT_SPACING__
 });
 
+// Move Message to DataDisplay folder since its not an overlay!
 const Message = forwardRef(function Message<Element extends ElementType>(
 	props: MessageProps<Element>,
 	ref: MessageRef<Element>
--- src/components/DataDisplay/index.ts
+++ src/components/DataDisplay/index.ts
@@ -6,25 +6,68 @@ import type {
 	DividerRef,
 	DividerVariant
 } from './components/Divider';
-import {
-	constants as dividerConstants,
-	Divider,
-	useGetDividerClasses,
-	useGetDividerStyles
-} from './components/Divider';
+import { constants as dividerConstants, Divider, useDividerClasses, useDividerStyles } from './components/Divider';
 import type { HeadlineProps, HeadlineRef, HeadlineRenderProps } from './components/Headline';
 import { Headline } from './components/Headline';
 import type { IconCategory, IconDefaultElement, IconElement, IconProps, IconRef, IconVariant } from './components/Icon';
-import { constants as iconConstants, Icon, useGetIconClasses } from './components/Icon';
+import { constants as iconConstants, Icon, useIconClasses } from './components/Icon';
+import type {
+	MessageActionsChildrenProps,
+	MessageActionsProps,
+	MessageActionsRef,
+	MessageBodyProps,
+	MessageBodyRef,
+	MessageContext,
+	MessageIconDefaultElement,
+	MessageIconElement,
+	MessageIconProps,
+	MessageIconRef,
+	MessageProps,
+	MessageRef,
+	MessageStackProps,
+	MessageStackRef,
+	MessageSubtitleDefaultElement,
+	MessageSubtitleElement,
+	MessageSubtitleProps,
+	MessageSubtitleRef,
+	MessageTitleDefaultElement,
+	MessageTitleElement,
+	MessageTitleProps,
+	MessageTitleRef,
+	MessageVariant
+} from './components/Message';
+import {
+	constants as messageConstants,
+	Message,
+	MessageActions,
+	MessageBody,
+	MessageIcon,
+	MessageStack,
+	MessageSubtitle,
+	MessageTitle,
+	useMessageClasses,
+	useMessageContext
+} from './components/Message';
 
 // Constants
-export const constants = { ...dividerConstants, ...iconConstants };
+export const constants = { ...dividerConstants, ...messageConstants, ...iconConstants };
 
 // Hooks
-export { useGetDividerClasses, useGetDividerStyles, useGetIconClasses };
+export { useDividerClasses, useDividerStyles, useIconClasses, useMessageClasses, useMessageContext };
 
 // Components
-export { Divider, Headline, Icon };
+export {
+	Divider,
+	Headline,
+	Icon,
+	Message,
+	MessageActions,
+	MessageBody,
+	MessageIcon,
+	MessageStack,
+	MessageSubtitle,
+	MessageTitle
+};
 
 // Component Types
 export type {
@@ -42,5 +85,28 @@ export type {
 	IconElement,
 	IconProps,
 	IconRef,
-	IconVariant
+	IconVariant,
+	MessageActionsChildrenProps,
+	MessageActionsProps,
+	MessageActionsRef,
+	MessageBodyProps,
+	MessageBodyRef,
+	MessageContext,
+	MessageIconDefaultElement,
+	MessageIconElement,
+	MessageIconProps,
+	MessageIconRef,
+	MessageProps,
+	MessageRef,
+	MessageStackProps,
+	MessageStackRef,
+	MessageSubtitleDefaultElement,
+	MessageSubtitleElement,
+	MessageSubtitleProps,
+	MessageSubtitleRef,
+	MessageTitleDefaultElement,
+	MessageTitleElement,
+	MessageTitleProps,
+	MessageTitleRef,
+	MessageVariant
 };
