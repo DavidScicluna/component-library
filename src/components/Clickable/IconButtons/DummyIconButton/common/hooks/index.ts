9dad366e181a321f6755b492808c63ab58e59d0f
DavidScicluna <dave_nostra@hotmail.com>
2023-09-06 22:14:06 +0200

    Removed old IconButtons

--- src/components/Clickable/IconButtons/DummyIconButton/common/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-import useDummyIconButtonContext from './useDummyIconButtonContext';
-
-export { useDummyIconButtonContext };
--- src/components/Clickable/IconButtons/OriginalIconButton/components/IconButtonSpinner/common/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { NoUndefinedField } from '../../../../../../../../common/types';
-import { IconButtonProps } from '../../../../common/types';
-
-export type IconButtonSpinnerProps = NoUndefinedField<Pick<IconButtonProps, 'variant'>>;
