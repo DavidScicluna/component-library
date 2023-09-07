19081d61287b4a2606ed4531b6e73f9bd5e58019
DavidScicluna <dave_nostra@hotmail.com>
2023-09-07 22:39:44 +0200

    Feature: DummyButton - DummyButtonIcon

---
+++ src/components/Buttons/DummyButton/components/DummyButtonIcon/common/keys/index.ts
@@ -0,0 +1,3 @@
+import { __DEFAULT_CLASS_PREFIX__ } from '@common/constants';
+
+export const __KEY_DUMMY_BUTTON_ICON_CLASS__ = `${__DEFAULT_CLASS_PREFIX__}-dummy-button-icon`;
---
+++ src/components/Buttons/DummyButton/components/DummyButtonIcon/index.ts
@@ -0,0 +1,17 @@
+import { __KEY_DUMMY_BUTTON_ICON_CLASS__ } from './common/keys';
+import type {
+	DummyButtonIconDefaultElement,
+	DummyButtonIconElement,
+	DummyButtonIconProps,
+	DummyButtonIconRef
+} from './common/types';
+import DummyButtonIcon from './DummyButtonIcon';
+
+// Keys
+export const keys = { __KEY_DUMMY_BUTTON_ICON_CLASS__ };
+
+// Components
+export { DummyButtonIcon };
+
+// Component Types
+export type { DummyButtonIconDefaultElement, DummyButtonIconElement, DummyButtonIconProps, DummyButtonIconRef };
