6e0b9ab1dd31df530dafd55fac05be29b73738e1
DavidScicluna <dave_nostra@hotmail.com>
2023-09-07 22:40:56 +0200

    Feature: DummyButton - DummyButtonSkeleton

---
+++ src/components/Buttons/DummyButton/components/DummyButtonSkeleton/DummyButtonSkeleton.tsx
@@ -0,0 +1,47 @@
+import type { ElementType, ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASSNAME__ } from '@common/constants';
+
+import { Skeleton } from '@components/Feedback';
+import { __KEY_SKELETON_OVERLAY_CLASS__ } from '@components/Feedback/components/Skeleton/common/keys';
+import { useDummyPushableOverlayStyles } from '@components/Overlay/components/DummyPushableOverlay';
+
+import { useDummyButtonContext } from '../../common/hooks';
+
+import { __KEY_DUMMY_BUTTON_SKELETON_CLASS__ } from './common/keys';
+import type { DummyButtonSkeletonProps, DummyButtonSkeletonRef } from './common/types';
+
+const DummyButtonSkeleton = forwardRef(function DummyButtonSkeleton<Element extends ElementType>(
+	props: DummyButtonSkeletonProps<Element>,
+	ref: DummyButtonSkeletonRef<Element>
+): ReactElement {
+	const { color, colorMode, variant } = useDummyButtonContext();
+
+	const { children, className = __DEFAULT_CLASSNAME__, ...rest } = props;
+
+	const styles = useDummyPushableOverlayStyles({
+		color,
+		colorMode,
+		variant: variant === 'text' ? 'transparent' : variant
+	});
+
+	return (
+		<Skeleton<Element>
+			{...rest}
+			ref={ref}
+			className={classNames(__KEY_DUMMY_BUTTON_SKELETON_CLASS__, { [className]: !!className })}
+			color={color}
+			colorMode={colorMode}
+			isAnimated={false}
+			isLoaded={false}
+			sx={{ [`& .${__KEY_SKELETON_OVERLAY_CLASS__}`]: { background: styles.color } }}
+		>
+			{children}
+		</Skeleton>
+	);
+});
+
+export default DummyButtonSkeleton;
---
+++ src/components/Buttons/DummyButton/components/DummyButtonSkeleton/index.ts
@@ -0,0 +1,12 @@
+import { __KEY_DUMMY_BUTTON_SKELETON_CLASS__ } from './common/keys';
+import type { DummyButtonSkeletonProps, DummyButtonSkeletonRef } from './common/types';
+import DummyButtonSkeleton from './DummyButtonSkeleton';
+
+// Keys
+export const keys = { __KEY_DUMMY_BUTTON_SKELETON_CLASS__ };
+
+// Components
+export { DummyButtonSkeleton };
+
+// Component Types
+export type { DummyButtonSkeletonProps, DummyButtonSkeletonRef };
