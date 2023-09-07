e81d88cea7cde38fcf64283849a6bb6af2b66057
DavidScicluna <dave_nostra@hotmail.com>
2023-09-07 23:08:16 +0200

    Fix: Theme - FontSizes

--- src/common/theme/index.ts
+++ src/common/theme/index.ts
@@ -310,9 +310,9 @@ const container: ThemeBreakpointObj = {
 const fontSizes: ThemeFontSizeObj = {
 	'xs': '0.75rem' /* 12px */,
 	'sm': '0.875rem' /* 14px */,
-	'lg': '1rem' /* 16px */,
-	'xl': '1.125rem' /* 18px */,
-	'md': '1.25rem' /* 20px */,
+	'md': '1rem' /* 16px */,
+	'lg': '1.125rem' /* 18px */,
+	'xl': '1.25rem' /* 20px */,
 	'2xl': '1.5rem' /* 24px */,
 	'3xl': '1.875rem' /* 30px */,
 	'4xl': '2.25rem' /* 36px */,
