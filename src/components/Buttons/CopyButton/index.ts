78cd330c52306e54584ae51be4f4eedf37af060a
DavidScicluna <dave_nostra@hotmail.com>
2023-09-08 11:25:40 +0200

    Fix: CopyButton

--- src/components/Buttons/CopyButton/index.ts
+++ src/components/Buttons/CopyButton/index.ts
@@ -1,13 +1,10 @@
-import { __DEFAULT_COPY_BUTTON_DURATION__, __DEFAULT_COPY_BUTTON_TIMEOUT__ } from './common/constants';
+import * as commonConstants from './common/constants';
 import { useCopyToClipboard } from './common/hooks';
 import type { CopyButtonChildrenProps, CopyButtonProps, CopyButtonTimeout } from './common/types';
 import CopyButton from './CopyButton';
 
 // Constants
-export const constants = {
-	__DEFAULT_COPY_BUTTON_DURATION__,
-	__DEFAULT_COPY_BUTTON_TIMEOUT__
-};
+export const constants = { ...commonConstants };
 
 // Hooks
 export { useCopyToClipboard };
