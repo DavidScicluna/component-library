f891df15e81d5aceb42c3ed2c25cf4b03dfb5ae4
DavidScicluna <dave_nostra@hotmail.com>
2023-09-11 23:33:16 +0200

    Fix: Fade - Keys - type error

--- src/components/Animation/components/Fade/common/keys/index.ts
+++ src/components/Animation/components/Fade/common/keys/index.ts
@@ -1,8 +1,8 @@
 import { __DEFAULT_CLASS_PREFIX__ } from '@common/constants';
+import type { PolymorphicDefaultElement } from '@common/types';
 
 import type { FadeProps } from '../types';
-import { PolymorphicDefaultElement } from '@common/types';
 
 export const __KEYS_FADE_CLASS__ = `${__DEFAULT_CLASS_PREFIX__}-fade`;
 
-export const __KEYS_FADE__: Array<keyof FadeProps<PolymorphicDefaultElement> = ['transition', 'transitionEnd'];
+export const __KEYS_FADE__: Array<keyof FadeProps<PolymorphicDefaultElement>> = ['transition', 'transitionEnd'];
