4d6965628d33830de6dfe7ab9d81944afbc07176
DavidScicluna <dave_nostra@hotmail.com>
2023-09-15 22:20:25 +0200

    Fix: HorizontalScroll - HorizontalScrollArrowIconButton

---
+++ src/components/DataDisplay/HorizontalScroll/components/HorizontalScrollArrowIconButton/common/constants/props.ts
@@ -0,0 +1,8 @@
+import type { TooltipPlacement } from '@components/Overlay/components/Tooltip';
+
+export const __DEFAULT_HORIZONTAL_SCROLL_ARROW_ICON_BUTTON_HAS_TOOLTIP__ = true;
+
+export const __DEFAULT_HORIZONTAL_SCROLL_ARROW_ICON_BUTTON_LEFT_LABEL__ = 'Scroll Left';
+export const __DEFAULT_HORIZONTAL_SCROLL_ARROW_ICON_BUTTON_PLACEMENT__ = 'Scroll Right';
+
+export const __DEFAULT_HORIZONTAL_SCROLL_ARROW_ICON_BUTTON_RIGHT_LABEL__: TooltipPlacement = 'top';
---
+++ src/components/DataDisplay/HorizontalScroll/components/HorizontalScrollArrowIconButton/index.ts
@@ -0,0 +1,26 @@
+import * as commonConstants from './common/constants';
+import * as commonKeys from './common/keys';
+import type {
+	HorizontalScrollArrowIconButtonDefaultElement,
+	HorizontalScrollArrowIconButtonElement,
+	HorizontalScrollArrowIconButtonProps,
+	HorizontalScrollArrowIconButtonRef
+} from './common/types';
+import HorizontalScrollArrowIconButton from './HorizontalScrollArrowIconButton';
+
+// Constants
+export const constants = { ...commonConstants };
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { HorizontalScrollArrowIconButton };
+
+// Component Types
+export type {
+	HorizontalScrollArrowIconButtonDefaultElement,
+	HorizontalScrollArrowIconButtonElement,
+	HorizontalScrollArrowIconButtonProps,
+	HorizontalScrollArrowIconButtonRef
+};
