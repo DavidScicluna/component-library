9725395288f90a254cd8118b93421e7163ecd642
DavidScicluna <dave_nostra@hotmail.com>
2023-09-16 23:17:46 +0200

    Fix: DummyButton - Classes

--- src/components/Buttons/DummyButton/common/hooks/useDummyButtonClasses.ts
+++ src/components/Buttons/DummyButton/common/hooks/useDummyButtonClasses.ts
@@ -2,21 +2,9 @@ import type { ElementType } from 'react';
 
 import classNames from 'classnames';
 
+import classes from '@common/classes';
 import { useGetClass } from '@common/hooks';
-import type {
-	ClassName,
-	TextAlignClass,
-	// OutlineStyleClass,
-	// OutlineWidthClass,
-	TextLetterSpacingClass,
-	TextOverflowClass,
-	TextTransformClass,
-	ThemeFontSize,
-	ThemeFontWeight,
-	ThemeLineHeight,
-	WhitespaceClass,
-	WidthClass
-} from '@common/types';
+import type { ClassName, WidthClass } from '@common/types';
 
 import {
 	__DEFAULT_DUMMY_BUTTON_IS_COMPACT__,
@@ -51,33 +39,16 @@ const useDummyButtonClasses = <Element extends ElementType>(
 
 	const widthClassName = useGetClass<WidthClass>(isFullWidth ? 'full' : 'auto', ['sizing', 'width']);
 
-	const alignClassName = useGetClass<TextAlignClass>('center', ['typography', 'align']);
-	const fontSizeClassName = useGetClass<ThemeFontSize>(config.fontSize, ['typography', 'font_size']);
-	const fontWeightClassName = useGetClass<ThemeFontWeight>('semibold', ['typography', 'font_weight']);
-	const letterSpacingClassName = useGetClass<TextLetterSpacingClass>('normal', ['typography', 'letter_spacing']);
-	const lineHeightClassName = useGetClass<ThemeLineHeight>(__DEFAULT_DUMMY_BUTTON_LINE_HEIGHT_SIZE__, [
-		'typography',
-		'line_height'
-	]);
-	const textTransformClassName = useGetClass<TextTransformClass>('uppercase', ['typography', 'transform']);
-	const textOverflowClassName = useGetClass<TextOverflowClass>('ellipsis', ['typography', 'text_overflow']);
-	const whitespaceClassName = useGetClass<WhitespaceClass>('nowrap', ['typography', 'whitespace']);
-
-	// const outlineWidthClassName = useGetClass<OutlineWidthClass>(0, ['borders', 'outline_width']);
-	// const outlineStyleClassName = useGetClass<OutlineStyleClass>('none', ['borders', 'outline_style']);
-
 	return classNames(
 		widthClassName,
-		alignClassName,
-		fontSizeClassName,
-		fontWeightClassName,
-		letterSpacingClassName,
-		lineHeightClassName,
-		textTransformClassName,
-		textOverflowClassName,
-		whitespaceClassName
-		// outlineWidthClassName,
-		// outlineStyleClassName
+		classes.typography.align.center,
+		classes.typography.font_size[config.fontSize],
+		classes.typography.font_weight.semibold,
+		classes.typography.letter_spacing.normal,
+		classes.typography.line_height[__DEFAULT_DUMMY_BUTTON_LINE_HEIGHT_SIZE__],
+		classes.typography.transform.uppercase,
+		classes.typography.text_overflow.ellipsis,
+		classes.typography.whitespace.nowrap
 	);
 };
 
