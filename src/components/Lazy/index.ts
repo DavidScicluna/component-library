6aa09d7d261775eae41704dca4f64aca1fccbdfa
DavidScicluna <dave_nostra@hotmail.com>
2023-09-16 21:40:10 +0200

    Fix: Lazy - Moved Suspense & ErrorBoundary to components folder

---
+++ src/components/Lazy/index.ts
@@ -0,0 +1,10 @@
+import type { ErrorBoundaryProps } from './components/ErrorBoundary';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import type { SuspenseProps } from './components/Suspense';
+import { Suspense } from './components/Suspense';
+
+// Components
+export { ErrorBoundary, Suspense };
+
+// Component Types
+export type { ErrorBoundaryProps, SuspenseProps };
