4608a85c4e3c6dea67fbfe6876e14a016ed09176
DavidScicluna <dave_nostra@hotmail.com>
2023-09-19 22:24:58 +0200

    Fix: LoadingOverlay - useLoadingOverlayStyles - Updated default color & colorMode props

--- src/components/Overlay/components/LoadingOverlay/common/hooks/useLoadingOverlayStyles.ts
+++ src/components/Overlay/components/LoadingOverlay/common/hooks/useLoadingOverlayStyles.ts
@@ -2,7 +2,8 @@ import type { ElementType } from 'react';
 
 import { transparentize } from 'color2k';
 
-import { useGetColor, useGetResponsiveValue } from '@common/hooks';
+import { __DEFAULT_COLOR__ } from '@common/constants';
+import { useAppTheme, useGetColor, useGetResponsiveValue } from '@common/hooks';
 import type { Style } from '@common/types';
 
 import { __DEFAULT_LOADING_OVERLAY_BACKDROP_AMOUNT__, __DEFAULT_LOADING_OVERLAY_HAS_BACKGROUND__ } from '../constants';
@@ -17,9 +18,11 @@ type UseLoadingOverlayStylesReturn = Style;
 const useLoadingOverlayStyles = <Element extends ElementType>(
 	props: UseLoadingOverlayStylesProps<Element>
 ): UseLoadingOverlayStylesReturn => {
+	const { colorMode: __DEFAULT_LOADING_OVERLAY_COLORMODE__ } = useAppTheme();
+
 	const {
-		color,
-		colorMode,
+		color = __DEFAULT_COLOR__,
+		colorMode = __DEFAULT_LOADING_OVERLAY_COLORMODE__,
 		backdropAmount: ba = __DEFAULT_LOADING_OVERLAY_BACKDROP_AMOUNT__,
 		hasBackground = __DEFAULT_LOADING_OVERLAY_HAS_BACKGROUND__
 	} = props;
