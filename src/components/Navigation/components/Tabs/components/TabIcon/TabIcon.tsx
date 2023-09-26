71287e5e3195027f1446d68e7d1321e9f55cd26c
DavidScicluna <dave_nostra@hotmail.com>
2023-09-26 23:59:47 +0200

    Feature: Tabs - TabIcon

---
+++ src/components/Navigation/components/Tabs/components/TabIcon/TabIcon.tsx
@@ -0,0 +1,47 @@
+import type { ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASSNAME__ } from '@common/constants';
+
+import { Icon } from '@components/DataDisplay';
+
+import { useTabsContext, useTabsTabFontSize } from '../../common/hooks';
+
+import { __KEYS_TAB_ICON_CLASS__ } from './common/keys';
+import type { TabIconDefaultElement, TabIconElement, TabIconProps, TabIconRef } from './common/types';
+
+const TabIcon = forwardRef(function TabIcon<Element extends TabIconElement = TabIconDefaultElement>(
+	props: TabIconProps<Element>,
+	ref: TabIconRef<Element>
+): ReactElement {
+	const { color: __DEFAULT_TAB_ICON_COLOR__, colorMode: __DEFAULT_TAB_ICON_COLORMODE__, size } = useTabsContext();
+
+	const {
+		className = __DEFAULT_CLASSNAME__,
+		color = __DEFAULT_TAB_ICON_COLOR__,
+		colorMode = __DEFAULT_TAB_ICON_COLORMODE__,
+		...rest
+	} = props;
+
+	const fontSize = useTabsTabFontSize({ size });
+
+	return (
+		<Icon
+			{...rest}
+			ref={ref}
+			className={classNames(__KEYS_TAB_ICON_CLASS__, { [className]: !!className })}
+			w={`${fontSize}px`}
+			h={`${fontSize}px`}
+			color={color}
+			colorMode={colorMode}
+			size={`${fontSize}px`}
+			variant='unstyled'
+		/>
+	);
+});
+
+TabIcon.displayName = 'TabIcon';
+
+export default TabIcon;
---
+++ src/components/Navigation/components/Tabs/components/TabIcon/index.ts
@@ -0,0 +1,12 @@
+import * as commonKeys from './common/keys';
+import type { TabIconDefaultElement, TabIconElement, TabIconProps, TabIconRef } from './common/types';
+import TabIcon from './TabIcon';
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { TabIcon };
+
+// Component Types
+export type { TabIconDefaultElement, TabIconElement, TabIconProps, TabIconRef };
