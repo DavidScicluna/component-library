55cc56a6039103c4a0b19e2e34aa12659337aae7
DavidScicluna <dave_nostra@hotmail.com>
2023-09-27 20:20:37 +0200

    Fix: TabBars - Removed OriginalTabBar

--- src/components/Navigation/TabBars/OriginalTabBar/components/TabBarList/components/Tab/common/styles/active/index.ts
+++
@@ -1,8 +0,0 @@
-import { Style } from '../../../../../../../../../../../common/types';
-
-export default (): Style => ({
-	'cursor': 'default',
-	'pointerEvents': 'none',
-
-	'& .ds-cl-tab-bar-tab-stack': { opacity: 1 }
-});
--- src/components/Navigation/TabBars/OriginalTabBar/components/TabBarList/components/Tab/common/styles/light/index.ts
+++
@@ -1,5 +0,0 @@
-import active from './active';
-import disabled from './disabled';
-import tab from './tab';
-
-export default { active, disabled, tab };
