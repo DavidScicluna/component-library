7194a3899c2c12983aefef4f2afff6f35d8e9798
DavidScicluna <dave_nostra@hotmail.com>
2023-10-01 15:48:47 +0200

    Feature: Carousel - CarouselOverlayArrowIconButton

---
+++ src/components/DataDisplay/components/Carousel/components/CarouselOverlayArrowIconButton/index.ts
@@ -0,0 +1,26 @@
+import * as commonConstants from './common/constants';
+import * as commonKeys from './common/keys';
+import type {
+	CarouselOverlayArrowIconButtonDefaultElement,
+	CarouselOverlayArrowIconButtonElement,
+	CarouselOverlayArrowIconButtonProps,
+	CarouselOverlayArrowIconButtonRef
+} from './common/types';
+import CarouselOverlayArrowIconButton from './CarouselOverlayArrowIconButton';
+
+// Constants
+export const constants = { ...commonConstants };
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { CarouselOverlayArrowIconButton };
+
+// Component Types
+export type {
+	CarouselOverlayArrowIconButtonDefaultElement,
+	CarouselOverlayArrowIconButtonElement,
+	CarouselOverlayArrowIconButtonProps,
+	CarouselOverlayArrowIconButtonRef
+};
