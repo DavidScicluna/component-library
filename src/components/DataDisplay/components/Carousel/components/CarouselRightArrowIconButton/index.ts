3eb4a14d30edb1e5266934fa9a1984e13ac06bc1
DavidScicluna <dave_nostra@hotmail.com>
2023-10-01 15:50:04 +0200

    Feature: Carousel - CarouselRightArrowIconButton

---
+++ src/components/DataDisplay/components/Carousel/components/CarouselRightArrowIconButton/index.ts
@@ -0,0 +1,22 @@
+import * as commonKeys from './common/keys';
+import type {
+	CarouselRightArrowIconButtonDefaultElement,
+	CarouselRightArrowIconButtonElement,
+	CarouselRightArrowIconButtonProps,
+	CarouselRightArrowIconButtonRef
+} from './common/types';
+import CarouselRightArrowIconButton from './CarouselRightArrowIconButton';
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { CarouselRightArrowIconButton };
+
+// Component Types
+export type {
+	CarouselRightArrowIconButtonDefaultElement,
+	CarouselRightArrowIconButtonElement,
+	CarouselRightArrowIconButtonProps,
+	CarouselRightArrowIconButtonRef
+};
