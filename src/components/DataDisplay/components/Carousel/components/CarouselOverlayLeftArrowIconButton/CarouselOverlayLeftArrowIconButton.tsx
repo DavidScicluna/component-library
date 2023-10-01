8e0916274410cff720c41f2579c2aee490680609
DavidScicluna <dave_nostra@hotmail.com>
2023-10-01 15:49:20 +0200

    Feature: Carousel - CarouselOverlayLeftArrowIconButton

---
+++ src/components/DataDisplay/components/Carousel/components/CarouselOverlayLeftArrowIconButton/CarouselOverlayLeftArrowIconButton.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable max-len */
+import type { ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+
+import { __DEFAULT_CLASSNAME__ } from '@common/constants';
+
+import { useCarouselArrowState, useCarouselManager } from '../../common/hooks';
+import type { CarouselArrowIconButtonMouseEvent } from '../CarouselArrowIconButton';
+import { CarouselOverlayArrowIconButton } from '../CarouselOverlayArrowIconButton';
+
+import { __KEYS_CAROUSEL_OVERLAY_LEFT_ARROW_ICON_BUTTON_CLASS__ } from './common/keys';
+import type {
+	CarouselOverlayLeftArrowIconButtonDefaultElement,
+	CarouselOverlayLeftArrowIconButtonElement,
+	CarouselOverlayLeftArrowIconButtonProps,
+	CarouselOverlayLeftArrowIconButtonRef
+} from './common/types';
+
+const CarouselOverlayLeftArrowIconButton = forwardRef(function CarouselOverlayLeftArrowIconButton<
+	Element extends CarouselOverlayLeftArrowIconButtonElement = CarouselOverlayLeftArrowIconButtonDefaultElement
+>(
+	props: CarouselOverlayLeftArrowIconButtonProps<Element>,
+	ref: CarouselOverlayLeftArrowIconButtonRef<Element>
+): ReactElement {
+	const { className = __DEFAULT_CLASSNAME__, onClick, ...rest } = props;
+
+	const { scrollPrev } = useCarouselManager();
+	const { isVisible } = useCarouselArrowState('left');
+
+	const handleScrollPrev = (event: CarouselArrowIconButtonMouseEvent): void => {
+		scrollPrev();
+
+		if (onClick) {
+			onClick(event);
+		}
+	};
+
+	return (
+		<CarouselOverlayArrowIconButton<Element>
+			{...rest}
+			ref={ref}
+			className={classNames(__KEYS_CAROUSEL_OVERLAY_LEFT_ARROW_ICON_BUTTON_CLASS__, {
+				[className]: !!className
+			})}
+			direction='left'
+			isVisible={isVisible}
+			onClick={handleScrollPrev}
+		/>
+	);
+});
+
+CarouselOverlayLeftArrowIconButton.displayName = 'CarouselOverlayLeftArrowIconButton';
+
+export default CarouselOverlayLeftArrowIconButton;
---
+++ src/components/DataDisplay/components/Carousel/components/CarouselOverlayLeftArrowIconButton/index.ts
@@ -0,0 +1,22 @@
+import * as commonKeys from './common/keys';
+import type {
+	CarouselOverlayLeftArrowIconButtonDefaultElement,
+	CarouselOverlayLeftArrowIconButtonElement,
+	CarouselOverlayLeftArrowIconButtonProps,
+	CarouselOverlayLeftArrowIconButtonRef
+} from './common/types';
+import CarouselOverlayLeftArrowIconButton from './CarouselOverlayLeftArrowIconButton';
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { CarouselOverlayLeftArrowIconButton };
+
+// Component Types
+export type {
+	CarouselOverlayLeftArrowIconButtonDefaultElement,
+	CarouselOverlayLeftArrowIconButtonElement,
+	CarouselOverlayLeftArrowIconButtonProps,
+	CarouselOverlayLeftArrowIconButtonRef
+};
