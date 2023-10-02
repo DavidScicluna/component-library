e9045e03e00554171243a9e58fce5deb3d3c97fe
DavidScicluna <dave_nostra@hotmail.com>
2023-10-02 23:28:56 +0200

    Feature: Cards - CardTitle

---
+++ src/components/DataDisplay/components/Card/components/CardTitle/index.ts
@@ -0,0 +1,12 @@
+import * as commonKeys from './common/keys';
+import type { CardTitleDefaultElement, CardTitleElement, CardTitleProps, CardTitleRef } from './common/types';
+import CardTitle from './CardTitle';
+
+// Keys
+export const keys = { ...commonKeys };
+
+// Components
+export { CardTitle };
+
+// Component Types
+export type { CardTitleDefaultElement, CardTitleElement, CardTitleProps, CardTitleRef };
