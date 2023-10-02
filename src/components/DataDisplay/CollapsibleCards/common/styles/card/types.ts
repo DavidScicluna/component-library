5982a5f65d357c849b72d9e50e6607263d020582
DavidScicluna <dave_nostra@hotmail.com>
2023-10-02 23:25:33 +0200

    Fix: Removed CollapsibleCards (Merged with new Card)

--- src/components/DataDisplay/CollapsibleCards/common/styles/card/types.ts
+++
@@ -1,3 +0,0 @@
-import { CollapsibleCardStyleProps as AllCollapsibleCardStyleProps } from '../types';
-
-export type CollapsibleCardStyleProps = Pick<AllCollapsibleCardStyleProps, 'theme' | 'isClickable' | 'isFixed'>;
