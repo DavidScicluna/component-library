6257e7418fde638496f470963f0b671bd00b0368
DavidScicluna <dave_nostra@hotmail.com>
2023-10-05 22:45:09 +0200

    Fix: Container exports

--- src/components/Layout/components/Container/index.ts
+++ src/components/Layout/components/Container/index.ts
@@ -1,17 +1,14 @@
-import * as commonConstants from './common/constants';
-import { useContainerClasses } from './common/hooks';
-import * as commonKeys from './common/keys';
 import type { ContainerBreakpoint, ContainerBreakpoints, ContainerProps, ContainerRef } from './common/types';
 import Container from './Container';
 
 // Constants
-export const constants = { ...commonConstants };
+export * as constants from './common/constants';
 
 // Keys
-export const keys = { ...commonKeys };
+export * as keys from './common/keys';
 
 // Hooks
-export { useContainerClasses };
+export * as hooks from './common/hooks';
 
 // Components
 export { Container };
