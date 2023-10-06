a82c93414ca0ac178cc849fee02d9d6fb2fb72cd
DavidScicluna <dave_nostra@hotmail.com>
2023-10-06 21:57:06 +0200

    Fix: DeleteIconButton

--- src/components/Buttons/components/DeleteIconButton/index.ts
+++ src/components/Buttons/components/DeleteIconButton/index.ts
@@ -1,5 +1,3 @@
-import * as commonConstants from './common/constants';
-import * as commonKeys from './common/keys';
 import type {
 	DeleteIconButtonDefaultElement,
 	DeleteIconButtonElement,
@@ -9,10 +7,10 @@ import type {
 import DeleteIconButton from './DeleteIconButton';
 
 // Constants
-export const constants = { ...commonConstants };
+export * as constants from './common/constants';
 
 // Keys
-export const keys = { ...commonKeys };
+export * as keys from './common/keys';
 
 // Components
 export { DeleteIconButton };
