a95121d4ed89f37b661e5c9d0ecf7b44de0acd4c
DavidScicluna <dave_nostra@hotmail.com>
2023-10-08 21:29:11 +0200

    Fix: Types - Polymorphic

--- src/common/types/polymorphic.ts
+++ src/common/types/polymorphic.ts
@@ -1,17 +1,39 @@
-import type { ComponentPropsWithRef, ElementType, HTMLAttributes, MouseEvent } from 'react';
+import type {
+	ChangeEvent,
+	ComponentPropsWithoutRef,
+	ComponentPropsWithRef,
+	ElementType,
+	FocusEvent,
+	FormEvent,
+	HTMLAttributes,
+	MouseEvent
+} from 'react';
 
-import type { __DEFAULT_POLYMORPHIC_ELEMENT__, __DEFAULT_POLYMORPHIC_OBJECT__ } from '@common/constants';
+import type { __DEFAULT_POLYMORPHIC_OBJECT__ } from '@common/constants';
 
 import type { PickFrom, Style } from '.';
 
 export type PolymorphicDefaultElement = PickFrom<ElementType, 'div'>;
 
-export type PolymorphicMouseEvent<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> = MouseEvent<
+export type PolymorphicMouseEvent<Element extends ElementType = PolymorphicDefaultElement> = MouseEvent<
 	PolymorphicRef<Element>,
 	globalThis.MouseEvent
 >;
 
-type PolymorphicProps<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> = {
+export type PolymorphicChangeEvent<Element extends ElementType = PolymorphicDefaultElement> = ChangeEvent<
+	PolymorphicRef<Element>
+>;
+
+export type PolymorphicFocusEvent<Element extends ElementType = PolymorphicDefaultElement> = FocusEvent<
+	PolymorphicRef<Element>,
+	globalThis.FocusEvent
+>;
+
+export type PolymorphicFormEvent<Element extends ElementType = PolymorphicDefaultElement> = FormEvent<
+	PolymorphicRef<Element>
+>;
+
+type PolymorphicProps<Element extends ElementType = PolymorphicDefaultElement> = {
 	/**
 	 * The component used for the root node. Either a string to use an HTML element or a component.
 	 */
@@ -24,9 +46,9 @@ type PolymorphicProps<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC
 // & ComponentPropsWithoutRef<Element>;
 
 export type PolymorphicComponentProps<
-	Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__,
+	Element extends ElementType = PolymorphicDefaultElement,
 	Props extends object = typeof __DEFAULT_POLYMORPHIC_OBJECT__
 > = Props & PolymorphicProps<Element>;
 
-export type PolymorphicRef<Element extends ElementType = typeof __DEFAULT_POLYMORPHIC_ELEMENT__> =
+export type PolymorphicRef<Element extends ElementType = PolymorphicDefaultElement> =
 	ComponentPropsWithRef<Element>['ref'];
