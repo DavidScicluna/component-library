9179c04464e0199c4296eb3b5af0cddd6a13d0f4
DavidScicluna <dave_nostra@hotmail.com>
2023-10-15 18:34:31 +0200

    Fix: Modal - Removed ModalTransition

--- src/components/Overlay/components/Modal/components/ModalTransition/common/keys/index.ts
+++
@@ -1,7 +0,0 @@
-import { __DEFAULT_CLASS_PREFIX__ } from '@common/constants';
-
-import type { ModalTransitionProps } from '../types';
-
-export const __KEYS_MODAL_TRANSITION_CLASS__ = `${__DEFAULT_CLASS_PREFIX__}-modal-transition`;
-
-export const __KEYS_MODAL_TRANSITION__: Array<keyof ModalTransitionProps> = ['transition', 'transitionEnd'];
