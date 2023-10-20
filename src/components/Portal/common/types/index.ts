29a6b88156a65fc1884d05cb9aca876350e6a08f
DavidScicluna <dave_nostra@hotmail.com>
2023-10-20 21:29:29 +0200

    Fix: Removed Portal

--- src/components/Portal/common/types/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ElementType } from 'react';
-
-import type { BoxProps, BoxRef } from '@components/Box';
-
-export type PortalProps<Element extends ElementType> = BoxProps<Element>;
-
-export type PortalRef<Element extends ElementType> = BoxRef<Element>;
