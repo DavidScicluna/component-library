1a41c3c5b0578d648a01225f2f11a26b0cc356a8
DavidScicluna <dave_nostra@hotmail.com>
2023-10-22 16:46:27 +0200

    Fix: FormHeader

--- src/components/Forms/components/FormHeader/common/types/index.ts
+++ src/components/Forms/components/FormHeader/common/types/index.ts
@@ -5,10 +5,7 @@ import type { ThemeAppAppearanceProps } from '@common/types';
 import type { BoxProps, BoxRef } from '@components/Box';
 import type { GridProps } from '@components/Layout';
 
-export type FormHeaderRenderProps = ThemeAppAppearanceProps & {
-	w?: number;
-	h?: number;
-};
+export type FormHeaderRenderProps = ThemeAppAppearanceProps & { w?: number; h?: number };
 
 type FormHeaderOtherProps<Element extends ElementType> = Pick<GridProps<Element>, 'spacing'> & {
 	renderLeft?: (props: FormHeaderRenderProps) => ReactNode;
