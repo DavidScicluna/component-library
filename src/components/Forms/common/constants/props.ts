9be8a9199dff34764ed47494f573d37ae4132825
DavidScicluna <dave_nostra@hotmail.com>
2023-10-22 16:40:01 +0200

    Fix: Forms - Created common constants

---
+++ src/components/Forms/common/constants/props.ts
@@ -0,0 +1,14 @@
+import type { FormsCommonSize, FormsCommonVariant } from '../types';
+
+export const __DEFAULT_FORMS_IS_DISABLED__ = false;
+export const __DEFAULT_FORMS_IS_ERROR__ = false;
+export const __DEFAULT_FORMS_IS_FOCUSED__ = false;
+export const __DEFAULT_FORMS_IS_OUTLINED__ = false;
+export const __DEFAULT_FORMS_IS_READONLY__ = false;
+export const __DEFAULT_FORMS_IS_REQUIRED__ = false;
+export const __DEFAULT_FORMS_IS_SUCCESS__ = false;
+export const __DEFAULT_FORMS_IS_WARNING__ = false;
+
+export const __DEFAULT_FORMS_SIZE__: FormsCommonSize = 'md';
+
+export const __DEFAULT_FORMS_VARIANT__: FormsCommonVariant = 'outlined';
