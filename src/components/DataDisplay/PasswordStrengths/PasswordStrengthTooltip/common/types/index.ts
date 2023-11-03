9a7280b606b6e95a7d59bb47be523704f0b5dd5c
DavidScicluna <dave_nostra@hotmail.com>
2023-11-03 21:15:04 +0100

    Fix: Temporary removed PasswordStrengths

--- src/components/DataDisplay/PasswordStrengths/PasswordStrengthTooltip/common/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { PopperProps } from '../../../../../Overlay/Popper/common/types';
-
-export type PasswordStrengthTooltipProps = Omit<PopperProps, 'children'> & {
-	password: string;
-};
--- src/components/DataDisplay/PasswordStrengths/common/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type Strength = 'weak' | 'average' | 'strong' | 'excellent';
-export type Strengths = Strength[];
-
-export type StrengthType = 'lowercase' | 'uppercase' | 'symbol' | 'number';
-export type StrengthTypes = StrengthType[];
