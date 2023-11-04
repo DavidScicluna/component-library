dc0149fc99d0c749fe06a70d1ef20432a20db903
DavidScicluna <dave_nostra@hotmail.com>
2023-11-04 17:40:10 +0100

    Feature: Stepper - StepPanels

---
+++ src/components/Navigation/components/Stepper/components/StepPanels/common/keys/index.ts
@@ -0,0 +1,3 @@
+import { __DEFAULT_CLASS_PREFIX__ } from '@common/constants';
+
+export const __KEYS_STEPPER_STEP_PANELS_CLASS__ = `${__DEFAULT_CLASS_PREFIX__}-stepper-step-panels`;
---
+++ src/components/Navigation/components/Stepper/components/StepPanels/common/types/index.ts
@@ -0,0 +1,16 @@
+import type { ElementType } from 'react';
+
+import type { PolymorphicDefaultElement } from '@common/types';
+
+import type { BoxOtherProps, BoxProps, BoxRef } from '@components/Box';
+import type { StepperProps } from '@components/Navigation';
+
+export type StepPanelsProps<Element extends ElementType = PolymorphicDefaultElement> = Omit<
+	BoxProps<Element>,
+	keyof BoxOtherProps
+>;
+
+export type StepPanelProps<Element extends ElementType = PolymorphicDefaultElement> = BoxProps<Element> &
+	Pick<StepperProps<Element>, 'index'>;
+
+export type StepPanelsRef<Element extends ElementType = PolymorphicDefaultElement> = BoxRef<Element>;
