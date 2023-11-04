7bbeda3468d87ba9e44b551378ac60f29ca24130
DavidScicluna <dave_nostra@hotmail.com>
2023-11-04 17:40:18 +0100

    Feature: Stepper - StepProgress

---
+++ src/components/Navigation/components/Stepper/components/StepProgress/index.ts
@@ -0,0 +1,11 @@
+import type { StepProgressProps, StepProgressRef } from './common/types';
+import StepProgress from './StepProgress';
+
+// Keys
+export * as keys from './common/keys';
+
+// Components
+export { StepProgress };
+
+// Component Types
+export type { StepProgressProps, StepProgressRef };
