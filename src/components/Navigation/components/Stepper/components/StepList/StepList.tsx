45364718059a7b8a36770e77aad0258ab4485fe0
DavidScicluna <dave_nostra@hotmail.com>
2023-11-04 17:40:00 +0100

    Feature: Stepper - StepList

---
+++ src/components/Navigation/components/Stepper/components/StepList/StepList.tsx
@@ -0,0 +1,103 @@
+import type { ElementType, ReactElement } from 'react';
+import { forwardRef } from 'react';
+
+import classNames from 'classnames';
+import { compact, isArray } from 'lodash-es';
+import { useElementSize } from 'usehooks-ts';
+
+import { __DEFAULT_CLASSNAME__ } from '@common/constants';
+import type { PolymorphicDefaultElement } from '@common/types';
+
+import {
+	Carousel,
+	CarouselLeftLinearGradient,
+	CarouselOverlayLeftArrowIconButton,
+	CarouselOverlayRightArrowIconButton,
+	CarouselRightLinearGradient
+} from '@components/DataDisplay';
+import { Grid, GridItem } from '@components/Layout';
+
+import { useStepperContext } from '../../common/hooks';
+import { getStepListID } from '../../common/utils';
+
+import { __KEYS_STEPPER_STEP_LIST_CLASS__ } from './common/keys';
+import type { StepListProps, StepListRef } from './common/types';
+
+const StepList = forwardRef(function StepList<Element extends ElementType = PolymorphicDefaultElement>(
+	props: StepListProps<Element>,
+	ref: StepListRef<Element>
+): ReactElement {
+	const { color, colorMode, align, id, isFitted, orientation } = useStepperContext();
+
+	const [childrenRef, { width: childrenWidth, height: childrenHeight }] = useElementSize();
+
+	const { children, className = __DEFAULT_CLASSNAME__, renderLeft, renderRight, ...rest } = props;
+
+	return (
+		<Grid<Element>
+			{...rest}
+			ref={ref}
+			aria-orientation={orientation === 'top' || orientation === 'bottom' ? 'horizontal' : 'vertical'}
+			id={getStepListID(id)}
+			className={classNames(__KEYS_STEPPER_STEP_LIST_CLASS__, { [className]: !!className })}
+			role='tablist'
+			w='100%'
+			h='100%'
+			templateColumns={
+				orientation === 'top' || orientation === 'bottom'
+					? compact([renderLeft ? 'auto' : null, 'auto', renderRight ? 'auto' : null]).join(' ')
+					: 1
+			}
+			templateRows={
+				orientation === 'left' || orientation === 'right'
+					? compact([renderLeft ? 'auto' : null, 'auto', renderRight ? 'auto' : null]).join(' ')
+					: 1
+			}
+			alignItems='stretch'
+			alignContent='stretch'
+			justifyItems='stretch'
+			justifyContent='stretch'
+			spacing={0}
+		>
+			{renderLeft ? (
+				<GridItem alignSelf='center' justifySelf='center'>
+					{renderLeft({ color, colorMode, w: childrenWidth, h: childrenHeight })}
+				</GridItem>
+			) : null}
+
+			{isArray(children) ? (
+				<GridItem alignSelf='stretch' justifySelf={isFitted ? 'stretch' : align}>
+					<Carousel
+						ref={childrenRef}
+						w='100%'
+						h='100%'
+						colorMode={colorMode}
+						renderLeftAction={(_variant, props) => (
+							<CarouselOverlayLeftArrowIconButton {...props} isCompact variant='icon' />
+						)}
+						renderLeftLinearGradient={() => <CarouselLeftLinearGradient />}
+						renderRightAction={(_variant, props) => (
+							<CarouselOverlayRightArrowIconButton {...props} isCompact variant='icon' />
+						)}
+						renderRightLinearGradient={() => <CarouselRightLinearGradient />}
+						scrollAmount={1}
+						orientation={orientation === 'top' || orientation === 'bottom' ? 'horizontal' : 'vertical'}
+						variant='overlay'
+					>
+						{children}
+					</Carousel>
+				</GridItem>
+			) : null}
+
+			{renderRight ? (
+				<GridItem alignSelf='center' justifySelf='center'>
+					{renderRight({ color, colorMode, w: childrenWidth, h: childrenHeight })}
+				</GridItem>
+			) : null}
+		</Grid>
+	);
+});
+
+StepList.displayName = 'StepList';
+
+export default StepList;
---
+++ src/components/Navigation/components/Stepper/components/StepList/common/types/index.ts
@@ -0,0 +1,27 @@
+import type { ElementType, ReactNode } from 'react';
+
+import type { PolymorphicDefaultElement } from '@common/types';
+
+import type { BoxOtherProps, BoxProps, BoxRef } from '@components/Box';
+import type { StepperProps } from '@components/Navigation';
+
+export type StepListRenderProps<Element extends ElementType = PolymorphicDefaultElement> = Pick<
+	StepperProps<Element>,
+	'color' | 'colorMode'
+> & {
+	w?: number;
+	h?: number;
+};
+
+type StepListOtherProps<Element extends ElementType = PolymorphicDefaultElement> = {
+	renderLeft?: (props: StepListRenderProps<Element>) => ReactNode;
+	renderRight?: (props: StepListRenderProps<Element>) => ReactNode;
+};
+
+export type StepListProps<Element extends ElementType = PolymorphicDefaultElement> = Omit<
+	BoxProps<Element>,
+	keyof BoxOtherProps
+> &
+	StepListOtherProps<Element>;
+
+export type StepListRef<Element extends ElementType = PolymorphicDefaultElement> = BoxRef<Element>;
