1a5133eb98ea114f9a80cf01aeb5d51653c10782
DavidScicluna <dave_nostra@hotmail.com>
2023-11-06 21:54:41 +0100

    Fix: Stepper - StepIcon

--- src/components/Navigation/components/Stepper/components/StepIcon/StepIcon.tsx
+++ src/components/Navigation/components/Stepper/components/StepIcon/StepIcon.tsx
@@ -26,7 +26,6 @@ const StepIcon = forwardRef(function StepIcon<Element extends StepIconElement = 
 		className = __DEFAULT_CLASSNAME__,
 		color = __DEFAULT_STEP_ICON_COLOR__,
 		colorMode = __DEFAULT_STEP_ICON_COLORMODE__,
-		index,
 		...rest
 	} = props;
 
