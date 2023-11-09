6896b697d4ff70a4b0a1c5206c66b1bfaa6c957f
DavidScicluna <dave_nostra@hotmail.com>
2023-11-09 20:29:43 +0100

    Fix: Removed Slide (Merged into Transition)

--- src/components/Animation/components/Slide/common/constants/props.ts
+++
@@ -1,3 +0,0 @@
-export const __DEFAULT_SLIDE_OFFSET_X__ = 0;
-export const __DEFAULT_SLIDE_OFFSET_Y__ = 8;
-export const __DEFAULT_SLIDE_IS_REVERSED__ = true;
--- src/components/Animation/components/Slide/common/types/story.ts
+++
@@ -1,7 +0,0 @@
-import type { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
-
-import type Slide from '../../Slide';
-
-export type SlideStoryMeta = StorybookMeta<typeof Slide>;
-
-export type SlideStory = StorybookStory<typeof Slide>;
