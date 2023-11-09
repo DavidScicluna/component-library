d887abca775ff7998998f35aa49be72a50b7fa81
DavidScicluna <dave_nostra@hotmail.com>
2023-11-09 20:28:21 +0100

    Fix: Removed Collapse (Merged into Transition)

--- src/components/Animation/components/Collapse/common/types/story.ts
+++
@@ -1,7 +0,0 @@
-import type { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
-
-import type Collapse from '../../Collapse';
-
-export type CollapseStoryMeta = StorybookMeta<typeof Collapse>;
-
-export type CollapseStory = StorybookStory<typeof Collapse>;
