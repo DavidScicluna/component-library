2f6d6d6074b880a0f77563a34d66f4dbbadd429a
DavidScicluna <dave_nostra@hotmail.com>
2023-11-09 21:11:48 +0100

    Fix: Temporary removed PageTransition

--- src/components/Animation/components/PageTransition/common/constants/props.ts
+++
@@ -1,4 +0,0 @@
-import theme from '@common/theme';
-import { convertREMToPixels, convertStringToNumber } from '@common/utils';
-
-export const __DEFAULT_PAGE_TRANSITION_BLUR__ = convertREMToPixels(convertStringToNumber(theme.spacing[2], 'rem'));
