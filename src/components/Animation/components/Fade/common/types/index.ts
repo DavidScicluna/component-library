8d61e1b90cd11d805f8b519ce0d7ee3c0663bd31
DavidScicluna <dave_nostra@hotmail.com>
2023-11-09 20:28:54 +0100

    Fix: Removed Fade (Merged into Transition)

--- src/components/Animation/components/Fade/common/types/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ElementType } from 'react';
-
-import type { PolymorphicDefaultElement } from '@common/types';
-
-import type { AnimationCommonProps, AnimationCommonRef } from '@components/Animation';
-
-export type FadeProps<Element extends ElementType = PolymorphicDefaultElement> = AnimationCommonProps<Element>;
-
-export type FadeRef<Element extends ElementType = PolymorphicDefaultElement> = AnimationCommonRef<Element>;
--- src/components/Animation/components/Fade/common/types/story.ts
+++
@@ -1,7 +0,0 @@
-import type { Meta as StorybookMeta, StoryFn as StorybookStory } from '@storybook/react';
-
-import type Fade from '../../Fade';
-
-export type FadeStoryMeta = StorybookMeta<typeof Fade>;
-
-export type FadeStory = StorybookStory<typeof Fade>;
