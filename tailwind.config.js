c89a3e35994b0ae0d0701fbd0ba789c07619bb47
DavidScicluna <dave_nostra@hotmail.com>
2023-11-22 22:30:37 +0100

    Fix: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,6 @@ import theme from './src/common/theme';
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: { ...theme },
-	plugins: [require('tailwind-scrollbar-hide')]
+	plugins: [require('tailwind-scrollbar-hide')],
+	...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
 };
