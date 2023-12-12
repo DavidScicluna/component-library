18d3ec09290429ce50ee660b94fc02e56c9a51e9
DavidScicluna <dave_nostra@hotmail.com>
2023-12-12 22:32:18 +0100

    Fix: globals.d.ts

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,13 +1,10 @@
-// import type { ForwardedRef, ReactNode, RefAttributes } from 'react';
+import type { ForwardedRef, ReactNode, RefAttributes } from 'react';
+import type { Nullish } from 'utility-types';
 
-// import type { Nullish } from 'utility-types';
-
-// declare module 'react' {
-// 	// eslint-disable-next-line @typescript-eslint/ban-types
-// 	function forwardRef<T, P = {}>(
-// 		render: (props: P, ref: ForwardedRef<T>) => Nullish<ReactNode>,
-// 		displayName: string
-// 	): (props: P & RefAttributes<T>) => Nullish<ReactNode>;
-// }
+declare module 'react' {
+	function forwardRef<T, P = {}>(
+		render: (props: P, ref: Ref<T>) => Nullish<ReactNode>
+	): (props: P & RefAttributes<T>) => Nullish<ReactNode>;
+}
 
 declare module '*.png';
