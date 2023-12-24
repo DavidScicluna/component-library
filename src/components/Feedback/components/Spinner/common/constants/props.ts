392e625659ed73182bc5f0230c588f1e3829ec8f
DavidScicluna <dave_nostra@hotmail.com>
2023-12-24 19:06:59 +0100

    Removed old Spinner

--- src/components/Feedback/components/Spinner/common/constants/props.ts
+++
@@ -1,5 +0,0 @@
-import type { SpinnerSize, SpinnerVariant } from '../types';
-
-export const __DEFAULT_SPINNER_IS_VISIBLE__ = true;
-export const __DEFAULT_SPINNER_VARIANT__: SpinnerVariant = 'three_dots';
-export const __DEFAULT_SPINNER_SIZE__: SpinnerSize = 'xl';
