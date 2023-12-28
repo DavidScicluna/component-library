327c57010469ab10e22cbc8cdf6bef244e5654f7
DavidScicluna <dave_nostra@hotmail.com>
2023-12-28 22:57:01 +0100

    Fix: Lazy Exports

--- src/components/Lazy/index.ts
+++ src/components/Lazy/index.ts
@@ -1,10 +1,2 @@
-import type { ErrorBoundaryProps } from './components/ErrorBoundary';
-import { ErrorBoundary } from './components/ErrorBoundary';
-import type { SuspenseProps } from './components/Suspense';
-import { Suspense } from './components/Suspense';
-
-// Components
-export { ErrorBoundary, Suspense };
-
-// Component Types
-export type { ErrorBoundaryProps, SuspenseProps };
+export * from './components/ErrorBoundary';
+export * from './components/Suspense';
