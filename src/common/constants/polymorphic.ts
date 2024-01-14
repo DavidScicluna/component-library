c2fbcfcabff6dad945e42158f536169dc1210f9e
DavidScicluna <dave_nostra@hotmail.com>
2024-01-14 23:51:51 +0100

    Fix: polymorphic constants

--- src/common/constants/polymorphic.ts
+++ src/common/constants/polymorphic.ts
@@ -1,5 +1,5 @@
-import type { ElementType } from 'react';
+import type { PolymorphicElementType } from '@common/types';
 
-export const __DEFAULT_POLYMORPHIC_ELEMENT__: ElementType = 'div';
+export const __DEFAULT_POLYMORPHIC_ELEMENT__: PolymorphicElementType = 'div';
 export const __DEFAULT_POLYMORPHIC_OBJECT__ = {};
 export const __DEFAULT_POLYMORPHIC_SX__ = {};
