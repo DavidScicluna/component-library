0ad47c0d7d690590417913a71cd5099f046c7e5f
DavidScicluna <dave_nostra@hotmail.com>
2024-01-15 20:23:22 +0100

    Fix: Icon - useIconStyles

--- src/components/DataDisplay/components/Icon/common/hooks/useIconStyles.ts
+++ src/components/DataDisplay/components/Icon/common/hooks/useIconStyles.ts
@@ -2,16 +2,14 @@ import type { Style } from '@common/types';
 import { checkFontSizeType } from '@common/utils';
 
 import { __DEFAULT_ICON_SIZE__ } from '../constants';
-import type { IconDefaultElement, IconElement, IconProps } from '../types';
+import type { IconProps } from '../types';
 
 import useIconResponsiveValues from './useIconResponsiveValues';
 
-type UseIconStylesProps<Element extends IconElement = IconDefaultElement> = Pick<IconProps<Element>, 'size'>;
+type UseIconStylesProps = Pick<IconProps, 'size'>;
 type UseIconStylesReturn = Style;
 
-const useIconStyles = <Element extends IconElement = IconDefaultElement>(
-	props: UseIconStylesProps<Element>
-): UseIconStylesReturn => {
+const useIconStyles = (props: UseIconStylesProps): UseIconStylesReturn => {
 	const { size: sizeProp = __DEFAULT_ICON_SIZE__ } = props;
 
 	const { size } = useIconResponsiveValues({ size: sizeProp });
