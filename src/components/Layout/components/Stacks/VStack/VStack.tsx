fec0e3e307234fc1799619d7ea22fc55c1a5fd76
DavidScicluna <dave_nostra@hotmail.com>
2024-01-16 23:01:11 +0100

    Fix: Stacks - VStack

--- src/components/Layout/components/Stacks/VStack/VStack.tsx
+++ src/components/Layout/components/Stacks/VStack/VStack.tsx
@@ -2,13 +2,7 @@ import type { ReactElement } from 'react';
 import { forwardRef } from 'react';
 
 import { __DEFAULT_CLASSNAME__ } from '@common/constants';
-import type {
-	PolymorphicComponentPropsWithRef,
-	PolymorphicComponentWithRef,
-	PolymorphicDefaultElement,
-	PolymorphicDefaultProps,
-	PolymorphicElementType
-} from '@common/types';
+import type { PolymorphicDefaultElement, PolymorphicElementType } from '@common/types';
 
 import { Stack } from '../Stack';
 
@@ -18,14 +12,15 @@ import type { VStackProps, VStackRef } from './common/types';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const classNames = require('classnames');
 
-const VStack: PolymorphicComponentWithRef = forwardRef(function VStack<
-	Element extends PolymorphicElementType = PolymorphicDefaultElement
->(props: VStackProps<Element>, ref: VStackRef<Element>): ReactElement {
+const VStack = forwardRef(function VStack<Element extends PolymorphicElementType = PolymorphicDefaultElement>(
+	props: VStackProps<Element>,
+	ref: VStackRef<Element>
+): ReactElement {
 	const { children, className = __DEFAULT_CLASSNAME__, ...rest } = props;
 
 	return (
-		<Stack<Element>
-			{...rest}
+		<Stack
+			{...(rest as VStackProps<Element>)}
 			ref={ref}
 			className={classNames(__KEYS_V_STACK_CLASS__, { [className]: !!className })}
 			direction='column'
@@ -35,8 +30,6 @@ const VStack: PolymorphicComponentWithRef = forwardRef(function VStack<
 	);
 });
 
-VStack.displayName = 'VStack';
+// VStack.displayName = 'VStack';
 
-export default <Element extends PolymorphicElementType = PolymorphicDefaultElement, Props = PolymorphicDefaultProps>(
-	props: PolymorphicComponentPropsWithRef<Element, Props>
-) => <VStack<Element> {...props} />;
+export default VStack;
