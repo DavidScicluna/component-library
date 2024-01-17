04d28fbea6cf92a076fdf426efbddf840d05c55c
DavidScicluna <dave_nostra@hotmail.com>
2024-01-17 22:10:31 +0100

    Feature: Types - Utility - Added MergeTypes type

--- src/common/types/utility.ts
+++ src/common/types/utility.ts
@@ -5,3 +5,7 @@ export type Nullish<T> = T | null; // Adds null to type
 export type PickFrom<T, K extends T> = {
 	[P in keyof K]: P extends K ? P : never;
 };
+
+export type MergeTypes<T, U> = {
+	[K in keyof T]: K extends keyof U ? U[K] : T[K];
+} & U;
