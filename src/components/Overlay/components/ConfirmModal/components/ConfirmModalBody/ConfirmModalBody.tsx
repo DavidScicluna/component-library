16e2d47a57020b496724318e94b13bb2e5b8bc89
DavidScicluna <dave_nostra@hotmail.com>
2024-01-21 18:40:43 +0100

    Fix: ConfirmModal - ConfirmModalBody

--- src/components/Overlay/components/ConfirmModal/components/ConfirmModalBody/ConfirmModalBody.tsx
+++ src/components/Overlay/components/ConfirmModal/components/ConfirmModalBody/ConfirmModalBody.tsx
@@ -2,14 +2,9 @@ import type { ReactElement } from 'react';
 import { forwardRef } from 'react';
 
 import { __DEFAULT_CLASSNAME__ } from '@common/constants';
-import type {
-	PolymorphicComponentPropsWithRef,
-	PolymorphicComponentWithRef,
-	PolymorphicDefaultElement,
-	PolymorphicDefaultProps,
-	PolymorphicElementType
-} from '@common/types';
+import type { PolymorphicElementType } from '@common/types';
 
+import type { VStackProps } from '@components/Layout';
 import { VStack } from '@components/Layout';
 
 import { __DEFAULT_CONFIRM_MODAL_BODY_SPACING__ } from './common/constants';
@@ -19,9 +14,10 @@ import type { ConfirmModalBodyProps, ConfirmModalBodyRef } from './common/types'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const classNames = require('classnames');
 
-const ConfirmModalBody: PolymorphicComponentWithRef = forwardRef(function ConfirmModalBody<
-	Element extends PolymorphicElementType = PolymorphicDefaultElement
->(props: ConfirmModalBodyProps<Element>, ref: ConfirmModalBodyRef<Element>): ReactElement {
+const ConfirmModalBody = forwardRef(function ConfirmModalBody<Element extends PolymorphicElementType>(
+	props: ConfirmModalBodyProps<Element>,
+	ref: ConfirmModalBodyRef<Element>
+): ReactElement {
 	const {
 		children,
 		className = __DEFAULT_CLASSNAME__,
@@ -32,8 +28,8 @@ const ConfirmModalBody: PolymorphicComponentWithRef = forwardRef(function Confir
 	} = props;
 
 	return (
-		<VStack<Element>
-			{...rest}
+		<VStack
+			{...(rest as VStackProps<Element>)}
 			ref={ref}
 			className={classNames(__KEYS_CONFIRM_MODAL_BODY_CLASS__, { [className]: !!className })}
 			w='100%'
@@ -47,8 +43,6 @@ const ConfirmModalBody: PolymorphicComponentWithRef = forwardRef(function Confir
 	);
 });
 
-ConfirmModalBody.displayName = 'ConfirmModalBody';
+// ConfirmModalBody.displayName = 'ConfirmModalBody';
 
-export default <Element extends PolymorphicElementType = PolymorphicDefaultElement, Props = PolymorphicDefaultProps>(
-	props: PolymorphicComponentPropsWithRef<Element, Props>
-) => <ConfirmModalBody<Element> {...props} />;
+export default ConfirmModalBody;
--- src/components/Overlay/components/ConfirmModal/components/ConfirmModalBody/common/types/index.ts
+++ src/components/Overlay/components/ConfirmModal/components/ConfirmModalBody/common/types/index.ts
@@ -1,11 +1,10 @@
-import type { PolymorphicDefaultElement, PolymorphicElementType } from '@common/types';
+import type { PolymorphicElementType } from '@common/types';
 
 import type { VStackProps, VStackRef } from '@components/Layout';
 
-export type ConfirmModalBodyProps<Element extends PolymorphicElementType = PolymorphicDefaultElement> = Omit<
+export type ConfirmModalBodyProps<Element extends PolymorphicElementType> = Omit<
 	VStackProps<Element>,
 	'w' | 'h' | 'divider'
 >;
 
-export type ConfirmModalBodyRef<Element extends PolymorphicElementType = PolymorphicDefaultElement> =
-	VStackRef<Element>;
+export type ConfirmModalBodyRef<Element extends PolymorphicElementType> = VStackRef<Element>;
