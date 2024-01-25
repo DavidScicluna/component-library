3b28bbee7f676f131f7e7bb6f4b436955cbe33b8
DavidScicluna <dave_nostra@hotmail.com>
2024-01-25 23:01:54 +0100

    Fix: Tabs - TabPanels

--- src/components/Navigation/components/Tabs/components/TabPanels/TabPanels.tsx
+++ src/components/Navigation/components/Tabs/components/TabPanels/TabPanels.tsx
@@ -8,7 +8,7 @@ import type { PolymorphicElementType } from '@common/types';
 
 import { Transition } from '@components/Animation';
 import type { GridProps } from '@components/Layout';
-import { Grid, GridItem } from '@components/Layout';
+import { Center, Grid, GridItem } from '@components/Layout';
 
 import { useTabsContext } from '../../common/hooks';
 import { getTabPanelID, getTabPanelsID, getTabsID } from '../../common/utils';
@@ -23,7 +23,15 @@ const TabPanel = <Element extends PolymorphicElementType>({ children, index }: T
 	const { index: panel } = useTabsContext();
 
 	return (
-		<Transition w='100%' h='100%' duration='slow' easing='ease-in-out' transition='fade' in={panel === index}>
+		<Transition
+			as={Center}
+			w='100%'
+			h='100%'
+			duration='slow'
+			easing='ease-in-out'
+			transition='fade'
+			in={panel === index}
+		>
 			{children}
 		</Transition>
 	);
