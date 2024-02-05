9ebc21088ed33e558c1af9026e2f9f1e0aebddd4
DavidScicluna <dave_nostra@hotmail.com>
2024-02-05 21:35:55 +0100

    Fix: Types - Responsive - ResponsiveValueProps

--- src/common/types/responsive.ts
+++ src/common/types/responsive.ts
@@ -4,6 +4,6 @@ export type ResponsiveArrayValue<D> = [D?, D?, D?, D?, D?, D?];
 export type ResponsiveObjectValue<D> = Record<ThemeBreakpoint, D>;
 export type ResponsiveValue<D> = ResponsiveArrayValue<D> | ResponsiveObjectValue<D> | D;
 
-export type ResponsiveValueProps<T, D extends keyof T> = {
-	[K in D]: ResponsiveValue<T[K]>;
+export type ResponsiveValueProps<D extends object> = {
+	[P in keyof D]: ResponsiveValue<D[P]>;
 };
