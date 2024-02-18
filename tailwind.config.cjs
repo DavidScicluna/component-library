664d19503fbfe8f197be56052c5932963d8165ff
DavidScicluna <dave_nostra@hotmail.com>
2024-02-18 22:57:00 +0100

    Fix: Renamed tailwind.config.js to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
-import theme from './src/common/theme';
+const path = require('path');
 
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: { ...theme },
+	theme: { ...path.resolve(__dirname, './src/common/theme') },
 	plugins: [require('tailwind-scrollbar-hide')],
 	...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
 };
