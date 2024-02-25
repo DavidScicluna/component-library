659cb4bb4353bf45ae2c3dc578f60ef8b002560f
DavidScicluna <dave_nostra@hotmail.com>
2024-02-25 19:09:36 +0100

    Fix: Hooks - useGetResponsiveValue

--- src/common/hooks/useGetResponsiveValue.ts
+++ src/common/hooks/useGetResponsiveValue.ts
@@ -1,17 +1,34 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+import { debounce } from 'lodash-es';
+import { useWindowEventListener } from 'rooks';
 
 import type { ResponsiveValue } from '@common/types';
 import { getResponsiveValue } from '@common/utils';
 
+import useBreakpoint from './useBreakpoint';
+
 /**
  * This hook will return the appropriate responsive value depending on current dimensions of the users device
  *
- * @param value - ResponsiveValue
+ * @param responsiveValue - ResponsiveValue
  * @returns - The responsive value within the array or object that aligns with the breakpoint
  */
-const useGetResponsiveValue = <D>(value: ResponsiveValue<D>): D => {
-	const v = useMemo<D>(() => getResponsiveValue<D>(value), [value]);
-	return v;
+const useGetResponsiveValue = <D>(responsiveValue: ResponsiveValue<D>): D => {
+	const breakpoint = useBreakpoint();
+	const [actualValue, setActualValue] = useState<D>(getResponsiveValue<D>(responsiveValue));
+
+	const handleGetValue = useCallback(
+		debounce(() => {
+			setActualValue(getResponsiveValue<D>(responsiveValue, breakpoint));
+		}, 0),
+		[responsiveValue, breakpoint]
+	);
+
+	useEffect(() => handleGetValue(), [responsiveValue, breakpoint]);
+	useWindowEventListener('resize', handleGetValue);
+
+	return actualValue;
 };
 
 export default useGetResponsiveValue;
