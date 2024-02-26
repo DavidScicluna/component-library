1635ac3c0d5e8baee42b3f384e66d991f6db2f19
DavidScicluna <dave_nostra@hotmail.com>
2024-02-26 21:58:34 +0100

    Fix: Utils exports

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,49 +1,9 @@
-import { getAmount } from './amount';
-import { getAnimationConfig, getAnimationDelay, getAnimationDuration, getAnimationEasings } from './animation';
-import { getClass } from './classes';
-import type {
-	ColorHueType,
-	FilterColorHexProps,
-	GetColorClassProps,
-	GetColorHexProps,
-	GetColorHueProps
-} from './color';
-import { filterColorHex, getColorClass, getColorHex, getColorHue, getColorMode } from './color';
-import { getFontSizeHeight, getIconFontFamily } from './fonts';
-import { getPercentage } from './number';
-import {
-	// checkIsTouchDevice,
-	convertPixelsToREM,
-	convertREMToPixels,
-	convertStringToNumber
-} from './other';
-import { getBreakpoint, getResponsiveValue } from './responsive';
-import { checkColorType, checkDurationType, checkFontSizeType } from './theme';
-
-export type { ColorHueType, FilterColorHexProps, GetColorClassProps, GetColorHexProps, GetColorHueProps };
-
-export {
-	checkColorType,
-	checkDurationType,
-	checkFontSizeType,
-	// checkIsTouchDevice,
-	convertPixelsToREM,
-	convertREMToPixels,
-	convertStringToNumber,
-	filterColorHex,
-	getAmount,
-	getAnimationConfig,
-	getAnimationDelay,
-	getAnimationDuration,
-	getAnimationEasings,
-	getBreakpoint,
-	getClass,
-	getColorClass,
-	getColorHex,
-	getColorHue,
-	getColorMode,
-	getFontSizeHeight,
-	getIconFontFamily,
-	getPercentage,
-	getResponsiveValue
-};
+export * from './amount';
+export * from './animation';
+export * from './classes';
+export * from './color';
+export * from './fonts';
+export * from './number';
+export * from './other';
+export * from './responsive';
+export * from './theme';
