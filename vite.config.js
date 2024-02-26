1b0617abd7bab78b0beb8716f151810da761270b
DavidScicluna <dave_nostra@hotmail.com>
2024-02-26 21:43:45 +0100

    Fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import { resolve } from 'path';
 import viteCompression from 'vite-plugin-compression';
 import dts from 'vite-plugin-dts';
 import tailwindcss from 'tailwindcss';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
+	optimizeDeps: {
+		esbuildOptions: {
+			jsx: 'automatic',
+			define: { global: 'globalThis' },
+			plugins: [NodeGlobalsPolyfillPlugin({ process: true, buffer: true }), NodeModulesPolyfillPlugin()]
+		}
+	},
 	build: {
 		outDir: 'build',
 		lib: {
@@ -27,45 +37,46 @@ export default defineConfig({
 					'react-dom': 'ReactDOM',
 					'tailwindcss': 'tailwindcss'
 				}
-			}
+			},
+			plugins: [rollupNodePolyFill()]
 		},
 		// commonjsOptions: { transformMixedEsModules: true },
 		sourcemap: true,
 		emptyOutDir: true
 	},
 	plugins: [
-		react(),
+		react({ jsxImportSource: '@emotion/react', babel: { plugins: ['@emotion/babel-plugin'] } }),
+		tsConfigPaths(),
 		libInjectCss(),
 		dts({ insertTypesEntry: true, rollupTypes: true }),
 		external(),
-		viteCommonjs(),
 		viteCompression()
 	],
 	// external: ['react', 'react-dom', 'tailwindcss'],
-	css: { postcss: { plugins: [tailwindcss] } },
-	resolve: {
-		alias: {
-			'@common/classes': resolve(__dirname, './src/common/classes'),
-			'@common/constants': resolve(__dirname, './src/common/constants'),
-			'@common/hooks': resolve(__dirname, './src/common/hooks'),
-			'@common/types': resolve(__dirname, './src/common/types'),
-			'@common/utils': resolve(__dirname, './src/common/utils'),
-			'@common/keys': resolve(__dirname, './src/common/keys'),
-			'@common/data': resolve(__dirname, './src/common/data'),
-			'@common/theme': resolve(__dirname, './src/common/theme'),
-			'@common/helpers': resolve(__dirname, './src/common/helpers'),
-			'@components/Layout': resolve(__dirname, './src/components/Layout'),
-			'@components/Typography': resolve(__dirname, './src/components/Typography'),
-			'@components/Provider': resolve(__dirname, './src/components/Provider'),
-			'@components/Box': resolve(__dirname, './src/components/Box'),
-			'@components/DataDisplay': resolve(__dirname, './src/components/DataDisplay'),
-			'@components/Buttons': resolve(__dirname, './src/components/Buttons'),
-			'@components/Animation': resolve(__dirname, './src/components/Animation'),
-			'@components/Feedback': resolve(__dirname, './src/components/Feedback'),
-			'@components/Overlay': resolve(__dirname, './src/components/Overlay'),
-			'@components/Navigation': resolve(__dirname, './src/components/Navigation'),
-			'@components/Forms': resolve(__dirname, './src/components/Forms'),
-			'@components/VisuallyHidden': resolve(__dirname, './src/components/VisuallyHidden')
-		}
-	}
+	css: { postcss: { plugins: [tailwindcss] } }
+	// resolve: {
+	// 	alias: {
+	// 		'@common/classes': resolve(__dirname, './src/common/classes'),
+	// 		'@common/constants': resolve(__dirname, './src/common/constants'),
+	// 		'@common/hooks': resolve(__dirname, './src/common/hooks'),
+	// 		'@common/types': resolve(__dirname, './src/common/types'),
+	// 		'@common/utils': resolve(__dirname, './src/common/utils'),
+	// 		'@common/keys': resolve(__dirname, './src/common/keys'),
+	// 		'@common/data': resolve(__dirname, './src/common/data'),
+	// 		'@common/theme': resolve(__dirname, './src/common/theme'),
+	// 		'@common/helpers': resolve(__dirname, './src/common/helpers'),
+	// 		'@components/Layout': resolve(__dirname, './src/components/Layout'),
+	// 		'@components/Typography': resolve(__dirname, './src/components/Typography'),
+	// 		'@components/Provider': resolve(__dirname, './src/components/Provider'),
+	// 		'@components/Box': resolve(__dirname, './src/components/Box'),
+	// 		'@components/DataDisplay': resolve(__dirname, './src/components/DataDisplay'),
+	// 		'@components/Buttons': resolve(__dirname, './src/components/Buttons'),
+	// 		'@components/Animation': resolve(__dirname, './src/components/Animation'),
+	// 		'@components/Feedback': resolve(__dirname, './src/components/Feedback'),
+	// 		'@components/Overlay': resolve(__dirname, './src/components/Overlay'),
+	// 		'@components/Navigation': resolve(__dirname, './src/components/Navigation'),
+	// 		'@components/Forms': resolve(__dirname, './src/components/Forms'),
+	// 		'@components/VisuallyHidden': resolve(__dirname, './src/components/VisuallyHidden')
+	// }
+	// }
 });
