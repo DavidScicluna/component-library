4ba5f2654c93257b6b31ade8488dba906aa4398b
DavidScicluna <dave_nostra@hotmail.com>
2024-03-09 17:41:42 +0100

    Fix: Indicator

--- src/components/Overlay/components/Indicator/Indicator.tsx
+++ src/components/Overlay/components/Indicator/Indicator.tsx
@@ -7,7 +7,6 @@ import type { PolymorphicElementType } from '@common/types';
 
 import { Transition } from '@components/Animation';
 import { Box } from '@components/Box';
-import type { GridProps } from '@components/Layout';
 import { Grid, GridItem } from '@components/Layout';
 
 import {
@@ -43,7 +42,7 @@ const Indicator = forwardRef(function Indicator<Element extends PolymorphicEleme
 
 	return (
 		<Grid
-			{...(rest as GridProps<Element>)}
+			{...rest}
 			ref={ref}
 			className={classNames(KEYS_INDICATOR_CLASS, { [className]: !!className })}
 			templateColumns={1}
